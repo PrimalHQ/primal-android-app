a73dec622214dfcc497626f9471cf8927d76fa52
Aleksandar Ilic <aleksandar@appollo41.com>
2023-05-19 19:43:37 +0200

    Implement extracting postId and postAuthorId for Repost from tags if content is missing

--- app/src/main/kotlin/net/primal/android/nostr/processor/RepostEventProcessor.kt
+++ app/src/main/kotlin/net/primal/android/nostr/processor/RepostEventProcessor.kt
@@ -1,12 +1,14 @@
 package net.primal.android.nostr.processor
 
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.jsonPrimitive
 import net.primal.android.db.PrimalDatabase
 import net.primal.android.feed.db.Repost
 import net.primal.android.nostr.model.NostrEvent
 import net.primal.android.nostr.model.NostrEventKind
 import net.primal.android.serialization.NostrJson
-import timber.log.Timber
 
 class RepostEventProcessor(
     private val database: PrimalDatabase,
@@ -19,33 +21,54 @@ class RepostEventProcessor(
         ShortTextNoteEventProcessor(database = database).process(referencedPosts)
 
         database.reposts().upsertAll(
-            events = referencedPosts.mapNotNull {
-                it.asRepost()
+            events = events.mapNotNull {
+                val (postId, postAuthorId) = it.findPostAndAuthorIds()
+                if (postId != null && postAuthorId != null) {
+                    it.asRepost(postId, postAuthorId)
+                } else null
             }
         )
-
-        events.filter { it.content.isEmpty() }.forEach {
-            Timber.e("Unable to process following nostr event: $it")
-        }
     }
 
-    private fun NostrEvent.asRepost(): Repost? {
-        val contentNostrEvent = NostrJson.decodeFromJsonElement<NostrEvent>(
-            NostrJson.parseToJsonElement(this.content)
-        )
+    private fun NostrEvent.asRepost(postId: String, postAuthorId: String) = Repost(
+        eventId = this.id,
+        authorId = this.pubKey,
+        createdAt = this.createdAt,
+        tags = this.tags,
+        sig = this.sig,
+        postId = postId,
+        postAuthorId = postAuthorId
+    )
 
-        val postId = contentNostrEvent.id
-        val postAuthorId = contentNostrEvent.pubKey
+    private fun NostrEvent.findPostAndAuthorIds(): Pair<String?, String?> {
+        val contentNostrEvent = if (this.content.isNotEmpty()) {
+            NostrJson.decodeFromJsonElement<NostrEvent>(
+                NostrJson.parseToJsonElement(this.content)
+            )
+        } else null
 
-        return Repost(
-            eventId = this.id,
-            authorId = this.pubKey,
-            createdAt = this.createdAt,
-            tags = this.tags,
-            sig = this.sig,
-            postId = postId,
-            postAuthorId = postAuthorId
-        )
+        val postId = contentNostrEvent?.id ?: this.tags.findPostId()
+        val postAuthorId = contentNostrEvent?.pubKey ?: this.tags.findPostAuthorId()
+
+        return Pair(postId, postAuthorId)
+    }
+
+    private fun List<JsonArray>.findPostId(): String? {
+        val postTag = firstOrNull { it.isEventIdTag() && it.hasMentionMarker() }
+        return postTag?.getTagValueOrNull()
+    }
+
+    private fun JsonArray.isEventIdTag() = getOrNull(0)?.jsonPrimitive?.content == "e"
+
+    private fun JsonArray.isPubKeyTag() = getOrNull(0)?.jsonPrimitive?.content == "p"
+
+    private fun JsonArray.getTagValueOrNull() = getOrNull(1)?.jsonPrimitive?.content
+
+    private fun JsonArray.hasMentionMarker() = contains(JsonPrimitive("mention"))
+
+    private fun List<JsonArray>.findPostAuthorId(): String? {
+        val postAuthorTag = firstOrNull { it.isPubKeyTag() }
+        return postAuthorTag?.getTagValueOrNull()
     }
 
 }
