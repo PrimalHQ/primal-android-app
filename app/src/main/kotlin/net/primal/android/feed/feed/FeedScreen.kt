d6b62e2a5fab109271c4f072455199b8d2548a1c
Aleksandar Ilic <aleksandar@appollo41.com>
2023-06-28 23:01:02 +0200

    Fix not showing default avatar on FeedScreen if user doesn't have avatar

--- app/src/main/kotlin/net/primal/android/feed/feed/FeedScreen.kt
+++ app/src/main/kotlin/net/primal/android/feed/feed/FeedScreen.kt
@@ -76,6 +76,7 @@ import net.primal.android.core.compose.PrimalNavigationBar
 import net.primal.android.core.compose.PrimalTopAppBar
 import net.primal.android.core.compose.PrimalTopLevelDestination
 import net.primal.android.core.compose.icons.PrimalIcons
+import net.primal.android.core.compose.icons.primaliconpack.AvatarDefault
 import net.primal.android.core.compose.icons.primaliconpack.FeedPicker
 import net.primal.android.core.compose.isEmpty
 import net.primal.android.drawer.DrawerScreenDestination
@@ -162,6 +163,7 @@ fun FeedScreen(
                     PrimalTopAppBar(
                         title = state.feedTitle,
                         avatarUrl = state.activeAccountAvatarUrl,
+                        navigationIcon = PrimalIcons.AvatarDefault,
                         onNavigationIconClick = {
                             uiScope.launch { drawerState.open() }
                         },
