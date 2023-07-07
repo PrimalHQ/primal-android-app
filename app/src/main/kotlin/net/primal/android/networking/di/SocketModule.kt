1b5d01b6078a8fd889dd0c2743ca11e058e685e7
Aleksandar Ilic <aleksandar@appollo41.com>
2023-07-07 22:44:25 +0200

    Change primal base url

--- app/src/main/kotlin/net/primal/android/networking/di/SocketModule.kt
+++ app/src/main/kotlin/net/primal/android/networking/di/SocketModule.kt
@@ -13,7 +13,7 @@ object SocketModule {
     @Provides
     @PrimalApiWS
     fun providePrimalApiWSRequest() = Request.Builder()
-        .url("wss://cache3.primal.net/cache17")
+        .url("wss://cache1.primal.net/v1")
         .build()
 
 }
