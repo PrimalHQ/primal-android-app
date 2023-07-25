5d833f16ce6097f66cd44764cf3bdf876ff5a45f
Aleksandar Ilic <aleksandar@appollo41.com>
2023-07-26 01:56:37 +0200

    Add recommendedRelay and marker to event and pubkey tags creation

--- app/src/main/kotlin/net/primal/android/nostr/notary/TagsExt.kt
+++ app/src/main/kotlin/net/primal/android/nostr/notary/TagsExt.kt
@@ -4,14 +4,24 @@ import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.add
 import kotlinx.serialization.json.buildJsonArray
 
-fun String.asEventIdTag(): JsonArray = buildJsonArray {
+fun String.asEventIdTag(
+    recommendedRelay: String = "",
+    marker: String? = null,
+): JsonArray = buildJsonArray {
     add("e")
     add(this@asEventIdTag)
+    add(recommendedRelay)
+    if (marker != null) add(marker)
 }
 
-fun String.asPubkeyTag(): JsonArray = buildJsonArray {
+fun String.asPubkeyTag(
+    recommendedRelay: String = "",
+    marker: String? = null,
+): JsonArray = buildJsonArray {
     add("p")
     add(this@asPubkeyTag)
+    add(recommendedRelay)
+    if (marker != null) add(marker)
 }
 
 fun String.asIdentifierTag(): JsonArray = buildJsonArray {
--- app/src/test/kotlin/net/primal/android/nostr/notary/TagExtTest.kt
+++ app/src/test/kotlin/net/primal/android/nostr/notary/TagExtTest.kt
@@ -11,21 +11,35 @@ class TagExtTest {
     @Test
     fun `asEventIdTag returns proper JsonArray tag`() {
         val eventId = "eventId"
-        val actual = eventId.asEventIdTag()
+        val expectedRecommendedRelay = ""
+        val expectedMarker = "root"
+        val actual = eventId.asEventIdTag(
+            recommendedRelay = expectedRecommendedRelay,
+            marker = expectedMarker,
+        )
         actual shouldBe instanceOf(JsonArray::class)
-        actual.size shouldBe 2
+        actual.size shouldBe 4
         actual[0].jsonPrimitive.content shouldBe "e"
         actual[1].jsonPrimitive.content shouldBe eventId
+        actual[2].jsonPrimitive.content shouldBe expectedRecommendedRelay
+        actual[3].jsonPrimitive.content shouldBe expectedMarker
     }
 
     @Test
     fun `asPubkeyTag returns proper JsonArray tag`() {
         val pubkey = "myPubkey"
-        val actual = pubkey.asPubkeyTag()
+        val expectedRecommendedRelay = ""
+        val expectedMarker = "mention"
+        val actual = pubkey.asPubkeyTag(
+            recommendedRelay = expectedRecommendedRelay,
+            marker = expectedMarker,
+        )
         actual shouldBe instanceOf(JsonArray::class)
-        actual.size shouldBe 2
+        actual.size shouldBe 4
         actual[0].jsonPrimitive.content shouldBe "p"
         actual[1].jsonPrimitive.content shouldBe pubkey
+        actual[2].jsonPrimitive.content shouldBe expectedRecommendedRelay
+        actual[3].jsonPrimitive.content shouldBe expectedMarker
     }
 
     @Test
