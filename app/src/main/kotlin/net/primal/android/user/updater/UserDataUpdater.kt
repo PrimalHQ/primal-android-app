950004b51c10a485e09104ddd502ae18614c3cb1
Aleksandar Ilic <aleksandar@appollo41.com>
2023-08-15 14:16:24 +0200

    Add try-catch in UserDataUpdater for WssException

--- app/src/main/kotlin/net/primal/android/user/updater/UserDataUpdater.kt
+++ app/src/main/kotlin/net/primal/android/user/updater/UserDataUpdater.kt
@@ -2,8 +2,10 @@ package net.primal.android.user.updater
 
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import net.primal.android.networking.sockets.errors.WssException
 import net.primal.android.settings.repository.SettingsRepository
 import net.primal.android.user.repository.UserRepository
+import timber.log.Timber
 import java.time.Instant
 
 class UserDataUpdater @AssistedInject constructor(
@@ -20,8 +22,12 @@ class UserDataUpdater @AssistedInject constructor(
 
     suspend fun updateUserDataWithDebounce(timeoutInSeconds: Long) {
         if (isUserDataSyncedInLast(seconds = timeoutInSeconds)) {
-            updateData()
-            lastTimeFetched = Instant.now()
+            try {
+                updateData()
+                lastTimeFetched = Instant.now()
+            } catch (error: WssException) {
+                Timber.i(error)
+            }
         }
     }
 
