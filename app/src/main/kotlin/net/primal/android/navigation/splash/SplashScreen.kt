61f7c01eac42e0902688ce43b3a825b5ffe8daf8
Nikola Lukovic <nikola.lukovic@live.com>
2023-09-11 19:09:38 +0200

    Spelling fix and separation of fetching follows and creating a nostr account

--- app/src/main/kotlin/net/primal/android/navigation/splash/SplashScreen.kt
+++ app/src/main/kotlin/net/primal/android/navigation/splash/SplashScreen.kt
@@ -1,9 +1,9 @@
 package net.primal.android.navigation.splash
 
 import androidx.compose.runtime.Composable
-import net.primal.android.core.compose.PrimalLoadingSinner
+import net.primal.android.core.compose.PrimalLoadingSpinner
 
 @Composable
 fun SplashScreen() {
-    PrimalLoadingSinner()
+    PrimalLoadingSpinner()
 }
