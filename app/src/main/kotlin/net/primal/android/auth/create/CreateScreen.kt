a1d40933cab2cfd7f130c538efeb53fe6e4b2a10
Nikola Lukovic <nikola.lukovic@live.com>
2023-09-13 18:11:37 +0200

    Fixed UI/UX follow/unfollow

--- app/src/main/kotlin/net/primal/android/auth/create/CreateScreen.kt
+++ app/src/main/kotlin/net/primal/android/auth/create/CreateScreen.kt
@@ -45,7 +45,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -587,118 +586,126 @@ fun ProfilePreviewStep(
 fun FollowRecommendedAccountsStep(
     state: CreateContract.UiState,
     eventPublisher: (CreateContract.UiEvent) -> Unit
-) = if (state.loading && state.recommendedFollows.isEmpty()) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        PrimalLoadingSpinner()
-    }
-} else {
+) {
     LazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
-        state.recommendedFollows.forEach {
-            stickyHeader {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(70.dp)
-                        .background(
-                            brush = Brush.linearGradient(
-                                colors = listOf(
-                                    Color(0xFF181818), Color(0xFF222222)
-                                )
-                            )
-                        )
-                        .border(
-                            width = 1.dp,
-                            color = Color(0xFF222222),
-                            shape = RoundedCornerShape(8.dp),
-                        )
-                        .padding(8.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
+        if (state.loading && state.recommendedFollows.isEmpty()) {
+            item {
+                Column(
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Text(it.key)
-                    PrimalOutlinedButton(
-                        onClick = {
-                            val pubkeys =
-                                state.recommendedFollows[it.key]!!.map { rf -> rf.pubkey }.toSet()
-
-                            if (state.following.containsAll(pubkeys)) {
-                                eventPublisher(CreateContract.UiEvent.GroupUnfollowEvent(groupName = it.key))
-                            } else {
-                                eventPublisher(CreateContract.UiEvent.GroupFollowEvent(groupName = it.key))
-                            }
-                        }
-                    ) {
-                        Text("Follow All")
-                    }
+                    PrimalLoadingSpinner()
                 }
             }
-
-            items(it.value) { suggestion ->
-                Row(
-                    modifier = Modifier
-                        .padding(horizontal = 32.dp, vertical = 12.dp)
-                        .fillMaxWidth()
-                        .height(48.dp)
-                        .background(Color.Black),
-                    horizontalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    val model =
-                        ImageRequest.Builder(LocalContext.current)
-                            .data(suggestion.content.picture)
-                            .build()
-                    AsyncImage(
-                        model = model,
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop,
+        } else {
+            val follows = state.recommendedFollows.groupBy { it.groupName }
+            follows.forEach { group ->
+                stickyHeader {
+                    Row(
                         modifier = Modifier
-                            .height(48.dp)
-                            .width(48.dp)
-                            .clip(CircleShape)
-                    )
-                    Column(
-                        modifier = Modifier
-                            .padding(start = 8.dp)
-                            .fillMaxWidth(0.65f),
-                        horizontalAlignment = Alignment.Start,
-                        verticalArrangement = Arrangement.Center
+                            .fillMaxWidth()
+                            .height(70.dp)
+                            .background(
+                                brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        Color(0xFF181818), Color(0xFF222222)
+                                    )
+                                )
+                            )
+                            .border(
+                                width = 1.dp,
+                                color = Color(0xFF222222),
+                                shape = RoundedCornerShape(8.dp),
+                            )
+                            .padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
                     ) {
-                        Text(
-                            text = suggestion.content.displayName ?: suggestion.content.name
-                            ?: "",
-                            fontWeight = FontWeight.W700,
-                            fontSize = 14.sp,
-                            lineHeight = 12.sp,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            color = Color.White
-                        )
-                        Text(
-                            text = suggestion.content.nip05 ?: "",
-                            fontWeight = FontWeight.W400,
-                            fontSize = 12.sp,
-                            lineHeight = 16.sp,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            color = Color(0xFF666666)
-                        )
-                    }
-                    PrimalOutlinedButton(
-                        onClick = {
-                            if (state.following.contains(suggestion.pubkey)) {
-                                eventPublisher(CreateContract.UiEvent.UnfollowEvent(pubkey = suggestion.pubkey))
-                            } else {
-                                eventPublisher(CreateContract.UiEvent.FollowEvent(pubkey = suggestion.pubkey))
+                        Text(group.key)
+                        PrimalOutlinedButton(
+                            onClick = {
+                                eventPublisher(
+                                    CreateContract.UiEvent.ToggleGroupFollowEvent(
+                                        groupName = group.key
+                                    )
+                                )
                             }
-                        },
+                        ) {
+                            val text =
+                                if (state.recommendedFollows.filter { it.groupName == group.key }
+                                        .any { !it.isCurrentUserFollowing }) "Follow All" else "Unfollow All"
+                            Text(text)
+                        }
+                    }
+                }
+
+                items(group.value) { suggestion ->
+                    Row(
                         modifier = Modifier
-                            .defaultMinSize(minWidth = 92.dp)
-                            .height(36.dp)
+                            .padding(horizontal = 32.dp, vertical = 12.dp)
+                            .fillMaxWidth()
+                            .height(48.dp)
+                            .background(Color.Black),
+                        horizontalArrangement = Arrangement.spacedBy(4.dp)
                     ) {
-                        Text("Follow")
+                        val model =
+                            ImageRequest.Builder(LocalContext.current)
+                                .data(suggestion.content.picture)
+                                .build()
+                        AsyncImage(
+                            model = model,
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .height(48.dp)
+                                .width(48.dp)
+                                .clip(CircleShape)
+                        )
+                        Column(
+                            modifier = Modifier
+                                .padding(start = 8.dp)
+                                .fillMaxWidth(0.6f),
+                            horizontalAlignment = Alignment.Start,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = suggestion.content.displayName ?: suggestion.content.name
+                                ?: "",
+                                fontWeight = FontWeight.W700,
+                                fontSize = 14.sp,
+                                lineHeight = 12.sp,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                color = Color.White
+                            )
+                            Text(
+                                text = suggestion.content.nip05 ?: "",
+                                fontWeight = FontWeight.W400,
+                                fontSize = 12.sp,
+                                lineHeight = 16.sp,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                color = Color(0xFF666666)
+                            )
+                        }
+                        PrimalOutlinedButton(
+                            onClick = {
+                                eventPublisher(
+                                    CreateContract.UiEvent.ToggleFollowEvent(
+                                        groupName = group.key,
+                                        pubkey = suggestion.pubkey
+                                    )
+                                )
+                            },
+                            modifier = Modifier
+                                .defaultMinSize(minWidth = 92.dp)
+                                .height(36.dp)
+                        ) {
+                            val text =
+                                if (state.recommendedFollows.first { it.pubkey == suggestion.pubkey && it.groupName == suggestion.groupName }.isCurrentUserFollowing) "Unfollow" else "Follow"
+                            Text(text)
+                        }
                     }
                 }
             }
