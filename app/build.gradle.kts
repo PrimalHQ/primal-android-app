23d95f60743177b95ea23836753d9f82804094eb
Aleksandar Ilic <aleksandar@appollo41.com>
2023-09-19 12:59:06 +0200

    Primal 0.16.4 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 37
-        versionName = "0.16.3"
+        versionCode = 36
+        versionName = "0.16.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
