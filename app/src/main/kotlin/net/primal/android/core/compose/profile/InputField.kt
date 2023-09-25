65ffa6c406a295afef6e7fec9b9db318a5dc4928
Nikola Lukovic <nikola.lukovic@live.com>
2023-09-25 14:26:17 +0200

    Extracted create account components to core/compose/profile

---
+++ app/src/main/kotlin/net/primal/android/core/compose/profile/InputField.kt
@@ -0,0 +1,102 @@
+package net.primal.android.core.compose.profile
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import net.primal.android.theme.AppTheme
+
+@Composable
+fun InputField(
+    header: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    isRequired: Boolean = false,
+    prefix: String? = null,
+    isMultiline: Boolean = false
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 32.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = header,
+                fontWeight = FontWeight.W400,
+                fontSize = 16.sp,
+                color = AppTheme.extraColorScheme.onSurfaceVariantAlt4
+            )
+            if (isRequired) {
+                Text(buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Red, fontWeight = FontWeight.W400, fontSize = 16.sp
+                        )
+                    ) {
+                        append("*")
+                    }
+                    withStyle(
+                        style = SpanStyle(
+                            color = AppTheme.extraColorScheme.onSurfaceVariantAlt4,
+                            fontWeight = FontWeight.W400,
+                            fontSize = 16.sp
+                        )
+                    ) {
+                        append(" required")
+                    }
+                })
+            }
+        }
+        Spacer(modifier = Modifier.height(4.dp))
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Color(0xFF181818),
+                unfocusedBorderColor = Color.Transparent,
+                focusedBorderColor = Color.Transparent,
+            ),
+            shape = RoundedCornerShape(size = 12.dp),
+            singleLine = !isMultiline,
+            minLines = if (isMultiline) 6 else 0,
+            value = value,
+            onValueChange = onValueChange,
+            textStyle = AppTheme.typography.bodySmall,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            leadingIcon = if (prefix != null) {
+                {
+                    Text(
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterHorizontally)
+                            .padding(bottom = 6.dp), // this is stupid
+                        text = prefix,
+                        fontWeight = FontWeight.W500,
+                        fontSize = 18.sp,
+                        color = AppTheme.extraColorScheme.onSurfaceVariantAlt4
+                    )
+                }
+            } else null,
+        )
+    }
+}
