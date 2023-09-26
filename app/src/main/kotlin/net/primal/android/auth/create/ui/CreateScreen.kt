401bc13fff268088ab502ad0047561840874aa8b
Aleksandar Ilic <aleksandar@appollo41.com>
2023-09-26 15:01:39 +0200

    Remove navigationBarsPadding from CreateContent in create account

--- app/src/main/kotlin/net/primal/android/auth/create/ui/CreateScreen.kt
+++ app/src/main/kotlin/net/primal/android/auth/create/ui/CreateScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -160,7 +159,6 @@ fun CreateContent(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .navigationBarsPadding()
             .imePadding()
             .padding(paddingValues = paddingValues),
         horizontalAlignment = Alignment.CenterHorizontally
