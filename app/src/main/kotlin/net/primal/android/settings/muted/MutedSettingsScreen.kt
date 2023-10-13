48e52be35111bc1b3fc10b6fb14c15a04901b7d9
Nikola Lukovic <nikola.lukovic@live.com>
2023-10-13 14:07:22 +0200

    Implemented listing of muted users

--- app/src/main/kotlin/net/primal/android/settings/muted/MutedSettingsScreen.kt
+++ app/src/main/kotlin/net/primal/android/settings/muted/MutedSettingsScreen.kt
@@ -1,11 +1,21 @@
 package net.primal.android.settings.muted
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,14 +23,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import net.primal.android.R
 import net.primal.android.core.compose.PrimalTopAppBar
+import net.primal.android.core.compose.button.PrimalOutlinedButton
 import net.primal.android.core.compose.icons.PrimalIcons
 import net.primal.android.core.compose.icons.primaliconpack.ArrowBack
 import net.primal.android.settings.muted.model.MutedUser
+import net.primal.android.theme.AppTheme
 import net.primal.android.theme.PrimalTheme
 
 @Composable
@@ -63,8 +83,15 @@ fun MutedSettingsScreen(
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.Start
             ) {
-                items(state.mutelist) { item ->
+                itemsIndexed(state.mutelist) { index, item ->
                     MutedUserItem(item = item, eventPublisher = eventPublisher)
+                    if (index < state.mutelist.lastIndex) {
+                        Divider(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(1.dp)
+                        )
+                    }
                 }
             }
         }
@@ -76,7 +103,62 @@ fun MutedUserItem(
     item: MutedUser,
     eventPublisher: (MutedSettingsContract.UiEvent) -> Unit
 ) {
-    Text(text = item.name)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (item.avatarUrl == null) {
+            Image(
+                modifier = Modifier.size(36.dp),
+                painter = painterResource(id = R.drawable.default_avatar),
+                contentDescription = null
+            )
+        } else {
+            val model = ImageRequest.Builder(LocalContext.current).data(item.avatarUrl).build()
+            AsyncImage(
+                modifier = Modifier
+                    .size(36.dp)
+                    .clip(CircleShape),
+                model = model,
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(
+            modifier = Modifier.weight(1.0f),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = item.name,
+                fontWeight = FontWeight.W700,
+                fontSize = 16.sp,
+                lineHeight = 16.sp
+            )
+            Text(
+                text = item.nip05InternetIdentifier ?: "",
+                fontWeight = FontWeight.W400,
+                fontSize = 14.sp,
+                lineHeight = 14.sp,
+                color = AppTheme.extraColorScheme.onSurfaceVariantAlt4
+            )
+        }
+        PrimalOutlinedButton(
+            modifier = Modifier.height(32.dp),
+            onClick = {
+
+        }) {
+            Text(
+                text = "Unmute",
+                fontWeight = FontWeight.W500,
+                fontSize = 12.sp
+            )
+        }
+    }
 }
 
 @Preview
@@ -89,7 +171,7 @@ fun PreviewMutedScreen() {
                     MutedUser(
                         pubkey = "pubkey",
                         name = "username",
-                        avatarUrl = "avatarUrl",
+                        avatarUrl = null,
                         nip05InternetIdentifier = "nip05"
                     ),
                     MutedUser(
@@ -101,13 +183,13 @@ fun PreviewMutedScreen() {
                     MutedUser(
                         pubkey = "pubkey",
                         name = "username",
-                        avatarUrl = "avatarUrl",
+                        avatarUrl = null,
                         nip05InternetIdentifier = "nip05"
                     ),
                     MutedUser(
                         pubkey = "pubkey",
                         name = "username",
-                        avatarUrl = "avatarUrl",
+                        avatarUrl = null,
                         nip05InternetIdentifier = "nip05"
                     ), MutedUser(
                         pubkey = "pubkey",
