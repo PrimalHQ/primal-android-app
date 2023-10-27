682f6a2d296641359cd340d6a40cd2278627f0ec
Aleksandar Ilic <aleksandar@appollo41.com>
2023-10-27 16:46:11 +0200

    Implement sorting muted accounts by displayName

--- app/src/main/kotlin/net/primal/android/settings/muted/db/MutedUserDao.kt
+++ app/src/main/kotlin/net/primal/android/settings/muted/db/MutedUserDao.kt
@@ -10,7 +10,13 @@ interface MutedUserDao {
     @Upsert
     fun upsertAll(data: Set<MutedUserData>)
 
-    @Query("SELECT * FROM MutedUserData INNER JOIN ProfileData ON MutedUserData.userId = ProfileData.ownerId")
+    @Query(
+        """
+        SELECT * FROM MutedUserData 
+        INNER JOIN ProfileData ON MutedUserData.userId = ProfileData.ownerId
+        ORDER BY ProfileData.displayName ASC
+        """
+    )
     fun observeMutedUsers(): Flow<List<MutedUser>>
 
     @Query("SELECT EXISTS(SELECT * FROM MutedUserData WHERE userId = :pubkey)")
