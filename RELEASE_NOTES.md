b974a9318fcd3fde87b1695ce011bde8260ebd45
Aleksandar Ilic <aleksandar@appollo41.com>
2023-10-27 19:07:03 +0200

    Primal 0.20 release

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,5 +1,3 @@
 ## Changes
 
-- Implement Feed settings screen;
-- Implement Mute User feature;
-- Minor cosmetics changes on settings screens;
+- Direct messages
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 46
-        versionName = "0.19.0"
+        versionCode = 47
+        versionName = "0.20.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
