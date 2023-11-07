130dbcf72e4912d47603e0c1af5436d742ce3618
Aleksandar Ilic <aleksandar@appollo41.com>
2023-11-07 16:50:30 +0100

    Implement new UI in RepostOrQuoteBottomSheet

--- app/src/main/kotlin/net/primal/android/core/compose/feed/RepostOrQuoteBottomSheet.kt
+++ app/src/main/kotlin/net/primal/android/core/compose/feed/RepostOrQuoteBottomSheet.kt
@@ -1,19 +1,23 @@
 package net.primal.android.core.compose.feed
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import net.primal.android.R
 import net.primal.android.core.compose.AdjustSystemBarColors
+import net.primal.android.core.compose.IconText
+import net.primal.android.core.compose.button.PrimalFilledButton
 import net.primal.android.core.compose.icons.PrimalIcons
 import net.primal.android.core.compose.icons.primaliconpack.Quote
 import net.primal.android.core.compose.icons.primaliconpack.Repost
@@ -32,38 +36,57 @@ fun RepostOrQuoteBottomSheet(
         tonalElevation = 0.dp,
         onDismissRequest = onDismiss,
     ) {
-        val listItemColors = ListItemDefaults.colors(
-            containerColor = AppTheme.extraColorScheme.surfaceVariantAlt2,
-        )
-        ListItem(
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
-                .clickable {
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            ActionButton(
+                text = stringResource(id = R.string.post_repost_button_confirmation),
+                leadingIcon = PrimalIcons.Repost,
+                onClick = {
                     onDismiss()
                     onRepostClick()
-                },
-            colors = listItemColors,
-            leadingContent = {
-                Icon(imageVector = PrimalIcons.Repost, contentDescription = null)
-            },
-            headlineContent = {
-                Text(text = stringResource(id = R.string.post_repost_button_confirmation))
-            }
-        )
-        ListItem(
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
-                .clickable {
+                }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            ActionButton(
+                text = stringResource(id = R.string.post_quote_button_confirmation),
+                leadingIcon = PrimalIcons.Quote,
+                onClick = {
                     onDismiss()
                     onPostQuoteClick()
                 },
-            colors = listItemColors,
-            leadingContent = {
-                Icon(imageVector = PrimalIcons.Quote, contentDescription = null)
-            },
-            headlineContent = {
-                Text(text = stringResource(id = R.string.post_quote_button_confirmation))
-            }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+private fun ActionButton(
+    text: String,
+    leadingIcon: ImageVector,
+    onClick: () -> Unit,
+) {
+    PrimalFilledButton(
+        modifier = Modifier
+            .width(240.dp)
+            .height(56.dp),
+        containerColor = AppTheme.extraColorScheme.surfaceVariantAlt1,
+        contentColor = AppTheme.colorScheme.onSurface,
+        textStyle = AppTheme.typography.titleMedium.copy(
+            fontSize = 18.sp,
+        ),
+        onClick = onClick,
+    ) {
+        IconText(
+            text = text,
+            leadingIcon = leadingIcon,
+            leadingIconSize = 32.sp,
         )
     }
 }
