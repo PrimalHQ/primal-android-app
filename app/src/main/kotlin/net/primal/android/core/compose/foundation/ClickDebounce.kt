48d11b45fcfe5031dd6a644c25bcc1f99498bbcb
Aleksandar Ilic <aleksandar@appollo41.com>
2024-01-16 14:15:09 +0100

    Increase click debounce to 700 millis

--- app/src/main/kotlin/net/primal/android/core/compose/foundation/ClickDebounce.kt
+++ app/src/main/kotlin/net/primal/android/core/compose/foundation/ClickDebounce.kt
@@ -1,6 +1,6 @@
 package net.primal.android.core.compose.foundation
 
-class ClickDebounce(private val timeoutMillis: Long = 500L) {
+class ClickDebounce(private val timeoutMillis: Long = 700L) {
     private val now: Long get() = System.currentTimeMillis()
 
     private var lastEventTimeInMillis: Long = 0
