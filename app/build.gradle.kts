ef49f4e37e969f5ac715761570f4f8638dde4ce5
Aleksandar Ilic <aleksandar@appollo41.com>
2024-01-24 16:12:33 +0100

    Primal 0.90.2 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 80
-        versionName = "0.90.1"
+        versionCode = 81
+        versionName = "0.90.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
--- app/src/main/github/release-notes/github.md
+++ app/src/main/github/release-notes/github.md
@@ -1,2 +1,4 @@
 ## Features
-- Primal Wallet beta release;
+- Fixed unable to zap with Primal wallet and empty wallet transactions history on some devices;
+- Fixed issues when creating lightning invoice;
+- Added nostr lightning address to wallet settings;
