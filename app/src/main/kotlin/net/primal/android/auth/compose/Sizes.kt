7c5ab6cc58f4c319e2dbcd76cd8ebf9fee01d35a
Aleksandar Ilic <aleksandar@appollo41.com>
2024-03-06 16:46:19 +0100

    Implement new login screen

---
+++ app/src/main/kotlin/net/primal/android/auth/compose/Sizes.kt
@@ -0,0 +1,5 @@
+package net.primal.android.auth.compose
+
+const val MAX_COMPONENT_WIDTH = 320
+const val ONE_HALF = 0.5f
+const val TWO_FIFTHS = 0.4f
--- app/src/main/kotlin/net/primal/android/auth/welcome/WelcomeScreen.kt
+++ app/src/main/kotlin/net/primal/android/auth/welcome/WelcomeScreen.kt
@@ -18,23 +18,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlin.math.roundToInt
 import net.primal.android.R
-import net.primal.android.core.compose.ApplySystemBarColors
+import net.primal.android.auth.compose.MAX_COMPONENT_WIDTH
+import net.primal.android.auth.compose.ONE_HALF
+import net.primal.android.auth.compose.OnboardingButton
+import net.primal.android.auth.compose.TWO_FIFTHS
 import net.primal.android.core.compose.ToSAndPrivacyPolicyText
 import net.primal.android.core.compose.UiDensityMode
-import net.primal.android.core.compose.button.PrimalFilledButton
 import net.primal.android.core.compose.detectUiDensityModeFromMaxHeight
 import net.primal.android.theme.AppTheme
 import net.primal.android.theme.PrimalTheme
@@ -42,18 +38,14 @@ import net.primal.android.theme.domain.PrimalTheme
 
 @Composable
 fun WelcomeScreen(onSignInClick: () -> Unit, onCreateAccountClick: () -> Unit) {
-    ApplySystemBarColors(
-        statusBarColor = Color.Transparent,
-        navigationBarColor = Color.Transparent,
-    )
     BoxWithConstraints {
         val uiMode = this.maxHeight.detectUiDensityModeFromMaxHeight()
 
         Image(
             modifier = Modifier.fillMaxSize(),
-            imageVector = ImageVector.vectorResource(id = R.drawable.onboarding_bg_full),
-            contentScale = ContentScale.FillHeight,
-            alignment = OnboardingAlignment(fullXOffset = 0.0f),
+            painter = painterResource(id = R.drawable.onboarding_spot1),
+            contentScale = ContentScale.FillBounds,
+            alignment = Alignment.Center,
             contentDescription = null,
         )
 
@@ -109,14 +101,14 @@ fun WelcomeScreen(onSignInClick: () -> Unit, onCreateAccountClick: () -> Unit) {
                     ),
                 )
 
-                WelcomeButton(
+                OnboardingButton(
                     text = stringResource(id = R.string.welcome_sign_in_button_title),
                     onClick = onSignInClick,
                 )
 
                 Spacer(modifier = Modifier.height(8.dp))
 
-                WelcomeButton(
+                OnboardingButton(
                     text = stringResource(id = R.string.welcome_create_account_button_title),
                     onClick = onCreateAccountClick,
                 )
@@ -135,36 +127,6 @@ fun WelcomeScreen(onSignInClick: () -> Unit, onCreateAccountClick: () -> Unit) {
         }
     }
 }
-
-class OnboardingAlignment(var fullXOffset: Float = 0.0f) : Alignment {
-    override fun align(
-        size: IntSize,
-        space: IntSize,
-        layoutDirection: LayoutDirection,
-    ): IntOffset {
-        val shiftedX = (fullXOffset * (size.width - space.width)) * -1f
-        val centerY = (space.height - size.height).toFloat()
-        return IntOffset(shiftedX.roundToInt(), centerY.roundToInt())
-    }
-}
-
-@Composable
-private fun WelcomeButton(text: String, onClick: () -> Unit) {
-    PrimalFilledButton(
-        modifier = Modifier
-            .widthIn(240.dp, MAX_COMPONENT_WIDTH.dp)
-            .fillMaxWidth(),
-        containerColor = Color.Black,
-        contentColor = Color.White,
-        onClick = onClick,
-    ) {
-        Text(text = text)
-    }
-}
-
-private const val MAX_COMPONENT_WIDTH = 320
-private const val ONE_HALF = 0.5f
-private const val TWO_FIFTHS = 0.4f
 
 @Preview
 @Composable
