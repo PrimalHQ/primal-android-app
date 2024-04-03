2d5b91f199c7148d4d0c287067b837be940bbd01
Aleksandar Ilic <aleksandar@appollo41.com>
2024-04-03 20:20:52 +0200

    Primal 0.97.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 109
-        versionName = "0.96.10"
+        versionCode = 110
+        versionName = "0.97.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
