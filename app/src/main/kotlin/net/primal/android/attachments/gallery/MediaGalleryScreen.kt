231fa61027a468ff2e0948eb7daf8fbe7dbcc85a
Aleksandar Ilic <aleksandar@appollo41.com>
2024-04-17 18:18:36 +0200

    Fix gallery video player controls goes over navigation bar

--- app/src/main/kotlin/net/primal/android/attachments/gallery/MediaGalleryScreen.kt
+++ app/src/main/kotlin/net/primal/android/attachments/gallery/MediaGalleryScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
@@ -416,7 +417,7 @@ fun VideoScreen(
         contentAlignment = Alignment.BottomEnd,
     ) {
         AndroidView(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize().navigationBarsPadding(),
             factory = {
                 PlayerView(it).apply {
                     player = exoPlayer
