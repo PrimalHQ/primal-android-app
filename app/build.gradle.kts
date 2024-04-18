15fc5959674aa0ab69ffa46f891ab284d5b652d7
Aleksandar Ilic <aleksandar@appollo41.com>
2024-04-18 19:34:11 +0200

    Primal 0.98.2 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 115
-        versionName = "0.98.1"
+        versionCode = 117
+        versionName = "0.98.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
--- app/src/main/github/release-notes/github.md
+++ app/src/main/github/release-notes/github.md
@@ -1,4 +1,6 @@
 ## Changes
+- Improvements with network screen.
+- Implemented Content Display settings.
 - New screen transitions.
 - Videos in the feed.
 - Videos in the media gallery.
