73fa54ae0c5959fee0c7fd72001558fe0cf53c29
Aleksandar Ilic <aleksandar@appollo41.com>
2024-05-22 18:26:29 +0200

    Primal 0.100.2 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 127
-        versionName = "0.100.1"
+        versionCode = 128
+        versionName = "0.100.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
--- app/src/main/github/release-notes/github.md
+++ app/src/main/github/release-notes/github.md
@@ -1,3 +1,4 @@
 ## Features
 - Video uploads;
+- Adjust note font size;
 - Add image tags to notes;
