7af67e1693dfe4ca65a9b9f63b9c47abf07a1c0d
Aleksandar Ilic <aleksandar@appollo41.com>
2024-07-06 00:50:44 +0200

    Primal 1.0.3 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 137
-        versionName = "1.0.2"
+        versionCode = 138
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
--- app/src/main/github/release-notes/github.md
+++ app/src/main/github/release-notes/github.md
@@ -2,6 +2,8 @@
 - Reordering feeds;
 - Progress while uploading media attachments.
 - Take photo on note editor and threads screens.
+- Caching service override feature.
+- Publishing events through caching service proxy.
 
 ## Fixes
 - Stability fixes;
