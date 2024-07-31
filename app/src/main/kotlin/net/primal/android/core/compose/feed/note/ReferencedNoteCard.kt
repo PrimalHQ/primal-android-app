ab2d4a64a2042fb251391c681d69c646fb9e0be4
Aleksandar Ilic <aleksandar@appollo41.com>
2024-07-31 10:28:03 +0200

    Add border stroke to referenced notes in feeds

--- app/src/main/kotlin/net/primal/android/core/compose/feed/note/ReferencedNoteCard.kt
+++ app/src/main/kotlin/net/primal/android/core/compose/feed/note/ReferencedNoteCard.kt
@@ -1,5 +1,6 @@
 package net.primal.android.core.compose.feed.note
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -8,12 +9,14 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import kotlin.time.Duration.Companion.minutes
+import net.primal.android.LocalContentDisplaySettings
 import net.primal.android.attachments.domain.CdnImage
 import net.primal.android.core.compose.feed.model.EventStatsUi
 import net.primal.android.core.compose.feed.model.FeedPostUi
@@ -23,6 +26,7 @@ import net.primal.android.core.compose.feed.note.events.MediaClickEvent
 import net.primal.android.theme.AppTheme
 import net.primal.android.theme.PrimalTheme
 import net.primal.android.theme.domain.PrimalTheme
+import net.primal.android.user.domain.ContentDisplaySettings
 
 @Composable
 fun ReferencedNoteCard(
@@ -45,6 +49,7 @@ fun ReferencedNoteCard(
                 onClick = { onPostClick?.invoke(data.postId) },
             ),
         colors = colors,
+        border = BorderStroke(width = 0.5.dp, color = AppTheme.colorScheme.outline),
     ) {
         FeedNoteHeader(
             modifier = Modifier
@@ -82,85 +87,93 @@ fun ReferencedNoteCard(
 @Preview
 @Composable
 fun PreviewReferencedPostListItemLight() {
-    PrimalTheme(primalTheme = PrimalTheme.Sunrise) {
-        ReferencedNoteCard(
-            data = FeedPostUi(
-                postId = "random",
-                repostId = "repostRandom",
-                repostAuthorId = "repostId",
-                repostAuthorName = "jack",
-                content = """
+    CompositionLocalProvider(
+        LocalContentDisplaySettings provides ContentDisplaySettings(),
+    ) {
+        PrimalTheme(primalTheme = PrimalTheme.Sunrise) {
+            ReferencedNoteCard(
+                data = FeedPostUi(
+                    postId = "random",
+                    repostId = "repostRandom",
+                    repostAuthorId = "repostId",
+                    repostAuthorName = "jack",
+                    content = """
                     Unfortunately the days of using pseudonyms in metaspace are numbered. #nostr 
 
                     It won't be long before non-trivial numbers of individuals and businesses 
                     have augmented reality HUDs that incorporate real-time facial recognition. 
                     Hiding behind a pseudonym will become a distant dream.
-                """.trimIndent(),
-                attachments = emptyList(),
-                authorId = "npubSomething",
-                authorName = "android_robots_from_space",
-                authorHandle = "user",
-                authorInternetIdentifier = "android@primal.net",
-                authorAvatarCdnImage = CdnImage("https://i.imgur.com/Z8dpmvc.png"),
-                timestamp = Instant.now().minus(30.minutes.inWholeMinutes, ChronoUnit.MINUTES),
-                nostrUris = emptyList(),
-                stats = EventStatsUi(
-                    repliesCount = 11,
-                    likesCount = 256,
-                    userLiked = true,
-                    repostsCount = 42,
-                    satsZapped = 555,
+                    """.trimIndent(),
+                    attachments = emptyList(),
+                    authorId = "npubSomething",
+                    authorName = "android_robots_from_space",
+                    authorHandle = "user",
+                    authorInternetIdentifier = "android@primal.net",
+                    authorAvatarCdnImage = CdnImage("https://i.imgur.com/Z8dpmvc.png"),
+                    timestamp = Instant.now().minus(30.minutes.inWholeMinutes, ChronoUnit.MINUTES),
+                    nostrUris = emptyList(),
+                    stats = EventStatsUi(
+                        repliesCount = 11,
+                        likesCount = 256,
+                        userLiked = true,
+                        repostsCount = 42,
+                        satsZapped = 555,
+                    ),
+                    hashtags = listOf("#nostr"),
+                    rawNostrEventJson = "",
+                    replyToAuthorHandle = "alex",
                 ),
-                hashtags = listOf("#nostr"),
-                rawNostrEventJson = "",
-                replyToAuthorHandle = "alex",
-            ),
-            onPostClick = {},
-            onArticleClick = {},
-            onMediaClick = {},
-        )
+                onPostClick = {},
+                onArticleClick = {},
+                onMediaClick = {},
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewReferencedPostListItemDark() {
-    PrimalTheme(primalTheme = PrimalTheme.Sunset) {
-        ReferencedNoteCard(
-            data = FeedPostUi(
-                postId = "random",
-                repostId = "repostRandom",
-                repostAuthorId = "repostId",
-                repostAuthorName = "jack",
-                content = """
+    CompositionLocalProvider(
+        LocalContentDisplaySettings provides ContentDisplaySettings(),
+    ) {
+        PrimalTheme(primalTheme = PrimalTheme.Sunset) {
+            ReferencedNoteCard(
+                data = FeedPostUi(
+                    postId = "random",
+                    repostId = "repostRandom",
+                    repostAuthorId = "repostId",
+                    repostAuthorName = "jack",
+                    content = """
                     Unfortunately the days of using pseudonyms in metaspace are numbered. #nostr
 
                     It won't be long before non-trivial numbers of individuals and businesses 
                     have augmented reality HUDs that incorporate real-time facial recognition. 
                     Hiding behind a pseudonym will become a distant dream.
-                """.trimIndent(),
-                attachments = emptyList(),
-                authorId = "npubSomething",
-                authorName = "android",
-                authorHandle = "user",
-                authorInternetIdentifier = "android@primal.net",
-                authorAvatarCdnImage = CdnImage("https://i.imgur.com/Z8dpmvc.png"),
-                timestamp = Instant.now().minus(30.minutes.inWholeMinutes, ChronoUnit.MINUTES),
-                nostrUris = emptyList(),
-                stats = EventStatsUi(
-                    repliesCount = 11,
-                    userReplied = true,
-                    likesCount = 256,
-                    repostsCount = 42,
-                    satsZapped = 555,
+                    """.trimIndent(),
+                    attachments = emptyList(),
+                    authorId = "npubSomething",
+                    authorName = "android",
+                    authorHandle = "user",
+                    authorInternetIdentifier = "android@primal.net",
+                    authorAvatarCdnImage = CdnImage("https://i.imgur.com/Z8dpmvc.png"),
+                    timestamp = Instant.now().minus(30.minutes.inWholeMinutes, ChronoUnit.MINUTES),
+                    nostrUris = emptyList(),
+                    stats = EventStatsUi(
+                        repliesCount = 11,
+                        userReplied = true,
+                        likesCount = 256,
+                        repostsCount = 42,
+                        satsZapped = 555,
+                    ),
+                    hashtags = listOf("#nostr"),
+                    rawNostrEventJson = "",
+                    replyToAuthorHandle = null,
                 ),
-                hashtags = listOf("#nostr"),
-                rawNostrEventJson = "",
-                replyToAuthorHandle = null,
-            ),
-            onPostClick = {},
-            onArticleClick = {},
-            onMediaClick = {},
-        )
+                onPostClick = {},
+                onArticleClick = {},
+                onMediaClick = {},
+            )
+        }
     }
 }
