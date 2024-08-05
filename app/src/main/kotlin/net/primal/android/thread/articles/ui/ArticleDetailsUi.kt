259703cc7e64a1f37af517f3807c4fa775846fe5
Aleksandar Ilic <aleksandar@appollo41.com>
2024-08-05 23:39:16 +0200

    Implement zap options on ArticleDetailsScreen

---
+++ app/src/main/kotlin/net/primal/android/thread/articles/ui/ArticleDetailsUi.kt
@@ -0,0 +1,44 @@
+package net.primal.android.thread.articles.ui
+
+import java.time.Instant
+import net.primal.android.articles.db.Article
+import net.primal.android.attachments.domain.CdnImage
+import net.primal.android.core.compose.feed.model.EventStatsUi
+import net.primal.android.core.utils.asEllipsizedNpub
+import net.primal.android.core.utils.authorNameUiFriendly
+
+data class ArticleDetailsUi(
+    val eventId: String,
+    val articleId: String,
+    val authorId: String,
+    val title: String,
+    val content: String,
+    val summary: String?,
+    val publishedAt: Instant,
+    val authorDisplayName: String,
+    val authorInternetIdentifier: String? = null,
+    val authorAvatarCdnImage: CdnImage? = null,
+    val coverImageCdnImage: CdnImage? = null,
+    val readingTimeInMinutes: Int? = null,
+    val hashtags: List<String> = emptyList(),
+    val eventStatsUi: EventStatsUi = EventStatsUi(),
+)
+
+fun Article.mapAsArticleDetailsUi(): ArticleDetailsUi {
+    return ArticleDetailsUi(
+        eventId = this.data.eventId,
+        articleId = this.data.articleId,
+        authorId = this.data.authorId,
+        title = this.data.title,
+        content = this.data.content,
+        summary = this.data.summary,
+        publishedAt = Instant.ofEpochSecond(this.data.publishedAt),
+        authorDisplayName = this.author?.authorNameUiFriendly() ?: this.data.authorId.asEllipsizedNpub(),
+        authorInternetIdentifier = this.author?.internetIdentifier,
+        authorAvatarCdnImage = this.author?.avatarCdnImage,
+        coverImageCdnImage = this.data.imageCdnImage ?: this.author?.avatarCdnImage,
+        readingTimeInMinutes = ((this.data.wordsCount ?: 1) / 200) + 1,
+        hashtags = this.data.hashtags,
+        eventStatsUi = EventStatsUi.from(eventStats = this.eventStats, userStats = null),
+    )
+}
