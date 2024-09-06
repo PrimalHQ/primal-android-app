959eefe1d1f05731f84a5830b0d4f2654ca120f9
Marko Kocic <marko@appollo41.com>
2024-09-06 13:28:41 +0200

    Implement Instant utils for date/datetime formatting

--- app/src/main/kotlin/net/primal/android/core/utils/TimeUtils.kt
+++ app/src/main/kotlin/net/primal/android/core/utils/TimeUtils.kt
@@ -1,9 +1,35 @@
 package net.primal.android.core.utils
 
 import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.*
 import kotlin.time.Duration
 
 fun Instant?.isOlderThan(duration: Duration): Boolean {
     if (this == null) return true
     return this < Instant.now().minusSeconds(duration.inWholeSeconds)
+}
+
+fun Instant.formatToDefaultDateTimeFormat(dateTimeStyle: FormatStyle): String {
+    val zoneId: ZoneId = ZoneId.systemDefault()
+    val locale: Locale = Locale.getDefault()
+
+    val formatter: DateTimeFormatter = DateTimeFormatter
+        .ofLocalizedDateTime(dateTimeStyle)
+        .withLocale(locale)
+
+    return formatter.format(this.atZone(zoneId))
+}
+
+fun Instant.formatToDefaultDateFormat(dateStyle: FormatStyle): String {
+    val zoneId: ZoneId = ZoneId.systemDefault()
+    val locale: Locale = Locale.getDefault()
+
+    val formatter: DateTimeFormatter = DateTimeFormatter
+        .ofLocalizedDate(dateStyle)
+        .withLocale(locale)
+
+    return formatter.format(this.atZone(zoneId))
 }
