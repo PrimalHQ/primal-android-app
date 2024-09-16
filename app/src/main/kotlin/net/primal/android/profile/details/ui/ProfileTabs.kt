72e9cf4bccbc11600ecd6f8ba2c92150c7ba32fe
Marko Kocic <marko@appollo41.com>
2024-09-16 19:53:36 +0200

    Refactor ProfileTabs to work with PagerState

--- app/src/main/kotlin/net/primal/android/profile/details/ui/ProfileTabs.kt
+++ app/src/main/kotlin/net/primal/android/profile/details/ui/ProfileTabs.kt
@@ -3,15 +3,20 @@ package net.primal.android.profile.details.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,14 +25,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.text.NumberFormat
+import kotlinx.coroutines.launch
 import net.primal.android.R
 import net.primal.android.core.utils.shortened
 import net.primal.android.profile.domain.ProfileFeedSpec
 import net.primal.android.theme.AppTheme
+import timber.log.Timber
 
 @Composable
 fun ProfileTabs(
-    feedFeedSpec: ProfileFeedSpec,
     notesCount: Int?,
     onNotesCountClick: () -> Unit,
     repliesCount: Int?,
@@ -37,26 +43,20 @@ fun ProfileTabs(
     mediaCount: Int?,
     onMediaCountClick: () -> Unit,
     modifier: Modifier = Modifier,
+    pagerState: PagerState = rememberPagerState { 4 },
     placeholderText: String = "-",
 ) {
-    var tabIndex by remember {
-        mutableIntStateOf(
-            when (feedFeedSpec) {
-                ProfileFeedSpec.AuthoredNotes -> 0
-                ProfileFeedSpec.AuthoredReplies -> 1
-            },
-        )
-    }
+    val scope = rememberCoroutineScope()
 
     TabRow(
         modifier = modifier,
-        selectedTabIndex = tabIndex,
+        selectedTabIndex = pagerState.currentPage,
         containerColor = Color.Transparent,
         divider = { },
         indicator = { tabPositions ->
-            if (tabIndex < tabPositions.size) {
+            if (pagerState.currentPage < tabPositions.size) {
                 TabRowDefaults.SecondaryIndicator(
-                    modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
+                    modifier = Modifier.tabIndicatorOffset(tabPositions[pagerState.currentPage]),
                     height = 4.dp,
                     color = AppTheme.colorScheme.tertiary,
                 )
@@ -65,10 +65,10 @@ fun ProfileTabs(
     ) {
         CustomTab(
             modifier = Modifier.fillMaxWidth(),
-            selected = tabIndex == 0,
+            selected = pagerState.currentPage == 0,
             onClick = {
                 onNotesCountClick()
-                tabIndex = 0
+                scope.launch { pagerState.animateScrollToPage(0) }
             },
             text = notesCount?.asTabText() ?: placeholderText,
             label = stringResource(id = R.string.profile_notes_stat),
@@ -76,25 +76,31 @@ fun ProfileTabs(
 
         CustomTab(
             modifier = Modifier.fillMaxWidth(),
-            selected = tabIndex == 1,
+            selected = pagerState.currentPage == 1,
             onClick = {
                 onRepliesCountClick()
-                tabIndex = 1
+                scope.launch { pagerState.animateScrollToPage(1) }
             },
             text = repliesCount?.asTabText() ?: placeholderText,
             label = stringResource(id = R.string.profile_replies_stat),
         )
 
         CustomTab(
-            selected = false,
-            onClick = onReadsCountClick,
+            selected = pagerState.currentPage == 2,
+            onClick = {
+                onReadsCountClick()
+                scope.launch { pagerState.animateScrollToPage(2) }
+            },
             text = readsCount?.asTabText() ?: placeholderText,
             label = stringResource(id = R.string.profile_reads_stat),
         )
 
         CustomTab(
-            selected = false,
-            onClick = onMediaCountClick,
+            selected = pagerState.currentPage == 3,
+            onClick = {
+                onMediaCountClick()
+                scope.launch { pagerState.animateScrollToPage(3) }
+            },
             text = mediaCount?.asTabText() ?: placeholderText,
             label = stringResource(id = R.string.profile_media_stat),
         )
