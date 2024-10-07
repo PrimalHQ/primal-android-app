e10fcb3b7744d15eab6de933c40209f10becffa3
Aleksandar Ilic <aleksandar@appollo41.com>
2024-10-07 18:19:38 +0200

    Primal 1.7.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 155
-        versionName = "1.7.0"
+        versionCode = 156
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
