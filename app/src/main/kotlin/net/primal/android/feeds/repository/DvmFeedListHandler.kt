25fb09348878d857890bb11ca0294b3d974f94d1
Aleksandar Ilic <aleksandar@appollo41.com>
2024-10-19 12:54:58 +0200

    Fix dvm stats not updating properly

--- app/src/main/kotlin/net/primal/android/feeds/repository/DvmFeedListHandler.kt
+++ app/src/main/kotlin/net/primal/android/feeds/repository/DvmFeedListHandler.kt
@@ -7,8 +7,8 @@ import kotlinx.coroutines.launch
 import net.primal.android.core.ext.asMapByKey
 import net.primal.android.feeds.domain.FeedSpecKind
 import net.primal.android.feeds.dvm.ui.DvmFeedUi
-import net.primal.android.stats.repository.EventRepository
 import net.primal.android.profile.repository.ProfileRepository
+import net.primal.android.stats.repository.EventRepository
 
 class DvmFeedListHandler @Inject constructor(
     private val feedsRepository: FeedsRepository,
@@ -25,11 +25,11 @@ class DvmFeedListHandler @Inject constructor(
         val dvmFeeds = feedsRepository.fetchRecommendedDvmFeeds(userId = userId, specKind = specKind)
         val dvmIds = dvmFeeds.map { it.eventId }
         val stats = eventRepository.observeEventStats(eventIds = dvmIds).first().asMapByKey { it.eventId }
-        val userStats = eventRepository.observeUserEventStatus(userId = userId, eventIds = dvmIds).first().asMapByKey {
-            it.eventId
-        }
+        val userStats = eventRepository.observeUserEventStatus(userId = userId, eventIds = dvmIds)
+            .first()
+            .asMapByKey { it.eventId }
 
-        val feeds = dvmFeeds.map { dvmFeed ->
+        var feeds = dvmFeeds.map { dvmFeed ->
             DvmFeedUi(
                 data = dvmFeed,
                 userLiked = userStats[dvmFeed.eventId]?.liked,
@@ -43,40 +43,35 @@ class DvmFeedListHandler @Inject constructor(
         }
         update(feeds)
 
-        scope.launch { observeDvmStats(feeds = feeds, update = update) }
-        scope.launch { observeUserDvmStats(userId = userId, feeds = feeds, update = update) }
-    }
-
-    private suspend fun observeDvmStats(feeds: List<DvmFeedUi>, update: (List<DvmFeedUi>) -> Unit) {
-        eventRepository.observeEventStats(eventIds = feeds.map { it.data.eventId }).collect { eventStats ->
-            val dvmStats = eventStats.asMapByKey { it.eventId }
-            val newList = feeds.map {
-                it.copy(
-                    totalLikes = dvmStats[it.data.eventId]?.likes,
-                    totalSatsZapped = dvmStats[it.data.eventId]?.satsZapped,
-                )
+        scope.launch {
+            eventRepository.observeUserEventStatus(
+                userId = userId,
+                eventIds = feeds.map {
+                    it.data.eventId
+                },
+            ).collect { eventStats ->
+                val dvmStats = eventStats.asMapByKey { it.eventId }
+                feeds = feeds.map {
+                    it.copy(
+                        userLiked = dvmStats[it.data.eventId]?.liked,
+                        userZapped = dvmStats[it.data.eventId]?.zapped,
+                    )
+                }
+                update(feeds)
             }
-            update(newList)
         }
-    }
 
-    private suspend fun observeUserDvmStats(
-        userId: String,
-        feeds: List<DvmFeedUi>,
-        update: (List<DvmFeedUi>) -> Unit,
-    ) {
-        eventRepository.observeUserEventStatus(
-            userId = userId,
-            eventIds = feeds.map { it.data.eventId },
-        ).collect { eventStats ->
-            val dvmStats = eventStats.asMapByKey { it.eventId }
-            val newList = feeds.map {
-                it.copy(
-                    userLiked = dvmStats[it.data.eventId]?.liked,
-                    userZapped = dvmStats[it.data.eventId]?.zapped,
-                )
+        scope.launch {
+            eventRepository.observeEventStats(eventIds = feeds.map { it.data.eventId }).collect { eventStats ->
+                val dvmStats = eventStats.asMapByKey { it.eventId }
+                feeds = feeds.map {
+                    it.copy(
+                        totalLikes = dvmStats[it.data.eventId]?.likes,
+                        totalSatsZapped = dvmStats[it.data.eventId]?.satsZapped,
+                    )
+                }
+                update(feeds)
             }
-            update(newList)
         }
     }
 }
