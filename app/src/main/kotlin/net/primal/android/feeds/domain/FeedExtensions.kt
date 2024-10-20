9b7f2d400f60764a7c55aa924df93ff7babe9155
Aleksandar Ilic <aleksandar@appollo41.com>
2024-10-20 19:29:54 +0200

    Improve feed title and description resolving from feed spec

--- app/src/main/kotlin/net/primal/android/feeds/domain/FeedExtensions.kt
+++ app/src/main/kotlin/net/primal/android/feeds/domain/FeedExtensions.kt
@@ -1,5 +1,8 @@
 package net.primal.android.feeds.domain
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import net.primal.android.R
 import net.primal.android.core.utils.ellipsizeMiddle
 import net.primal.android.crypto.hexToNpubHrp
 
@@ -97,45 +100,58 @@ fun String.resolveFeedSpecKind(): FeedSpecKind? {
     }
 }
 
-fun String.resolveDefaultTitle(): String =
+@Composable
+fun String.resolveDefaultTitle(profileName: String? = null): String =
     runCatching {
         val topic = extractTopicFromFeedSpec()?.substringAfter("#")
         val advancedQuery = extractAdvancedSearchQuery()
         val simpleQuery = extractSimpleSearchQuery()
         val pubkey = extractPubkeyFromFeedSpec()
         when {
-            topic != null -> "Topic: $topic"
+            topic != null -> {
+                stringResource(R.string.explore_feed_topic_feed_title, topic)
+            }
 
-            isAdvancedSearchFeedSpec() && advancedQuery != null -> "Search: $advancedQuery"
+            isAdvancedSearchFeedSpec() && advancedQuery != null -> {
+                stringResource(R.string.explore_feed_advanced_search_feed_title)
+            }
 
-            isSimpleSearchFeedSpec() && simpleQuery != null -> "Search: $simpleQuery"
+            isSimpleSearchFeedSpec() && simpleQuery != null -> {
+                stringResource(R.string.explore_feed_simple_search_feed_title, simpleQuery)
+            }
 
-            pubkey != null -> "User: ${pubkey.hexToNpubHrp().ellipsizeMiddle(size = 8)}"
+            pubkey != null -> {
+                val name = profileName ?: pubkey.hexToNpubHrp().ellipsizeMiddle(size = 8)
+                stringResource(R.string.explore_feed_user_feed_title, name)
+            }
 
             else -> ""
         }
     }.getOrDefault(defaultValue = "")
 
+@Composable
 fun String.resolveDefaultDescription(): String =
     runCatching {
         val topic = extractTopicFromFeedSpec()?.substringAfter("#")
         return when {
             topic != null -> if (isNotesFeedSpec()) {
-                "All notes tagged with $topic"
+                stringResource(R.string.explore_feed_topic_notes_feed_description, topic)
             } else if (isReadsFeedSpec()) {
-                "All reads tagged with $topic"
+                stringResource(R.string.explore_feed_topic_reads_feed_description, topic)
             } else {
-                "Tagged with $topic"
+                stringResource(R.string.explore_feed_topic_feed_description, topic)
             }
 
-            isSearchFeedSpec() -> "Primal Saved Search"
+            isSearchFeedSpec() -> {
+                stringResource(R.string.explore_feed_search_feed_description)
+            }
 
             isPubkeyFeedSpec() -> if (isNotesFeedSpec()) {
-                "Latest notes by user follows"
+                stringResource(R.string.explore_feed_user_notes_feed_description)
             } else if (isReadsFeedSpec()) {
-                "Latest articles by user follows"
+                stringResource(R.string.explore_feed_user_reads_feed_description)
             } else {
-                "Latest by user follows"
+                stringResource(R.string.explore_feed_user_feed_description)
             }
 
             else -> ""
