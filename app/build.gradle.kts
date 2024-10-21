d9dffba4aded3f87175f141d62477d5b7e4c1ac0
Aleksandar Ilic <aleksandar@appollo41.com>
2024-10-21 19:44:23 +0200

    Primal 1.8.3 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 161
-        versionName = "1.8.2"
+        versionCode = 162
+        versionName = "1.8.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
