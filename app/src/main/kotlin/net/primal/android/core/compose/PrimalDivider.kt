cea44a30bc365a405d7f5e44d379699e26ca214f
Marko Kocic <marko@appollo41.com>
2024-10-21 14:38:53 +0200

    Add color parameter to PrimalDivider

--- app/src/main/kotlin/net/primal/android/core/compose/PrimalDivider.kt
+++ app/src/main/kotlin/net/primal/android/core/compose/PrimalDivider.kt
@@ -3,15 +3,20 @@ package net.primal.android.core.compose
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import net.primal.android.theme.AppTheme
 
 @Composable
-fun PrimalDivider(modifier: Modifier = Modifier, thickness: Dp = 0.5.dp) {
+fun PrimalDivider(
+    modifier: Modifier = Modifier,
+    thickness: Dp = 0.5.dp,
+    color: Color = AppTheme.colorScheme.outline,
+) {
     HorizontalDivider(
         modifier = modifier,
         thickness = thickness,
-        color = AppTheme.colorScheme.outline,
+        color = color,
     )
 }
