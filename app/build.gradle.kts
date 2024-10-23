274c91b0a75fe24154da067986a9f44caca0f114
Aleksandar Ilic <aleksandar@appollo41.com>
2024-10-23 19:48:40 +0200

    Primal 1.8.6 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 164
-        versionName = "1.8.5"
+        versionCode = 165
+        versionName = "1.8.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
