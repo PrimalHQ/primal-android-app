17d12b7b6511812a90883fe66c41ff8e64ac7f2b
Marko Kocic <marko@appollo41.com>
2024-10-28 13:59:51 +0100

    Implement zapping profiles through NWC (#214)
    ---------
    Co-authored-by: Aleksandar Ilic <aleksandar@appollo41.com>

--- app/src/main/kotlin/net/primal/android/profile/details/ProfileDetailsContract.kt
+++ app/src/main/kotlin/net/primal/android/profile/details/ProfileDetailsContract.kt
@@ -2,6 +2,8 @@ package net.primal.android.profile.details
 
 import net.primal.android.core.compose.profile.model.ProfileDetailsUi
 import net.primal.android.core.compose.profile.model.ProfileStatsUi
+import net.primal.android.core.errors.UiError
+import net.primal.android.notes.feed.model.ZappingState
 import net.primal.android.profile.domain.ProfileFeedSpec
 import net.primal.android.profile.report.ReportType
 
@@ -24,6 +26,8 @@ interface ProfileDetailsContract {
             ProfileFeedSpec.AuthoredMedia,
         ),
         val error: ProfileError? = null,
+        val zapError: UiError? = null,
+        val zappingState: ZappingState = ZappingState(),
     ) {
         sealed class ProfileError {
             data class MissingRelaysConfiguration(val cause: Throwable) : ProfileError()
@@ -40,6 +44,7 @@ interface ProfileDetailsContract {
         data object ProfileUpdateFinished : SideEffect()
         data object ProfileFeedAdded : SideEffect()
         data object ProfileFeedRemoved : SideEffect()
+        data object ProfileZapSent : SideEffect()
     }
 
     sealed class UiEvent {
@@ -51,11 +56,19 @@ interface ProfileDetailsContract {
             val feedDescription: String,
         ) : UiEvent()
 
+        data class ZapProfile(
+            val profileId: String,
+            val profileLnUrlDecoded: String?,
+            val zapDescription: String? = null,
+            val zapAmount: ULong? = null,
+        ) : UiEvent()
+
         data class RemoveProfileFeedAction(val profileId: String) : UiEvent()
         data class MuteAction(val profileId: String) : UiEvent()
         data class UnmuteAction(val profileId: String) : UiEvent()
         data object RequestProfileUpdate : UiEvent()
         data class ReportAbuse(val type: ReportType, val profileId: String, val noteId: String? = null) : UiEvent()
         data object DismissError : UiEvent()
+        data object DismissZapError : UiEvent()
     }
 }
