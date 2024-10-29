a12a65e5163a1520810636d762e863f633709709
Marko Kocic <marko@appollo41.com>
2024-10-29 13:44:42 +0100

    Implement initial premium home screen (#218)

---
+++ app/src/main/kotlin/net/primal/android/premium/PremiumHomeViewModel.kt
@@ -0,0 +1,38 @@
+package net.primal.android.premium
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.getAndUpdate
+import kotlinx.coroutines.launch
+import net.primal.android.premium.PremiumHomeContract.UiEvent
+import net.primal.android.premium.PremiumHomeContract.UiState
+
+@HiltViewModel
+class PremiumHomeViewModel @Inject constructor() : ViewModel() {
+
+    private val _state = MutableStateFlow(UiState())
+    val state = _state.asStateFlow()
+    private fun setState(reducer: UiState.() -> UiState) = _state.getAndUpdate { it.reducer() }
+
+    private val events: MutableSharedFlow<UiEvent> = MutableSharedFlow()
+    fun setEvent(event: UiEvent) = viewModelScope.launch { events.emit(event) }
+
+    init {
+        observeEvents()
+    }
+
+    private fun observeEvents() =
+        viewModelScope.launch {
+            events.collect {
+                when (it) {
+                    is UiEvent.MoveToPremiumStage -> setState { copy(stage = it.stage) }
+                }
+            }
+        }
+}
