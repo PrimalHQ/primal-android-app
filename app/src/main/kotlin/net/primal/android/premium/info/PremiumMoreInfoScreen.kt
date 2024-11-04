3b79cef2d07f24b1fc0be39a6c3712f5a9a02cce
Marko Kocic <markokocic2000@gmail.com>
2024-11-04 17:43:01 +0100

    Auto-format code

--- app/src/main/kotlin/net/primal/android/premium/info/PremiumMoreInfoScreen.kt
+++ app/src/main/kotlin/net/primal/android/premium/info/PremiumMoreInfoScreen.kt
@@ -2,7 +2,6 @@ package net.primal.android.premium.info
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
