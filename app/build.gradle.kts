6500f69fdb68922530a92ba995e5d920193eb772
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-04 19:06:04 +0100

    Primal 2.0.5 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 172
-        versionName = "2.0.4"
+        versionCode = 173
+        versionName = "2.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
