11634bade8bd9f0ce717f657711b7c4a88b1e55f
Marko Kocic <marko@appollo41.com>
2024-11-05 11:29:50 +0100

    Implement premium promo code bottom sheet without api (#223)

--- app/src/main/kotlin/net/primal/android/premium/purchase/PremiumPurchaseContract.kt
+++ app/src/main/kotlin/net/primal/android/premium/purchase/PremiumPurchaseContract.kt
@@ -5,13 +5,12 @@ import net.primal.android.core.compose.profile.model.ProfileDetailsUi
 interface PremiumPurchaseContract {
     data class UiState(
         val profile: ProfileDetailsUi? = null,
+        val promoCodeValidity: Boolean? = null,
+        val isCheckingPromoCodeValidity: Boolean = false,
     )
 
     sealed class UiEvent {
         data class ApplyPromoCode(val promoCode: String) : UiEvent()
-    }
-
-    sealed class SideEffect {
-        data object InvalidPromoCode : SideEffect()
+        data object ClearPromoCodeValidity : UiEvent()
     }
 }
