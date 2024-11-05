5e2e38afc0d0af8d4248fd5af8689d06fa7d436d
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-05 21:52:24 +0100

    Quick fix to make buying premium scrollable

--- app/src/main/kotlin/net/primal/android/premium/purchase/PremiumPurchaseStage.kt
+++ app/src/main/kotlin/net/primal/android/premium/purchase/PremiumPurchaseStage.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -128,7 +130,8 @@ private fun PremiumPurchaseStage(
                 .navigationBarsPadding()
                 .fillMaxSize()
                 .padding(horizontal = 24.dp)
-                .padding(paddingValues),
+                .padding(paddingValues)
+                .verticalScroll(state = rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(24.dp, Alignment.CenterVertically),
         ) {
--- app/src/main/kotlin/net/primal/android/premium/ui/PremiumHomeStage.kt
+++ app/src/main/kotlin/net/primal/android/premium/ui/PremiumHomeStage.kt
@@ -8,17 +8,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -64,6 +66,10 @@ fun PremiumHomeStage(
                 navigationIcon = PrimalIcons.ArrowBack,
                 onNavigationIconClick = onClose,
                 showDivider = false,
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.Transparent,
+                    scrolledContainerColor = Color.Transparent,
+                ),
             )
         },
     ) { paddingValues ->
@@ -71,7 +77,7 @@ fun PremiumHomeStage(
             modifier = Modifier
                 .padding(16.dp)
                 .padding(paddingValues)
-                .fillMaxSize(),
+                .verticalScroll(state = rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Bottom),
         ) {
