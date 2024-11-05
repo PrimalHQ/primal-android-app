2020880bb479e87fdc8295d9138283d95f35c966
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-05 19:50:43 +0100

    Fix AOSP build to compile

--- app/src/aosp/kotlin/net/primal/android/wallet/store/AospBillingClient.kt
+++ app/src/aosp/kotlin/net/primal/android/wallet/store/AospBillingClient.kt
@@ -7,15 +7,22 @@ import kotlinx.coroutines.flow.SharedFlow
 import net.primal.android.wallet.store.domain.InAppProduct
 import net.primal.android.wallet.store.domain.SatsPurchase
 import net.primal.android.wallet.store.domain.SatsPurchaseQuote
+import net.primal.android.wallet.store.domain.SubscriptionProduct
+import net.primal.android.wallet.store.domain.SubscriptionPurchase
 
 @Singleton
 class AospBillingClient : PrimalBillingClient {
 
-    override val purchases: SharedFlow<SatsPurchase> = MutableSharedFlow()
+    override val satsPurchases: SharedFlow<SatsPurchase> = MutableSharedFlow()
+    override val subscriptionPurchases: SharedFlow<SubscriptionPurchase> = MutableSharedFlow()
 
     override val minSatsInAppProduct: InAppProduct? = null
+    override val subscriptionProducts: List<SubscriptionProduct> = emptyList()
 
     override suspend fun launchMinSatsBillingFlow(quote: SatsPurchaseQuote, activity: Activity) = Unit
+    override suspend fun launchSubscriptionBillingFlow(subscriptionProduct: SubscriptionProduct, activity: Activity) =
+        Unit
 
     override suspend fun refreshMinSatsInAppProduct() = Unit
+    override suspend fun refreshSubscriptionProducts() = Unit
 }
