d672b964430bdcf39485d332219a3243092e880d
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-06 19:40:34 +0100

    Implement handling play services not available

--- app/src/main/kotlin/net/primal/android/premium/buying/PremiumBuyingContract.kt
+++ app/src/main/kotlin/net/primal/android/premium/buying/PremiumBuyingContract.kt
@@ -6,6 +6,7 @@ import net.primal.android.wallet.store.domain.SubscriptionProduct
 
 interface PremiumBuyingContract {
     data class UiState(
+        val loading: Boolean = true,
         val subscriptions: List<SubscriptionProduct> = emptyList(),
         val stage: PremiumStage = PremiumStage.Home,
         val primalName: String? = null,
