3447aebd81fbba778a152d1e7f2ba88b17e3b6c7
Marko Kocic <marko@appollo41.com>
2024-11-11 13:17:53 +0100

    Implement premium media management screen (#230)
    ---------
    Approved-by: Aleksandar Ilic <aleksandar@appollo41.com>

---
+++ app/src/main/kotlin/net/primal/android/premium/manage/media/PremiumMediaManagementContract.kt
@@ -0,0 +1,16 @@
+package net.primal.android.premium.manage.media
+
+import net.primal.android.premium.manage.media.model.MediaUiItem
+
+interface PremiumMediaManagementContract {
+    data class UiState(
+        val usedStorageInBytes: Long? = null,
+        val maxStorageInBytes: Long? = null,
+        val imagesInBytes: Long? = null,
+        val videosInBytes: Long? = null,
+        val otherInBytes: Long? = null,
+        val mediaItems: List<MediaUiItem> = emptyList(),
+
+        val calculating: Boolean = true,
+    )
+}
