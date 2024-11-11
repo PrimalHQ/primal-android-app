a5362d84e19223156d2f96af64182041983894da
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-11 17:18:56 +0100

    Implement logic on Support Primal screen

--- app/src/main/kotlin/net/primal/android/premium/support/SupportPrimalViewModel.kt
+++ app/src/main/kotlin/net/primal/android/premium/support/SupportPrimalViewModel.kt
@@ -1,20 +1,40 @@
 package net.primal.android.premium.support
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.getAndUpdate
+import kotlinx.coroutines.launch
 import net.primal.android.premium.support.SupportPrimalContract.UiState
+import net.primal.android.premium.utils.isPremiumFree
+import net.primal.android.premium.utils.isPrimalLegend
+import net.primal.android.user.accounts.active.ActiveAccountStore
 
 @HiltViewModel
-class SupportPrimalViewModel @Inject constructor() : ViewModel() {
+class SupportPrimalViewModel @Inject constructor(
+    private val activeAccountStore: ActiveAccountStore,
+) : ViewModel() {
 
     private val _state = MutableStateFlow(UiState())
     val state = _state.asStateFlow()
     private fun setState(reducer: UiState.() -> UiState) = _state.getAndUpdate(reducer)
 
     init {
+        observeActiveAccount()
     }
+
+    private fun observeActiveAccount() =
+        viewModelScope.launch {
+            activeAccountStore.activeUserAccount.collect {
+                setState {
+                    copy(
+                        isPrimalLegend = it.premiumMembership?.cohort2.isPrimalLegend(),
+                        hasMembership = it.premiumMembership != null && !it.premiumMembership.cohort2.isPremiumFree(),
+                    )
+                }
+            }
+        }
 }
