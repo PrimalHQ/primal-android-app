0dc4e92612807be48b7dff90a5febbd2e3adb062
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-12 16:09:55 +0100

    Implement become legend intro stage

---
+++ app/src/main/kotlin/net/primal/android/premium/legend/ui/amount/BecomeLegendAmountStage.kt
@@ -0,0 +1,53 @@
+package net.primal.android.premium.legend.ui.amount
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import net.primal.android.R
+import net.primal.android.core.compose.PrimalTopAppBar
+import net.primal.android.core.compose.icons.PrimalIcons
+import net.primal.android.core.compose.icons.primaliconpack.ArrowBack
+import net.primal.android.premium.legend.ui.BecomeLegendBottomBarButton
+
+@ExperimentalMaterial3Api
+@Composable
+fun BecomeLegendAmountStage(modifier: Modifier = Modifier, onClose: () -> Unit) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            PrimalTopAppBar(
+                title = stringResource(id = R.string.premium_become_legend_primal_title),
+                navigationIcon = PrimalIcons.ArrowBack,
+                onNavigationIconClick = onClose,
+                showDivider = false,
+            )
+        },
+        bottomBar = {
+            BecomeLegendBottomBarButton(
+                text = stringResource(R.string.premium_become_legend_button_pay_now),
+                onClick = {},
+            )
+        },
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(state = rememberScrollState())
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+        ) {
+        }
+    }
+}
