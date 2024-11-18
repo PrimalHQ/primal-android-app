c4334c70b61fc1ca16720c7d4912279ad52468f3
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-18 18:34:57 +0100

    Add opening Extend Subscription from manage premium

--- app/src/main/kotlin/net/primal/android/premium/manage/PremiumManageContract.kt
+++ app/src/main/kotlin/net/primal/android/premium/manage/PremiumManageContract.kt
@@ -3,19 +3,20 @@ package net.primal.android.premium.manage
 interface PremiumManageContract {
 
     data class UiState(
+        val primalName: String = "",
         val isLegend: Boolean = false,
         val isRecurring: Boolean = false,
     )
 
-    enum class ManageDestination {
-        MediaManagement,
-        PremiumRelay,
-        ContactListBackup,
-        ContentBackup,
-        ManageSubscription,
-        ChangePrimalName,
-        ExtendSubscription,
-        LegendaryProfileCustomization,
-        BecomeALegend,
+    sealed class ManageDestination {
+        data object MediaManagement : ManageDestination()
+        data object PremiumRelay : ManageDestination()
+        data object ContactListBackup : ManageDestination()
+        data object ContentBackup : ManageDestination()
+        data object ManageSubscription : ManageDestination()
+        data object ChangePrimalName : ManageDestination()
+        data class ExtendSubscription(val primalName: String) : ManageDestination()
+        data object LegendaryProfileCustomization : ManageDestination()
+        data object BecomeALegend : ManageDestination()
     }
 }
