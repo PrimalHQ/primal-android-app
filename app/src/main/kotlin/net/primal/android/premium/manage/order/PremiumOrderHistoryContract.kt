b513984a70ca3b209a1d68c16a58a625a3270687
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-18 19:50:48 +0100

    Implement new subscription header

--- app/src/main/kotlin/net/primal/android/premium/manage/order/PremiumOrderHistoryContract.kt
+++ app/src/main/kotlin/net/primal/android/premium/manage/order/PremiumOrderHistoryContract.kt
@@ -7,9 +7,13 @@ interface PremiumOrderHistoryContract {
 
     data class UiState(
         val fetchingHistory: Boolean = true,
+        val primalName: String = "",
         val orders: List<PremiumPurchaseOrder> = emptyList(),
+        val subscriptionOrigin: String? = null,
         val isRecurringSubscription: Boolean = false,
+        val isLegend: Boolean = false,
         val cancellingSubscription: Boolean = false,
+        val expiresAt: Long? = null,
         val error: MembershipError? = null,
     )
 
