81398f17230c75485844b44abad5c682a2ce9013
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-18 13:30:41 +0100

    Fix media management uploads since/limit params

---
+++ app/src/main/kotlin/net/primal/android/premium/manage/media/api/model/MediaUploadsRequestBody.kt
@@ -0,0 +1,14 @@
+package net.primal.android.premium.manage.media.api.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import net.primal.android.nostr.model.NostrEvent
+
+@Serializable
+data class MediaUploadsRequestBody(
+    @SerialName("event_from_user") val eventFromUser: NostrEvent,
+    @SerialName("limit") val limit: Int? = null,
+    @SerialName("until") val until: Long? = null,
+    @SerialName("since") val since: Long? = null,
+    @SerialName("offset") val offset: Long? = null,
+)
