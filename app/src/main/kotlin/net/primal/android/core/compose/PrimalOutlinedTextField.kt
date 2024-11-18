b6b8ac31adb42f74b2b4cb9a31417d36fd05d361
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-18 11:18:51 +0100

    Implement forceFocus in PrimalOutlinedTextField; Enable forceFocus in PremiumPrimalNameStage;

--- app/src/main/kotlin/net/primal/android/core/compose/PrimalOutlinedTextField.kt
+++ app/src/main/kotlin/net/primal/android/core/compose/PrimalOutlinedTextField.kt
@@ -11,14 +11,24 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.TextUnit
@@ -32,6 +42,7 @@ fun PrimalOutlinedTextField(
     header: String?,
     value: String,
     onValueChange: (String) -> Unit,
+    forceFocus: Boolean = false,
     isRequired: Boolean = false,
     prefix: String? = null,
     isMultiline: Boolean = false,
@@ -39,6 +50,21 @@ fun PrimalOutlinedTextField(
     textAlign: TextAlign = TextAlign.Start,
     isError: Boolean = false,
 ) {
+    val focusRequester: FocusRequester = remember { FocusRequester() }
+    var focusRequested by rememberSaveable { mutableStateOf(false) }
+    LaunchedEffect(focusRequester) {
+        if (!focusRequested && forceFocus) {
+            focusRequester.requestFocus()
+            focusRequested = true
+        }
+    }
+
+    var textFieldValue by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(
+            TextFieldValue(text = value, selection = TextRange(start = value.length, end = value.length)),
+        )
+    }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -84,14 +110,19 @@ fun PrimalOutlinedTextField(
         }
         Spacer(modifier = Modifier.height(4.dp))
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .focusRequester(focusRequester)
+                .fillMaxWidth(),
             colors = PrimalDefaults.outlinedTextFieldColors(),
             shape = AppTheme.shapes.medium,
             isError = isError,
             singleLine = !isMultiline,
             minLines = if (isMultiline) 6 else 0,
-            value = value,
-            onValueChange = onValueChange,
+            value = textFieldValue,
+            onValueChange = {
+                textFieldValue = it
+                onValueChange(it.text)
+            },
             textStyle = AppTheme.typography.bodyLarge.copy(
                 fontSize = fontSize,
                 textAlign = textAlign,
