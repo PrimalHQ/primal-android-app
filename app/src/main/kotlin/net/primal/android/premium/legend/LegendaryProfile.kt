268f5ce7eb62344e3f06bdc099b6487a2f5d8ec7
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-19 13:34:15 +0100

    Implement UI for legendary profile customization

---
+++ app/src/main/kotlin/net/primal/android/premium/legend/LegendaryProfile.kt
@@ -0,0 +1,120 @@
+package net.primal.android.premium.legend
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+enum class LegendaryProfile(
+    val id: String,
+    val brush: Brush,
+) {
+    NO_CUSTOMIZATION(
+        id = "",
+        brush = Brush.linearGradient(
+            listOf(Color.Transparent, Color.Transparent),
+        ),
+    ),
+    GOLD(
+        id = "GOLD",
+        brush = Brush.verticalGradient(
+            colorStops = arrayOf(
+                0.00f to Color(0xFFFFB700),
+                0.49f to Color(0xFFFFB700),
+                0.50f to Color(0xFFCB721E),
+                1.00f to Color(0xFFFFB700),
+            ),
+        ),
+    ),
+    AQUA(
+        id = "AQUA",
+        brush = Brush.verticalGradient(
+            colorStops = arrayOf(
+                0.00f to Color(0xFF6BCCFF),
+                0.49f to Color(0xFF6BCCFF),
+                0.50f to Color(0xFF247FFF),
+                1.00f to Color(0xFF6BCCFF),
+            ),
+        ),
+    ),
+    SILVER(
+        id = "SILVER",
+        brush = Brush.verticalGradient(
+            colorStops = arrayOf(
+                0.00f to Color(0xFFCCCCCC),
+                0.49f to Color(0xFFCCCCCC),
+                0.50f to Color(0xFF777777),
+                1.00f to Color(0xFFCCCCCC),
+            ),
+        ),
+    ),
+    PURPLE(
+        id = "PURPLE",
+        brush = Brush.linearGradient(
+            colors = listOf(
+                Color(0xFFB300D3),
+                Color(0xFF4800FF),
+            ),
+            start = Offset.Zero,
+            end = Offset.Infinite,
+        ),
+    ),
+    PURPLE_HAZE(
+        id = "PURPLEHAZE",
+        brush = Brush.linearGradient(
+            colors = listOf(
+                Color(0xFFFB00C4),
+                Color(0xFF04F7FC),
+            ),
+            start = Offset.Zero,
+            end = Offset.Infinite,
+        ),
+    ),
+    TEAL(
+        id = "TEAL",
+        brush = Brush.linearGradient(
+            colorStops = arrayOf(
+                0.00f to Color(0xFF40FCFF),
+                0.20f to Color(0xFF40FCFF),
+                0.75f to Color(0xFF007D9F),
+                1.00f to Color(0xFF007D9F),
+            ),
+        ),
+    ),
+    BROWN(
+        id = "BROWN",
+        brush = Brush.linearGradient(
+            colorStops = arrayOf(
+                0.00f to Color(0xFFBB9971),
+                0.20f to Color(0xFFBB9971),
+                0.75f to Color(0xFF5C3B22),
+                1.00f to Color(0xFF5C3B22),
+            ),
+        ),
+    ),
+    BLUE(
+        id = "BLUE",
+        brush = Brush.linearGradient(
+            colorStops = arrayOf(
+                0.00f to Color(0xFF01E0FF),
+                0.05f to Color(0xFF01E0FF),
+                0.30f to Color(0xFF0190F8),
+                0.60f to Color(0xFF2555EE),
+                0.90f to Color(0xFF4A05CA),
+                1.00f to Color(0xFF4A05CA),
+            ),
+        ),
+    ),
+    SUN_FIRE(
+        id = "SUNFIRE",
+        brush = Brush.linearGradient(
+            colorStops = arrayOf(
+                0.00f to Color(0xFFFFA722),
+                0.05f to Color(0xFFFFA722),
+                0.30f to Color(0xFFFA3C3C),
+                0.60f to Color(0xFFF00492),
+                0.90f to Color(0xFF620BA9),
+                1.00f to Color(0xFF620BA9),
+            ),
+        ),
+    ),
+}
--- app/src/main/kotlin/net/primal/android/premium/legend/custimization/LegendaryProfileCustomizationScreen.kt
+++ app/src/main/kotlin/net/primal/android/premium/legend/custimization/LegendaryProfileCustomizationScreen.kt
@@ -1,12 +1,26 @@
 package net.primal.android.premium.legend.custimization
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -14,10 +28,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import net.primal.android.R
+import net.primal.android.core.compose.AvatarThumbnail
+import net.primal.android.core.compose.NostrUserText
+import net.primal.android.core.compose.PrimalDivider
+import net.primal.android.core.compose.PrimalSwitch
 import net.primal.android.core.compose.PrimalTopAppBar
+import net.primal.android.core.compose.button.PrimalFilledButton
 import net.primal.android.core.compose.icons.PrimalIcons
 import net.primal.android.core.compose.icons.primaliconpack.ArrowBack
+import net.primal.android.core.compose.icons.primaliconpack.LegendaryProfileNoCustomization
+import net.primal.android.premium.legend.LegendaryProfile
+import net.primal.android.premium.ui.PremiumBadge
+import net.primal.android.theme.AppTheme
 
 @Composable
 fun LegendaryProfileCustomizationScreen(viewModel: LegendaryProfileCustomizationViewModel, onClose: () -> Unit) {
@@ -41,16 +68,197 @@ fun LegendaryProfileCustomizationScreen(state: LegendaryProfileCustomizationCont
                 onNavigationIconClick = onClose,
             )
         },
+        bottomBar = {
+            BottomBarButton(
+                text = stringResource(id = R.string.premium_legend_profile_apply_button),
+                onClick = {
+
+                },
+            )
+        },
         snackbarHost = {
             SnackbarHost(hostState = snackbarHostState)
         },
     ) { paddingValues ->
         Column(
             modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(state = rememberScrollState())
+                .padding(horizontal = 16.dp)
                 .padding(paddingValues)
-                .fillMaxSize(),
+                .navigationBarsPadding(),
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically),
         ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                AvatarThumbnail(
+                    avatarCdnImage = state.avatarCdnImage,
+                    avatarSize = 80.dp,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                val primalName = state.membership?.premiumName ?: ""
+                NostrUserText(
+                    modifier = Modifier.padding(start = 8.dp),
+                    displayName = primalName,
+                    internetIdentifier = "$primalName@primal.net",
+                    internetIdentifierBadgeSize = 24.dp,
+                    fontSize = 20.sp,
+                )
+            }
+
+            if (state.membership != null) {
+                PremiumBadge(
+                    firstCohort = state.membership.cohort1,
+                    secondCohort = state.membership.cohort2,
+                    membershipExpired = state.membership.isExpired(),
+                    topColor = AppTheme.colorScheme.primary,
+                )
+
+                PrimalDivider(modifier = Modifier.padding(top = 16.dp))
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 32.dp, vertical = 8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                    ) {
+                        LegendProfileColorBox(profile = LegendaryProfile.NO_CUSTOMIZATION)
+
+                        LegendProfileColorBox(profile = LegendaryProfile.GOLD)
+
+                        LegendProfileColorBox(profile = LegendaryProfile.AQUA)
+
+                        LegendProfileColorBox(profile = LegendaryProfile.SILVER)
+
+                        LegendProfileColorBox(profile = LegendaryProfile.PURPLE)
+                    }
+
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                    ) {
+                        LegendProfileColorBox(profile = LegendaryProfile.PURPLE_HAZE)
+
+                        LegendProfileColorBox(profile = LegendaryProfile.TEAL)
+
+                        LegendProfileColorBox(profile = LegendaryProfile.BROWN)
+
+                        LegendProfileColorBox(profile = LegendaryProfile.BLUE)
+
+                        LegendProfileColorBox(profile = LegendaryProfile.SUN_FIRE)
+                    }
+                }
+
+                SwitchSettings(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                )
+
+                Text(
+                    color = AppTheme.extraColorScheme.onSurfaceVariantAlt2,
+                    text = stringResource(id = R.string.premium_legend_profile_customization_notice),
+                    style = AppTheme.typography.bodySmall,
+                    fontSize = 13.sp,
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun LegendProfileColorBox(profile: LegendaryProfile) {
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .background(brush = profile.brush, shape = CircleShape),
+        contentAlignment = Alignment.Center,
+    ) {
+        if (profile == LegendaryProfile.NO_CUSTOMIZATION) {
+            Icon(
+                imageVector = PrimalIcons.LegendaryProfileNoCustomization,
+                contentDescription = null,
+                tint = AppTheme.extraColorScheme.onSurfaceVariantAlt2,
+            )
+        }
+    }
+}
+
+@Composable
+private fun SwitchSettings(modifier: Modifier) {
+    Column(
+        modifier = modifier.background(
+            color = AppTheme.extraColorScheme.surfaceVariantAlt3,
+            shape = AppTheme.shapes.large,
+        ),
+    ) {
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = stringResource(R.string.premium_legend_profile_custom_badge),
+                fontSize = 16.sp,
+                style = AppTheme.typography.bodyMedium,
+            )
+            PrimalSwitch(
+                checked = false,
+                onCheckedChange = {},
+            )
+        }
+
+        PrimalDivider()
+
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = stringResource(R.string.premium_legend_profile_avatar_ring),
+                fontSize = 16.sp,
+                style = AppTheme.typography.bodyMedium,
+            )
+            PrimalSwitch(
+                checked = true,
+                onCheckedChange = {},
+            )
+        }
+    }
+}
+
+@Composable
+private fun BottomBarButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .navigationBarsPadding()
+            .padding(bottom = 36.dp)
+            .padding(horizontal = 36.dp),
+    ) {
+        PrimalFilledButton(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = onClick,
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 8.dp),
+                text = text,
+                style = AppTheme.typography.bodyLarge,
+                fontWeight = FontWeight.SemiBold,
+            )
         }
     }
 }
