cae7e15000b1b0d84852303d9fcd0896608e945a
Aleksandar Ilic <aleksandar@appollo41.com>
2024-11-20 16:41:38 +0100

    Implement reading and rendering contact list backups

--- app/src/main/kotlin/net/primal/android/premium/manage/contact/PremiumContactListContract.kt
+++ app/src/main/kotlin/net/primal/android/premium/manage/contact/PremiumContactListContract.kt
@@ -1,10 +1,15 @@
 package net.primal.android.premium.manage.contact
 
+import net.primal.android.premium.manage.contact.model.FollowListBackup
+
 interface PremiumContactListContract {
 
     data class UiState(
         val fetching: Boolean = false,
+        val backups: List<FollowListBackup> = emptyList(),
     )
 
-    sealed class UiEvent
+    sealed class UiEvent {
+        data class RecoverFollowList(val backup: FollowListBackup) : UiEvent()
+    }
 }
