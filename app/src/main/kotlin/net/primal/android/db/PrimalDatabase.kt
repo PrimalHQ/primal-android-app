504332fb0a98edf701224b5158af9e8b6eb4bbba
Aleksandar Ilic <aleksandar@appollo41.com>
2024-12-27 21:56:59 +0100

    Increase db scheme version needed for mixed-up feeds fix (0eb4e681)

--- app/src/main/kotlin/net/primal/android/db/PrimalDatabase.kt
+++ app/src/main/kotlin/net/primal/android/db/PrimalDatabase.kt
@@ -93,7 +93,7 @@ import net.primal.android.wallet.db.WalletTransactionData
         ArticleFeedCrossRef::class,
         HighlightData::class,
     ],
-    version = 50,
+    version = 51,
     exportSchema = true,
 )
 @TypeConverters(
