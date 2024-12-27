53cf140dab96e0e5206be2641ddd7741300135cf
Aleksandar Ilic <aleksandar@appollo41.com>
2024-12-27 14:15:41 +0100

    Format code and update baseline

--- app/src/main/kotlin/net/primal/android/thread/articles/details/ArticleDetailsScreen.kt
+++ app/src/main/kotlin/net/primal/android/thread/articles/details/ArticleDetailsScreen.kt
@@ -29,7 +29,6 @@ import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
