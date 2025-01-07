054c98a3675e99628d5d1065266ce94746fadfd0
Aleksandar Ilic <aleksandar@appollo41.com>
2025-01-07 12:15:37 +0100

    Primal 2.0.36 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "net.primal.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 203
-        versionName = "2.0.35"
+        versionCode = 204
+        versionName = "2.0.36"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
--- app/src/main/github/release-notes/github.md
+++ app/src/main/github/release-notes/github.md
@@ -1,2 +1,3 @@
 ## Fixes
-- Stability fixes.
+- Remove trailing empty spaces from tags in nostr events.
+- Prevent crash when going back to home screen and then tapping home icon fast.
