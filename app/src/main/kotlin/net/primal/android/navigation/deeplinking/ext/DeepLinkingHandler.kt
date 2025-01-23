89d20f80c0d11c8d55a5d3d12fcd3c95fc3950d5
Marko Kocic <marko@appollo41.com>
2025-01-23 18:01:58 +0100

    Handle different primal.net note links

--- app/src/main/kotlin/net/primal/android/navigation/deeplinking/ext/DeepLinkingHandler.kt
+++ app/src/main/kotlin/net/primal/android/navigation/deeplinking/ext/DeepLinkingHandler.kt
@@ -2,6 +2,7 @@ package net.primal.android.navigation.deeplinking.ext
 
 import net.primal.android.crypto.bech32ToHexOrThrow
 import net.primal.android.navigation.deeplinking.DeepLink
+import net.primal.android.nostr.utils.Nip19TLV
 import net.primal.android.user.domain.isNwcUrl
 import net.primal.android.user.domain.parseNWCUrl
 
@@ -13,13 +14,19 @@ private const val NOSTR_WALLET_CONNECT_ALT_SCHEMA = "nostrwalletconnect://"
 fun String.parseDeepLinkOrNull(): DeepLink? =
     when {
         PRIMAL_NOTE_REGEX.containsMatchIn(this) ->
-            DeepLink.Note(PRIMAL_NOTE_REGEX.replace(this, "").bech32ToHexOrThrow())
+            PRIMAL_NOTE_REGEX.replace(this, "").resolveNoteId()?.let { DeepLink.Note(it) }
 
         isNostrWalletConnectSchemaAndUrl() ->
             runCatching { DeepLink.NostrWalletConnect(nwc = this.parseNWCUrl()) }.getOrNull()
 
         else -> null
     }
+
+private fun String.resolveNoteId(): String? = when {
+    this.startsWith("note1") -> runCatching { bech32ToHexOrThrow() }.getOrNull()
+    this.startsWith("nevent1") -> Nip19TLV.parseUriAsNeventOrNull(this)?.eventId
+    else -> this
+}
 
 private fun String.isNostrWalletConnectSchemaAndUrl(): Boolean =
     (this.startsWith(NOSTR_WALLET_CONNECT_SCHEMA) || this.startsWith(NOSTR_WALLET_CONNECT_ALT_SCHEMA)) &&
