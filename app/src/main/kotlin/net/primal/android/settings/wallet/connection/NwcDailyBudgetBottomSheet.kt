7942382ad8850d8dbd0463e103ddeab9b89ca594
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-01-29 02:02:36 +0100

    Change ListItem in BottomSheet

--- app/src/main/kotlin/net/primal/android/settings/wallet/connection/NwcDailyBudgetBottomSheet.kt
+++ app/src/main/kotlin/net/primal/android/settings/wallet/connection/NwcDailyBudgetBottomSheet.kt
@@ -8,8 +8,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material.icons.automirrored.filled.FactCheck
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemColors
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
@@ -23,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -120,28 +130,25 @@ fun BudgetOptionRow(
     isSelected: Boolean,
     onSelect: () -> Unit,
 ) {
-    Row(
+    ListItem(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable(onClick = onSelect),
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        RadioButton(
-            selected = isSelected,
-            onClick = onSelect,
-            colors = RadioButtonDefaults.colors(
-                selectedColor = AppTheme.colorScheme.primary,
-                unselectedColor = Color.Transparent,
-            ),
+            .clickable(onClick = onSelect)
+            .clip(RoundedCornerShape(12.dp)),
+        headlineContent = {
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = option.toIntOrNull()?.let { "%,d sats".format(it) } ?: option,
+                style = AppTheme.typography.bodyMedium,
+            )
+        },
+        trailingContent = {
+            if (isSelected) {
+                Icon(imageVector = Icons.Default.Check, contentDescription = null)
+            }
+        },
+        colors = ListItemDefaults.colors(
+            containerColor = AppTheme.extraColorScheme.surfaceVariantAlt1,
         )
-
-        Text(
-            text = "${
-                option.toIntOrNull()?.let { "%,d".format(it) } ?: option
-            } sats",
-            modifier = Modifier.padding(start = 16.dp),
-            style = AppTheme.typography.bodyMedium,
-        )
-    }
+    )
 }
