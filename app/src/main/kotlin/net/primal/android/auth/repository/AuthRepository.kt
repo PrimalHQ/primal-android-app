70dd529401ff789afa728317c9e54818b361447f
Marko Kocic <marko@appollo41.com>
2025-02-10 17:35:22 +0100

    Fix failed login logouts everyone

--- app/src/main/kotlin/net/primal/android/auth/repository/AuthRepository.kt
+++ app/src/main/kotlin/net/primal/android/auth/repository/AuthRepository.kt
@@ -2,6 +2,8 @@ package net.primal.android.auth.repository
 
 import javax.inject.Inject
 import javax.inject.Singleton
+import net.primal.android.crypto.bech32ToHexOrThrow
+import net.primal.android.crypto.extractKeyPairFromPrivateKeyOrThrow
 import net.primal.android.user.accounts.active.ActiveAccountStore
 import net.primal.android.user.credentials.CredentialsStore
 import net.primal.android.user.repository.UserRepository
@@ -12,6 +14,11 @@ class AuthRepository @Inject constructor(
     private val activeAccountStore: ActiveAccountStore,
     private val userRepository: UserRepository,
 ) {
+    fun generateUserIdFromNsec(nostrKey: String): String {
+        val (_, pubkey) = nostrKey.extractKeyPairFromPrivateKeyOrThrow()
+        return pubkey.bech32ToHexOrThrow()
+    }
+
     suspend fun login(nostrKey: String): String {
         val userId = credentialsStore.save(nostrKey)
         activeAccountStore.setActiveUserId(userId)
--- app/src/main/kotlin/net/primal/android/auth/repository/LoginHandler.kt
+++ app/src/main/kotlin/net/primal/android/auth/repository/LoginHandler.kt
@@ -1,7 +1,9 @@
 package net.primal.android.auth.repository
 
 import javax.inject.Inject
+import kotlinx.coroutines.withContext
 import net.primal.android.bookmarks.BookmarksRepository
+import net.primal.android.core.coroutines.CoroutineDispatcherProvider
 import net.primal.android.settings.muted.repository.MutedUserRepository
 import net.primal.android.settings.repository.SettingsRepository
 import net.primal.android.user.repository.UserRepository
@@ -12,21 +14,22 @@ class LoginHandler @Inject constructor(
     private val userRepository: UserRepository,
     private val mutedUserRepository: MutedUserRepository,
     private val bookmarksRepository: BookmarksRepository,
+    private val dispatchers: CoroutineDispatcherProvider,
 ) {
 
-    suspend fun login(nostrKey: String) {
-        val userId = authRepository.login(nostrKey = nostrKey)
-        val postLoginResult = runCatching {
-            userRepository.fetchAndUpdateUserAccount(userId = userId)
-            bookmarksRepository.fetchAndPersistPublicBookmarks(userId = userId)
-            settingsRepository.fetchAndPersistAppSettings(userId = userId)
-            mutedUserRepository.fetchAndPersistMuteList(userId = userId)
-        }
+    suspend fun login(nostrKey: String) =
+        withContext(dispatchers.io()) {
+            runCatching {
+                val userId = authRepository.generateUserIdFromNsec(nostrKey = nostrKey)
 
-        val exception = postLoginResult.exceptionOrNull()
-        if (exception != null) {
-            authRepository.logout()
-            throw exception
+                userRepository.fetchAndUpdateUserAccount(userId = userId)
+                bookmarksRepository.fetchAndPersistPublicBookmarks(userId = userId)
+                settingsRepository.fetchAndPersistAppSettings(userId = userId)
+                mutedUserRepository.fetchAndPersistMuteList(userId = userId)
+            }.onFailure { exception ->
+                throw exception
+            }.onSuccess {
+                authRepository.login(nostrKey = nostrKey)
+            }
         }
-    }
 }
