535ae0930ef419d6a0c39c5f2b7e3b8fcc9c367f
Marko Kocic <marko@appollo41.com>
2025-02-21 13:45:37 +0100

    Implement articles pagination (#322)
    * Implement articles pagination
    * Implement better findRemoteKey logic
    * Implement article order by paging event
    * Reference article by aTag instead of eventId
    
    ---------
    
    Co-authored-by: Aleksandar Ilic <aleksandar@appollo41.com>

--- app/src/main/kotlin/net/primal/android/nostr/ext/NostrEvents.kt
+++ app/src/main/kotlin/net/primal/android/nostr/ext/NostrEvents.kt
@@ -1,11 +1,11 @@
 package net.primal.android.nostr.ext
 
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.decodeFromJsonElement
 import net.primal.android.core.serialization.json.NostrJson
 import net.primal.android.nostr.model.NostrEvent
 import net.primal.android.nostr.model.primal.PrimalEvent
+import net.primal.android.nostr.model.primal.content.ContentPrimalPaging
 import timber.log.Timber
 
 fun JsonObject?.asNostrEventOrNull(): NostrEvent? {
@@ -26,4 +26,11 @@ fun JsonObject?.asPrimalEventOrNull(): PrimalEvent? {
         Timber.w(this?.let(NostrJson::encodeToString))
         null
     }
+}
+
+fun List<NostrEvent>.orderByPagingIfNotNull(pagingEvent: ContentPrimalPaging?): List<NostrEvent> {
+    if (pagingEvent == null) return this
+
+    val eventsMap = this.associateBy { it.id }
+    return pagingEvent.elements.mapNotNull { eventsMap[it] }
 }
