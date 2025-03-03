1c63526fe8824739b67c053663d81fa83dd98766
Marko Kocic <marko@appollo41.com>
2025-03-03 19:14:27 +0100

    nip19/nevent: make author field optional

--- app/src/main/kotlin/net/primal/android/nostr/utils/Nevent.kt
+++ app/src/main/kotlin/net/primal/android/nostr/utils/Nevent.kt
@@ -2,7 +2,7 @@ package net.primal.android.nostr.utils
 
 data class Nevent(
     val kind: Int?,
-    val userId: String,
+    val userId: String?,
     val eventId: String,
     val relays: List<String> = emptyList(),
 )
