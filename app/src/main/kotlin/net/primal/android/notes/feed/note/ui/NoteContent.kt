fd300d02319a971df45488de1961d17033ae04cc
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-03-10 02:07:44 +0100

    remove timber

--- app/src/main/kotlin/net/primal/android/notes/feed/note/ui/NoteContent.kt
+++ app/src/main/kotlin/net/primal/android/notes/feed/note/ui/NoteContent.kt
@@ -49,7 +49,6 @@ import net.primal.android.notes.feed.note.ui.events.NoteCallbacks
 import net.primal.android.premium.legend.domain.asLegendaryCustomization
 import net.primal.android.theme.AppTheme
 import net.primal.android.theme.domain.PrimalTheme
-import timber.log.Timber
 
 private const val PROFILE_ID_ANNOTATION_TAG = "profileId"
 private const val URL_ANNOTATION_TAG = "url"
