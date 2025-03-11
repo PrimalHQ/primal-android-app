a8c9082007d4259a22188daa48c2ddbf138daa33
Aleksandar Ilic <aleksandar@appollo41.com>
2025-03-11 19:57:41 +0100

    Integrate AppConfig with PrimalApiClient in `shared` module
    Adds `config` package to `shared` library and integrates with
    PrimalApiClient and adds new dependencies: Ktorfit, DataStore
    and Kotlinx Datetime.

--- shared/src/androidMain/kotlin/net/primal/core/di/PlatformModule.android.kt
+++ shared/src/androidMain/kotlin/net/primal/core/di/PlatformModule.android.kt
@@ -1,11 +1,16 @@
 package net.primal.core.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStoreFile
 import androidx.sqlite.driver.AndroidSQLiteDriver
 import net.primal.core.coroutines.AndroidDispatcherProvider
 import net.primal.core.coroutines.DispatcherProvider
 import net.primal.db.PrimalDatabase
-import net.primal.db.buildAppDatabase
+import net.primal.db.buildPrimalDatabase
 import net.primal.db.getDatabaseBuilder
+import net.primal.networking.config.domain.AppConfig
+import net.primal.networking.config.store.AppConfigDataStoreFactory
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 internal actual fun platformModule() = module {
@@ -13,12 +18,18 @@ internal actual fun platformModule() = module {
     single<DispatcherProvider> { AndroidDispatcherProvider() }
 
     single<PrimalDatabase> {
-        buildAppDatabase(
+        buildPrimalDatabase(
             driver = AndroidSQLiteDriver(),
             queryCoroutineContext = get<DispatcherProvider>().io(),
             builder = getDatabaseBuilder(
                 context = get(),
             ),
         )
+    }
+
+    single<DataStore<AppConfig>> {
+        AppConfigDataStoreFactory.createDataStore {
+            androidContext().dataStoreFile(AppConfigDataStoreFactory.APP_CONFIG_DATA_STORE_NAME).path
+        }
     }
 }
---
+++ shared/src/commonMain/kotlin/net/primal/networking/config/store/AppConfigDataStore.kt
@@ -0,0 +1,51 @@
+package net.primal.networking.config.store
+
+import androidx.datastore.core.DataStore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.runBlocking
+import net.primal.core.coroutines.DispatcherProvider
+import net.primal.networking.config.domain.AppConfig
+
+internal class AppConfigDataStore(
+    dispatcherProvider: DispatcherProvider,
+    private val persistence: DataStore<AppConfig>,
+) {
+
+    private val scope = CoroutineScope(dispatcherProvider.io())
+
+    val config = persistence.data
+        .stateIn(
+            scope = scope,
+            started = SharingStarted.Eagerly,
+            initialValue = runBlocking { persistence.data.first() },
+        )
+
+    suspend fun updateConfig(reducer: AppConfig.() -> AppConfig): AppConfig {
+        return persistence.updateData { currentAppConfig ->
+            val newAppConfig = currentAppConfig.reducer()
+            if (!currentAppConfig.cacheUrlOverride) {
+                newAppConfig
+            } else {
+                currentAppConfig.copy(
+                    uploadUrl = newAppConfig.uploadUrl,
+                    walletUrl = newAppConfig.walletUrl,
+                )
+            }
+        }
+    }
+
+    suspend fun overrideCacheUrl(url: String) {
+        persistence.updateData {
+            it.copy(cacheUrl = url, cacheUrlOverride = true)
+        }
+    }
+
+    suspend fun revertCacheUrlOverrideFlag() {
+        persistence.updateData {
+            it.copy(cacheUrlOverride = false)
+        }
+    }
+}
