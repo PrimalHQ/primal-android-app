99d158709051a33615f3f99b7f63192d2bddec55
Aleksandar Ilic <aleksandar@appollo41.com>
2025-03-19 18:48:39 +0100

    Remove unnecessary User Agent from `app` module

--- app/src/main/kotlin/net/primal/android/networking/UserAgentProvider.kt
+++ app/src/main/kotlin/net/primal/android/networking/UserAgentProvider.kt
@@ -1,10 +1,5 @@
 package net.primal.android.networking
 
-import net.primal.android.BuildConfig
-
 object UserAgentProvider {
-
     const val APP_NAME = "Primal-Android"
-
-    const val USER_AGENT = "$APP_NAME/${BuildConfig.VERSION_NAME}"
 }
