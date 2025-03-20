796e9f0f24634c93b062bd5476c00821740ae303
Aleksandar Ilic <aleksandar@appollo41.com>
2025-03-20 17:01:17 +0100

    Move PrimalImportApi to `data:remote-caching` module

--- app/src/main/kotlin/net/primal/android/nostr/publish/NostrPublisher.kt
+++ app/src/main/kotlin/net/primal/android/nostr/publish/NostrPublisher.kt
@@ -1,7 +1,6 @@
 package net.primal.android.nostr.publish
 
 import javax.inject.Inject
-import net.primal.android.networking.primal.api.PrimalImportApi
 import net.primal.android.networking.relays.RelaysSocketManager
 import net.primal.android.networking.relays.errors.NostrPublishException
 import net.primal.android.nostr.model.content.ContentMetadata
@@ -11,6 +10,7 @@ import net.primal.android.nostr.notary.NostrUnsignedEvent
 import net.primal.android.user.domain.NostrWalletConnect
 import net.primal.android.user.domain.Relay
 import net.primal.android.wallet.nwc.model.LightningPayResponse
+import net.primal.data.remote.api.importing.PrimalImportApi
 import net.primal.domain.nostr.NostrEvent
 import timber.log.Timber
 
--- data/remote-caching/src/commonMain/kotlin/net/primal/data/remote/factory/PrimalApiFactory.kt
+++ data/remote-caching/src/commonMain/kotlin/net/primal/data/remote/factory/PrimalApiFactory.kt
@@ -3,20 +3,16 @@ package net.primal.data.remote.factory
 import net.primal.core.networking.primal.PrimalApiClient
 import net.primal.data.remote.api.feed.FeedApi
 import net.primal.data.remote.api.feed.FeedApiImpl
+import net.primal.data.remote.api.importing.PrimalImportApi
+import net.primal.data.remote.api.importing.PrimalImportApiImpl
+import net.primal.data.remote.api.upload.UploadApi
 
 object PrimalApiFactory {
 
     fun createFeedsApi(primalApiClient: PrimalApiClient): FeedApi = FeedApiImpl(primalApiClient)
 
-//    factory<UploadApi> {
-//        UploadApiSingleConnection(
-//            primalUploadClient = PrimalApiClientFactory.create(PrimalServerType.Upload),
-//        )
-//    }
-//
-//    factory<PrimalImportApi> {
-//        PrimalImportApiImpl(
-//            primalApiClient = PrimalApiClientFactory.create(PrimalServerType.Caching),
-//        )
-//    }
+    fun createUploadsApi(primalApiClient: PrimalApiClient): UploadApi = throw NotImplementedError()
+
+    fun createImportApi(primalApiClient: PrimalApiClient): PrimalImportApi = PrimalImportApiImpl(primalApiClient)
+
 }
