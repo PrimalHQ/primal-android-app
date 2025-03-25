ab54cf0d740c9ab01e346a993db2732906d7f15e
Marko Kocic <marko@appollo41.com>
2025-03-25 13:14:53 +0100

    Implement AmberContentResolver; Add nip04 amber methods and write custom serializer

--- app/src/main/kotlin/net/primal/android/signer/SignerMethod.kt
+++ app/src/main/kotlin/net/primal/android/signer/SignerMethod.kt
@@ -1,6 +1,34 @@
 package net.primal.android.signer
 
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import net.primal.android.signer.SignerMethod.entries
+
+@Serializable(with = SignerMethod.Companion::class)
 enum class SignerMethod(val method: String) {
     GET_PUBLIC_KEY("get_public_key"),
     SIGN_EVENT("sign_event"),
+    NIP04_DECRYPT("nip04_decrypt"),
+    NIP04_ENCRYPT("nip04_encrypt"),
+    ;
+
+    companion object : KSerializer<SignerMethod> {
+        override val descriptor: SerialDescriptor =
+            PrimitiveSerialDescriptor("SignerMethod", PrimitiveKind.STRING)
+
+        override fun serialize(encoder: Encoder, value: SignerMethod) {
+            encoder.encodeString(value.method)
+        }
+
+        override fun deserialize(decoder: Decoder): SignerMethod {
+            val decoded = decoder.decodeString()
+            return entries.firstOrNull { it.method == decoded }
+                ?: throw IllegalArgumentException("Unknown method: $decoded")
+        }
+    }
 }
