9b35e1a9890340b9be65d2f7832fe821f56760d2
Aleksandar Ilic <aleksandar@appollo41.com>
2025-03-26 18:25:34 +0100

    Fix warning in PrimalRepositoryFactory expect object

--- data/repository-caching/src/commonMain/kotlin/net/primal/data/repository/factory/PrimalRepositoryFactory.kt
+++ data/repository-caching/src/commonMain/kotlin/net/primal/data/repository/factory/PrimalRepositoryFactory.kt
@@ -1,4 +1,8 @@
 package net.primal.data.repository.factory
 
+import net.primal.domain.repository.FeedRepository
+
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
-expect object PrimalRepositoryFactory : RepositoryFactory
+expect object PrimalRepositoryFactory : RepositoryFactory {
+    override fun createFeedRepository(): FeedRepository
+}
