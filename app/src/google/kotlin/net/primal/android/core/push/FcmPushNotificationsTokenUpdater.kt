3a926d700ff5e577e018e32b5c67a92d7f21122e
Aleksandar Ilic <aleksandar@appollo41.com>
2025-04-09 21:19:58 +0200

    Fix compile issues on google flavour

--- app/src/google/kotlin/net/primal/android/core/push/FcmPushNotificationsTokenUpdater.kt
+++ app/src/google/kotlin/net/primal/android/core/push/FcmPushNotificationsTokenUpdater.kt
@@ -4,14 +4,14 @@ import com.google.firebase.messaging.FirebaseMessaging
 import javax.inject.Inject
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
-import net.primal.android.core.coroutines.CoroutineDispatcherProvider
 import net.primal.android.core.push.api.PrimalPushMessagesApi
 import net.primal.android.user.accounts.UserAccountsStore
 import net.primal.core.networking.sockets.errors.WssException
+import net.primal.core.utils.coroutines.DispatcherProvider
 import timber.log.Timber
 
 class FcmPushNotificationsTokenUpdater @Inject constructor(
-    private val dispatcherProvider: CoroutineDispatcherProvider,
+    private val dispatcherProvider: DispatcherProvider,
     private val userAccountsStore: UserAccountsStore,
     private val primalPushMessagesApi: PrimalPushMessagesApi,
 ) : PushNotificationsTokenUpdater {
