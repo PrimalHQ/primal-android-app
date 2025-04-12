4c24f410902ca1410b980c7e96c8fb1f9b96c123
Aleksandar Ilic <aleksandar@appollo41.com>
2025-04-12 23:31:01 +0200

    Implement registering notification channels properly

--- app/src/google/kotlin/net/primal/android/core/push/PrimalFirebaseMessagingService.kt
+++ app/src/google/kotlin/net/primal/android/core/push/PrimalFirebaseMessagingService.kt
@@ -1,7 +1,5 @@
 package net.primal.android.core.push
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -14,18 +12,6 @@ class PrimalFirebaseMessagingService : FirebaseMessagingService() {
 
     @Inject
     lateinit var tokenUpdater: PushNotificationsTokenUpdater
-
-    init {
-        val defaultChannelId = "all_app_events"
-        val defaultChannelName = "Primal App Events"
-        val channel = NotificationChannel(
-            defaultChannelId,
-            defaultChannelName,
-            NotificationManager.IMPORTANCE_DEFAULT,
-        )
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-    }
 
     @OptIn(DelicateCoroutinesApi::class)
     override fun onNewToken(token: String) {
