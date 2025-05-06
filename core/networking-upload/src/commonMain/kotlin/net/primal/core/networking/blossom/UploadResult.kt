d3e34b8655b19ac6350e527e21c039c1b409c789
Mehmedalija Karišik <mehmedalija@appollo41.com>
2025-05-06 14:51:30 +0200

    Add support for BUD-08 in blossom uploader (#456)

--- core/networking-upload/src/commonMain/kotlin/net/primal/core/networking/blossom/UploadResult.kt
+++ core/networking-upload/src/commonMain/kotlin/net/primal/core/networking/blossom/UploadResult.kt
@@ -1,10 +1,13 @@
 package net.primal.core.networking.blossom
 
+import net.primal.domain.nostr.Nip94Metadata
+
 sealed class UploadResult {
     data class Success(
         val remoteUrl: String,
         val originalFileSize: Long,
         val originalHash: String,
+        val nip94: Nip94Metadata? = null,
     ) : UploadResult()
 
     data class Failed(
