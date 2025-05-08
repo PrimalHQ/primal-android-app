aa0e99a76855466ff0b692276860d0dab8999ae3
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-05-08 02:07:32 +0200

    Fix crash when navigating to mediaItem
    Crash occurred because encoded URLs used as path segments can contain characters (like '/') that break NavController's routing. Switched to using query parameters instead.

--- app/src/main/kotlin/net/primal/android/navigation/PrimalAppNavigation.kt
+++ app/src/main/kotlin/net/primal/android/navigation/PrimalAppNavigation.kt
@@ -252,10 +252,10 @@ fun NavController.navigateToMediaGallery(
         "&$MEDIA_POSITION_MS=$mediaPositionMs",
 )
 
-fun NavController.navigateToMediaItem(mediaUrl: String) =
-    navigate(
-        route = "mediaItem/${mediaUrl.asUrlEncoded()}",
-    )
+fun NavController.navigateToMediaItem(mediaUrl: String) {
+    val encodedUrl = mediaUrl.asUrlEncoded()
+    navigate(route = "mediaItem?$MEDIA_URL=$encodedUrl")
+}
 
 fun NavController.navigateToExploreFeed(
     feedSpec: String,
@@ -775,10 +775,11 @@ fun SharedTransitionScope.PrimalAppNavigation(startDestination: String) {
         )
 
         mediaItem(
-            route = "mediaItem/{$MEDIA_URL}",
+            route = "mediaItem?$MEDIA_URL={$MEDIA_URL}",
             arguments = listOf(
                 navArgument(MEDIA_URL) {
                     type = NavType.StringType
+                    nullable = false
                 },
             ),
             navController = navController,
