b34ca28fdbe61f54beecc73d2991355a51922dbe
Aleksandar Ilic <aleksandar@appollo41.com>
2025-05-15 19:59:17 +0200

    Fix unused imports

--- app/src/main/kotlin/net/primal/android/articles/feed/ui/FeedArticleListItem.kt
+++ app/src/main/kotlin/net/primal/android/articles/feed/ui/FeedArticleListItem.kt
@@ -23,8 +23,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
--- app/src/main/kotlin/net/primal/android/settings/muted/tabs/MuteWords.kt
+++ app/src/main/kotlin/net/primal/android/settings/muted/tabs/MuteWords.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
