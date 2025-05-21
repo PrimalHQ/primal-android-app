3aeebd743297dbe107021ea9b2ab9ceb8279a473
Marko Kocic <marko@appollo41.com>
2025-05-21 17:13:08 +0200

    Implement `ConnectionIndicatorOverlay`

---
+++ app/src/main/kotlin/net/primal/android/core/compose/icons/primaliconpack/NoConnectionWhite.kt
@@ -0,0 +1,102 @@
+package net.primal.android.core.compose.icons.primaliconpack
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import net.primal.android.core.compose.icons.PrimalIcons
+
+val PrimalIcons.NoConnectionWhite: ImageVector
+    get() {
+        if (_NoConnectionWhite != null) {
+            return _NoConnectionWhite!!
+        }
+        _NoConnectionWhite = ImageVector.Builder(
+            name = "NoConnectionWhite",
+            defaultWidth = 20.dp,
+            defaultHeight = 20.dp,
+            viewportWidth = 20f,
+            viewportHeight = 20f
+        ).apply {
+            path(fill = SolidColor(Color(0xFFFA3C3C))) {
+                moveTo(10f, 1f)
+                curveTo(9.187f, 1f, 8.543f, 1.686f, 8.594f, 2.497f)
+                lineTo(9.203f, 12.252f)
+                curveTo(9.23f, 12.672f, 9.578f, 13f, 10f, 13f)
+                curveTo(10.422f, 13f, 10.771f, 12.672f, 10.797f, 12.252f)
+                lineTo(11.406f, 2.497f)
+                curveTo(11.457f, 1.686f, 10.813f, 1f, 10f, 1f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFFA3C3C))) {
+                moveTo(9.967f, 19f)
+                curveTo(11.071f, 19f, 11.967f, 18.105f, 11.967f, 17f)
+                curveTo(11.967f, 15.895f, 11.071f, 15f, 9.967f, 15f)
+                curveTo(8.862f, 15f, 7.967f, 15.895f, 7.967f, 17f)
+                curveTo(7.967f, 18.105f, 8.862f, 19f, 9.967f, 19f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFFFFFFF))) {
+                moveTo(12.347f, 11.491f)
+                lineTo(12.294f, 12.345f)
+                curveTo(12.278f, 12.595f, 12.223f, 12.834f, 12.134f, 13.056f)
+                curveTo(12.566f, 13.293f, 12.954f, 13.599f, 13.284f, 13.959f)
+                curveTo(13.619f, 14.324f, 14.222f, 14.384f, 14.544f, 14.007f)
+                curveTo(14.756f, 13.761f, 14.777f, 13.399f, 14.569f, 13.15f)
+                curveTo(13.972f, 12.437f, 13.212f, 11.865f, 12.347f, 11.491f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFFFFFFF))) {
+                moveTo(7.651f, 11.463f)
+                lineTo(7.706f, 12.345f)
+                curveTo(7.721f, 12.584f, 7.772f, 12.813f, 7.854f, 13.026f)
+                curveTo(7.401f, 13.267f, 6.994f, 13.584f, 6.649f, 13.959f)
+                curveTo(6.315f, 14.324f, 5.712f, 14.383f, 5.389f, 14.007f)
+                curveTo(5.178f, 13.76f, 5.156f, 13.399f, 5.365f, 13.15f)
+                curveTo(5.977f, 12.419f, 6.759f, 11.837f, 7.651f, 11.463f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFFFFFFF))) {
+                moveTo(12.51f, 8.887f)
+                curveTo(13.833f, 9.302f, 15.018f, 10.031f, 15.975f, 10.988f)
+                curveTo(16.302f, 11.314f, 16.845f, 11.323f, 17.146f, 10.972f)
+                curveTo(17.383f, 10.695f, 17.385f, 10.284f, 17.13f, 10.023f)
+                curveTo(15.905f, 8.764f, 14.35f, 7.828f, 12.606f, 7.352f)
+                lineTo(12.51f, 8.887f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFFFFFFF))) {
+                moveTo(7.393f, 7.334f)
+                lineTo(7.489f, 8.867f)
+                curveTo(6.139f, 9.278f, 4.931f, 10.015f, 3.958f, 10.987f)
+                curveTo(3.632f, 11.314f, 3.088f, 11.323f, 2.788f, 10.972f)
+                curveTo(2.55f, 10.695f, 2.548f, 10.284f, 2.803f, 10.023f)
+                curveTo(4.044f, 8.749f, 5.622f, 7.805f, 7.393f, 7.334f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFFFFFFF))) {
+                moveTo(12.764f, 4.814f)
+                curveTo(15.002f, 5.326f, 17.011f, 6.438f, 18.612f, 7.972f)
+                curveTo(18.936f, 8.282f, 19.458f, 8.275f, 19.749f, 7.935f)
+                curveTo(19.996f, 7.647f, 19.988f, 7.217f, 19.715f, 6.952f)
+                curveTo(17.854f, 5.146f, 15.493f, 3.852f, 12.859f, 3.299f)
+                lineTo(12.764f, 4.814f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFFFFFFF))) {
+                moveTo(7.14f, 3.286f)
+                lineTo(7.235f, 4.799f)
+                curveTo(4.971f, 5.304f, 2.938f, 6.423f, 1.321f, 7.972f)
+                curveTo(0.998f, 8.282f, 0.476f, 8.275f, 0.185f, 7.935f)
+                curveTo(-0.063f, 7.647f, -0.055f, 7.216f, 0.218f, 6.952f)
+                curveTo(2.095f, 5.131f, 4.48f, 3.831f, 7.14f, 3.286f)
+                close()
+            }
+        }.build()
+
+        return _NoConnectionWhite!!
+    }
+
+@Suppress("ObjectPropertyName")
+private var _NoConnectionWhite: ImageVector? = null
