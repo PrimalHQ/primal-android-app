ee77e41cf53c9065ee27e100f4870fca2692ab39
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-05-22 19:02:29 +0200

    Update db version

--- data/local-caching/src/commonMain/kotlin/net/primal/data/local/db/PrimalDatabase.kt
+++ data/local-caching/src/commonMain/kotlin/net/primal/data/local/db/PrimalDatabase.kt
@@ -85,7 +85,7 @@ import net.primal.data.local.serialization.ProfileTypeConverters
         ArticleFeedCrossRef::class,
         HighlightData::class,
     ],
-    version = 8,
+    version = 9,
     exportSchema = true,
 )
 @ConstructedBy(AppDatabaseConstructor::class)
