78a93cca112a86e86e0b6d71d47b2091e5779f3c
Marko Kocic <marko@appollo41.com>
2025-05-28 14:23:23 +0200

    Fix crash on failed network call in `EventZapsMediator`

--- data/repository-caching/src/commonMain/kotlin/net/primal/data/repository/events/paging/EventZapsMediator.kt
+++ data/repository-caching/src/commonMain/kotlin/net/primal/data/repository/events/paging/EventZapsMediator.kt
@@ -4,6 +4,7 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import io.github.aakira.napier.Napier
 import kotlinx.coroutines.withContext
 import net.primal.core.utils.coroutines.DispatcherProvider
 import net.primal.data.local.dao.events.EventZap
@@ -11,6 +12,7 @@ import net.primal.data.local.db.PrimalDatabase
 import net.primal.data.remote.api.events.EventStatsApi
 import net.primal.data.remote.api.events.model.EventZapsRequestBody
 import net.primal.data.repository.events.processors.persistToDatabaseAsTransaction
+import net.primal.domain.common.exception.NetworkException
 
 @ExperimentalPagingApi
 class EventZapsMediator(
@@ -21,17 +23,22 @@ class EventZapsMediator(
     private val database: PrimalDatabase,
 ) : RemoteMediator<Int, EventZap>() {
 
-    override suspend fun load(loadType: LoadType, state: PagingState<Int, EventZap>): MediatorResult {
+    override suspend fun load(loadType: LoadType, state: PagingState<Int, EventZap>): MediatorResult =
         withContext(dispatcherProvider.io()) {
-            val response = eventStatsApi.getEventZaps(
-                EventZapsRequestBody(
-                    eventId = eventId,
-                    userId = userId,
-                    limit = 100,
-                ),
-            )
-            response.persistToDatabaseAsTransaction(database)
+            try {
+                val response = eventStatsApi.getEventZaps(
+                    EventZapsRequestBody(
+                        eventId = eventId,
+                        userId = userId,
+                        limit = 100,
+                    ),
+                )
+                response.persistToDatabaseAsTransaction(database)
+
+                MediatorResult.Success(endOfPaginationReached = true)
+            } catch (error: NetworkException) {
+                Napier.w(throwable = error) { error.message ?: "" }
+                MediatorResult.Error(throwable = error)
+            }
         }
-        return MediatorResult.Success(endOfPaginationReached = true)
-    }
 }
