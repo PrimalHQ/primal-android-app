0cf73f49701feb41b3e44fc932313a02ad8eb48a
Aleksandar Ilic <aleksandar@appollo41.com>
2025-06-06 21:41:21 +0200

    Add KDoc to encodeToJsonString() and toNostrJsonObject()

--- core/utils/src/commonMain/kotlin/net/primal/core/utils/serialization/SerializationUtils.kt
+++ core/utils/src/commonMain/kotlin/net/primal/core/utils/serialization/SerializationUtils.kt
@@ -16,6 +16,12 @@ inline fun <reified T> String?.decodeFromJsonStringOrNull(): T? {
     return CommonJson.decodeFromStringOrNull(this)
 }
 
+/**
+ * Encodes an object to a JSON string using CommonJson serializer.
+ * 
+ * Note: When working with JsonObject, use `toString()` instead of this function
+ * to ensure proper formatting and compatibility with iOS.
+ */
 inline fun <reified T> T.encodeToJsonString(): String {
     return CommonJson.encodeToString(this)
 }
--- domain/nostr/src/commonMain/kotlin/net/primal/domain/nostr/serialization/NostrEventExt.kt
+++ domain/nostr/src/commonMain/kotlin/net/primal/domain/nostr/serialization/NostrEventExt.kt
@@ -8,6 +8,13 @@ import kotlinx.serialization.json.put
 import kotlinx.serialization.json.putJsonArray
 import net.primal.domain.nostr.NostrEvent
 
+
+/**
+ * Converts a NostrEvent to a JsonObject.
+ * 
+ * Note: When converting the returned JsonObject to a string, use `toString()` 
+ * instead of `encodeToString()` to ensure proper formatting and compatibility with iOS.
+ */
 fun NostrEvent.toNostrJsonObject(): JsonObject {
     val nostrEvent = this@toNostrJsonObject
     return buildJsonObject {
