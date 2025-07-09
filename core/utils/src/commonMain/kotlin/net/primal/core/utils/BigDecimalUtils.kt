39bf96c3a2e5719005cd9a10ccdf6517148a400e
Marko Kocic <marko@appollo41.com>
2025-07-09 17:39:17 +0200

    Implement `safeDivide` on `BigDecimal`

--- core/utils/src/commonMain/kotlin/net/primal/core/utils/BigDecimalUtils.kt
+++ core/utils/src/commonMain/kotlin/net/primal/core/utils/BigDecimalUtils.kt
@@ -1,6 +1,14 @@
 package net.primal.core.utils
 
 import com.ionspin.kotlin.bignum.decimal.BigDecimal
+import com.ionspin.kotlin.bignum.decimal.DecimalMode
+import com.ionspin.kotlin.bignum.decimal.RoundingMode
+
+fun BigDecimal.safeDivide(other: BigDecimal) =
+    divide(
+        other = other,
+        decimalMode = DecimalMode(decimalPrecision = 30, roundingMode = RoundingMode.ROUND_HALF_AWAY_FROM_ZERO),
+    )
 
 fun BigDecimal.toLong() = this.longValue(exactRequired = false)
 fun BigDecimal.toULong() = this.ulongValue(exactRequired = false)
