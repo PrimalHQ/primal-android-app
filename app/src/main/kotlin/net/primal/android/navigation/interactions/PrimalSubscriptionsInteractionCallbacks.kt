b2fefc08fa7d350a76bd4f8215822e12a42e337e
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-07-09 14:11:06 +0200

    Decompose NoteCallbacks into smaller classes

---
+++ app/src/main/kotlin/net/primal/android/navigation/interactions/PrimalSubscriptionsInteractionCallbacks.kt
@@ -0,0 +1,6 @@
+package net.primal.android.navigation.interactions
+
+data class PrimalSubscriptionsInteractionCallbacks(
+    val onGetPrimalPremiumClick: () -> Unit,
+    val onPrimalLegendsLeaderboardClick: () -> Unit,
+)
