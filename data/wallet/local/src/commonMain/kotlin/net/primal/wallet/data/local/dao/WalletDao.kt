9cc2a545af4ea89dc6c1d40b035615c32e57ca61
Marko Kocic <marko@appollo41.com>
2025-07-15 13:04:04 +0200

    Add support for nwc and active wallets to Wallet Database

--- data/wallet/local/src/commonMain/kotlin/net/primal/wallet/data/local/dao/WalletDao.kt
+++ data/wallet/local/src/commonMain/kotlin/net/primal/wallet/data/local/dao/WalletDao.kt
@@ -4,20 +4,51 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import net.primal.domain.wallet.WalletType
+import androidx.room.Transaction
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WalletDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsertActiveWallet(data: ActiveWalletData)
+
+    @Query("DELETE FROM ActiveWalletData WHERE userId = :userId")
+    suspend fun clearActiveWallet(userId: String)
+
+    @Query("SELECT * FROM ActiveWalletData WHERE userId = :userId")
+    suspend fun getActiveWallet(userId: String): ActiveWallet?
+
+    @Transaction
+    @Query("SELECT * FROM ActiveWalletData WHERE userId = :userId")
+    fun observeActiveWallet(userId: String): Flow<ActiveWallet?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsertWalletInfo(info: WalletInfo)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun upsertWalletBalance(balance: WalletBalance)
+    suspend fun upsertPrimalWalletData(data: PrimalWalletData)
 
-    @Query("SELECT * FROM WalletInfo WHERE userId = :userId")
-    suspend fun findWalletInfo(userId: String): List<WalletInfo>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsertNostrWalletData(data: NostrWalletData)
 
-    @Query("SELECT * FROM WalletInfo WHERE userId = :userId AND type = :type")
-    suspend fun findWalletInfo(userId: String, type: WalletType): WalletInfo?
+    @Query(
+        """
+        UPDATE WalletInfo 
+        SET balanceInBtc = :balanceInBtc, maxBalanceInBtc = :maxBalanceInBtc,
+         lastUpdatedAt = CAST(strftime('%s','now') AS INTEGER) 
+        WHERE walletId = :walletId
+        """,
+    )
+    suspend fun updateWalletBalance(
+        walletId: String,
+        balanceInBtc: Double,
+        maxBalanceInBtc: Double?,
+    )
+
+    @Query("SELECT * FROM WalletInfo WHERE walletId = :walletId")
+    suspend fun findWalletInfo(walletId: String): WalletInfo?
+
+    @Query("SELECT * FROM WalletInfo WHERE walletId = :walletId")
+    suspend fun findWallet(walletId: String): Wallet?
 }
