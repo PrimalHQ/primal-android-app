ae68f169f764f1476d4be018ea66469d9a7cd588
Marko Kocic <marko@appollo41.com>
2025-07-16 13:03:05 +0200

    Migrate `WalletSettings` to Wallet Database from `UserAccount`

--- app/src/main/kotlin/net/primal/android/settings/wallet/settings/WalletSettingsContract.kt
+++ app/src/main/kotlin/net/primal/android/settings/wallet/settings/WalletSettingsContract.kt
@@ -1,26 +1,21 @@
 package net.primal.android.settings.wallet.settings
 
 import net.primal.android.settings.wallet.domain.NwcConnectionInfo
-import net.primal.android.user.domain.WalletPreference
-import net.primal.core.networking.nwc.model.NostrWalletConnect
+import net.primal.domain.wallet.Wallet
 
 interface WalletSettingsContract {
     data class UiState(
-        val wallet: NostrWalletConnect? = null,
-        val walletPreference: WalletPreference = WalletPreference.Undefined,
-        val userLightningAddress: String? = null,
-        val maxWalletBalanceInBtc: String? = null,
-        val spamThresholdAmountInSats: Long? = null,
+        val wallet: Wallet? = null,
+        val preferPrimalWallet: Boolean? = null,
         val nwcConnectionsInfo: List<NwcConnectionInfo> = emptyList(),
         val connectionsState: ConnectionsState = ConnectionsState.Loading,
-        val isPrimalWalletActivated: Boolean = false,
     )
 
     sealed class UiEvent {
         data object DisconnectWallet : UiEvent()
         data object RequestFetchWalletConnections : UiEvent()
         data class RevokeConnection(val nwcPubkey: String) : UiEvent()
-        data class UpdateWalletPreference(val walletPreference: WalletPreference) : UiEvent()
+        data class UpdatePreferPrimalWallet(val value: Boolean) : UiEvent()
         data class UpdateMinTransactionAmount(val amountInSats: Long) : UiEvent()
         data class ConnectExternalWallet(val connectionLink: String) : UiEvent()
     }
