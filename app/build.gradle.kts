5f20b588123a1316f31a073861df03d7d48e3162
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-07-17 18:41:37 +0200

    Add the coil-video Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -321,6 +321,7 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.coil.svg)
     implementation(libs.coil.gif)
+    implementation(libs.coil.video)
     implementation(libs.coil.network)
     implementation(libs.telephoto.zoomable.image)
     implementation(libs.telephoto.zoomable.peek.overlay)
