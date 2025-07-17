822b88b8be9197ba839fccdcdc463b0b27a65d10
Marko Kocic <marko@appollo41.com>
2025-07-17 20:08:03 +0200

    Migrate from `NwcResult` to `Result`

--- data/wallet/remote-nwc/src/commonMain/kotlin/net/primal/core/networking/nwc/NwcResult.kt
+++
@@ -1,6 +0,0 @@
-package net.primal.core.networking.nwc
-
-sealed class NwcResult<T> {
-    data class Success<T>(val result: T) : NwcResult<T>()
-    data class Failure<T>(val error: Exception) : NwcResult<T>()
-}
