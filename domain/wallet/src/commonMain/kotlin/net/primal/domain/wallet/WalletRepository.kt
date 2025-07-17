3a29cde4205fa16aca73e372b9991bb7d71884a3
Marko Kocic <marko@appollo41.com>
2025-07-17 20:09:22 +0200

    Return `Result` from `WalletService` and migrated `WalletRepository` functions

--- domain/wallet/src/commonMain/kotlin/net/primal/domain/wallet/WalletRepository.kt
+++ domain/wallet/src/commonMain/kotlin/net/primal/domain/wallet/WalletRepository.kt
@@ -2,7 +2,7 @@ package net.primal.domain.wallet
 
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
-import net.primal.domain.common.exception.NetworkException
+import net.primal.core.utils.Result
 
 interface WalletRepository {
 
@@ -10,8 +10,7 @@ interface WalletRepository {
 
     suspend fun upsertWalletSettings(walletId: String, spamThresholdAmountInSats: Long)
 
-    @Throws(NetworkException::class, kotlin.coroutines.cancellation.CancellationException::class)
-    suspend fun fetchWalletBalance(walletId: String)
+    suspend fun fetchWalletBalance(walletId: String): Result<Unit>
 
     fun latestTransactions(userId: String): Flow<PagingData<TransactionWithProfile>>
 
