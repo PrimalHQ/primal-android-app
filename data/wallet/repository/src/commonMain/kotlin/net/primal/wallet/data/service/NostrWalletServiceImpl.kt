e40610ae92773a3caabceafd5d09164fea15acce
Marko Kocic <marko@appollo41.com>
2025-07-17 20:09:22 +0200

    Return `Result` from `WalletService` and migrated `WalletRepository` functions

--- data/wallet/repository/src/commonMain/kotlin/net/primal/wallet/data/service/NostrWalletServiceImpl.kt
+++ data/wallet/repository/src/commonMain/kotlin/net/primal/wallet/data/service/NostrWalletServiceImpl.kt
@@ -3,28 +3,31 @@ package net.primal.wallet.data.service
 import net.primal.core.networking.nwc.NwcClientFactory
 import net.primal.core.networking.nwc.model.NostrWalletConnect
 import net.primal.core.utils.CurrencyConversionUtils.msatsToBtc
+import net.primal.core.utils.Result
+import net.primal.core.utils.runCatching
 import net.primal.domain.wallet.Wallet
 import net.primal.domain.wallet.WalletService
 import net.primal.domain.wallet.model.WalletBalanceResult
 import net.primal.wallet.data.service.mappers.asNO
 
 class NostrWalletServiceImpl : WalletService {
-    override suspend fun fetchWalletBalance(wallet: Wallet): WalletBalanceResult {
-        require(wallet is Wallet.NWC) { "Wallet is not type NWC but `NostrWalletService` called." }
+    override suspend fun fetchWalletBalance(wallet: Wallet): Result<WalletBalanceResult> =
+        runCatching {
+            require(wallet is Wallet.NWC) { "Wallet is not type NWC but `NostrWalletService` called." }
 
-        val client = NwcClientFactory.createNwcApiClient(
-            nwcData = NostrWalletConnect(
-                lightningAddress = wallet.lightningAddress,
-                relays = wallet.relays,
-                pubkey = wallet.walletId,
-                keypair = wallet.keypair.asNO(),
-            ),
-        )
-        val response = client.getBalance().getOrThrow()
+            val client = NwcClientFactory.createNwcApiClient(
+                nwcData = NostrWalletConnect(
+                    lightningAddress = wallet.lightningAddress,
+                    relays = wallet.relays,
+                    pubkey = wallet.walletId,
+                    keypair = wallet.keypair.asNO(),
+                ),
+            )
+            val response = client.getBalance().getOrThrow()
 
-        return WalletBalanceResult(
-            balanceInBtc = response.balance.msatsToBtc(),
-            maxBalanceInBtc = null,
-        )
-    }
+            WalletBalanceResult(
+                balanceInBtc = response.balance.msatsToBtc(),
+                maxBalanceInBtc = null,
+            )
+        }
 }
--- domain/wallet/src/commonMain/kotlin/net/primal/domain/wallet/WalletService.kt
+++ domain/wallet/src/commonMain/kotlin/net/primal/domain/wallet/WalletService.kt
@@ -1,9 +1,8 @@
 package net.primal.domain.wallet
 
-import net.primal.domain.common.exception.NetworkException
+import net.primal.core.utils.Result
 import net.primal.domain.wallet.model.WalletBalanceResult
 
 interface WalletService {
-    @Throws(NetworkException::class, kotlin.coroutines.cancellation.CancellationException::class)
-    suspend fun fetchWalletBalance(wallet: Wallet): WalletBalanceResult
+    suspend fun fetchWalletBalance(wallet: Wallet): Result<WalletBalanceResult>
 }
