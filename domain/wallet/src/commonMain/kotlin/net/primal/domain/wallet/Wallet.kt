e0c6d015a99802753a3c5dcdfb5cc7252ec58322
Marko Kocic <marko@appollo41.com>
2025-07-18 18:02:07 +0200

    Add `userId` to Wallet domain object

--- domain/wallet/src/commonMain/kotlin/net/primal/domain/wallet/Wallet.kt
+++ domain/wallet/src/commonMain/kotlin/net/primal/domain/wallet/Wallet.kt
@@ -2,6 +2,7 @@ package net.primal.domain.wallet
 
 sealed class Wallet(
     open val walletId: String,
+    open val userId: String,
     open val lightningAddress: String?,
     open val spamThresholdAmountInSats: Long,
     open val balanceInBtc: Double? = null,
@@ -10,6 +11,7 @@ sealed class Wallet(
 ) {
     data class NWC(
         override val walletId: String,
+        override val userId: String,
         override val lightningAddress: String?,
         override val spamThresholdAmountInSats: Long,
         override val balanceInBtc: Double?,
@@ -17,15 +19,32 @@ sealed class Wallet(
         override val lastUpdatedAt: Long?,
         val relays: List<String>,
         val keypair: NostrWalletKeypair,
-    ) : Wallet(walletId, lightningAddress, spamThresholdAmountInSats, balanceInBtc, maxBalanceInBtc, lastUpdatedAt)
+    ) : Wallet(
+        walletId = walletId,
+        userId = userId,
+        lightningAddress = lightningAddress,
+        spamThresholdAmountInSats = spamThresholdAmountInSats,
+        balanceInBtc = balanceInBtc,
+        maxBalanceInBtc = maxBalanceInBtc,
+        lastUpdatedAt = lastUpdatedAt,
+    )
 
     data class Primal(
         override val walletId: String,
+        override val userId: String,
         override val lightningAddress: String?,
         override val spamThresholdAmountInSats: Long,
         override val balanceInBtc: Double?,
         override val maxBalanceInBtc: Double?,
         override val lastUpdatedAt: Long?,
         val kycLevel: WalletKycLevel,
-    ) : Wallet(walletId, lightningAddress, spamThresholdAmountInSats, balanceInBtc, maxBalanceInBtc, lastUpdatedAt)
+    ) : Wallet(
+        walletId = walletId,
+        userId = userId,
+        lightningAddress = lightningAddress,
+        spamThresholdAmountInSats = spamThresholdAmountInSats,
+        balanceInBtc = balanceInBtc,
+        maxBalanceInBtc = maxBalanceInBtc,
+        lastUpdatedAt = lastUpdatedAt,
+    )
 }
