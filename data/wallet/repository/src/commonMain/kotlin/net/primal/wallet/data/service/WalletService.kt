aced2c1a7fccee5dc5ff7500369dbf74978d3914
Marko Kocic <marko@appollo41.com>
2025-07-18 18:22:40 +0200

    Migrate existing transactions logic to support NWC transactions

--- data/wallet/repository/src/commonMain/kotlin/net/primal/wallet/data/service/WalletService.kt
+++ data/wallet/repository/src/commonMain/kotlin/net/primal/wallet/data/service/WalletService.kt
@@ -2,8 +2,12 @@ package net.primal.wallet.data.service
 
 import net.primal.core.utils.Result
 import net.primal.domain.wallet.Wallet
+import net.primal.wallet.data.model.TransactionsRequest
 import net.primal.domain.wallet.model.WalletBalanceResult
+import net.primal.wallet.data.model.Transaction
 
-interface WalletService {
+internal interface WalletService {
     suspend fun fetchWalletBalance(wallet: Wallet): Result<WalletBalanceResult>
+
+    suspend fun fetchTransactions(wallet: Wallet, request: TransactionsRequest): Result<List<Transaction>>
 }
--- domain/wallet/src/commonMain/kotlin/net/primal/domain/wallet/TransactionProfileData.kt
+++
@@ -1,8 +0,0 @@
-package net.primal.domain.wallet
-
-import net.primal.domain.profile.ProfileData
-
-data class TransactionWithProfile(
-    val transaction: WalletTransaction,
-    val otherProfileData: ProfileData?,
-)
