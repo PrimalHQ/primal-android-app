da1f91967f9c068a9b5c32113ec01fc7d8b8026d
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-07-18 18:32:04 +0200

    Correct image size calculation for missing variants

--- app/src/main/kotlin/net/primal/android/events/ui/CdnResourceVariantExt.kt
+++ app/src/main/kotlin/net/primal/android/events/ui/CdnResourceVariantExt.kt
@@ -2,32 +2,26 @@ package net.primal.android.events.ui
 
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import kotlin.math.min
 import net.primal.domain.links.CdnResourceVariant
 
 fun List<CdnResourceVariant>?.findNearestOrNull(maxWidthPx: Int): CdnResourceVariant? {
-    return this?.sortedBy { it.width }?.find { it.width >= maxWidthPx }
-        ?: this?.maxByOrNull { it.width }
+    if (this.isNullOrEmpty()) return null
+    return this.sortedBy { it.width }.find { it.width >= maxWidthPx }
+        ?: this.maxByOrNull { it.width }
 }
 
-fun CdnResourceVariant?.calculateImageSize(
-    maxWidth: Int,
-    maxHeight: Int,
-    density: Float,
-): DpSize {
-    if (this == null) return DpSize(maxWidth.dp, maxWidth.dp)
+fun CdnResourceVariant?.calculateImageSize(maxWidth: Int, maxHeight: Int): DpSize {
+    if (this == null || this.width == 0 || this.height == 0) {
+        val sideLength = min(maxWidth, maxHeight)
+        return DpSize(width = sideLength.dp, height = sideLength.dp)
+    }
 
-    val variantWidth = (width / density).toInt()
-    val variantHeight = (height / density).toInt()
-
-    val heightByAspectRation = ((maxWidth * variantHeight) / variantWidth)
+    val heightForMaxWidth = (maxWidth.toFloat() * this.height.toFloat()) / this.width.toFloat()
+    val finalHeight = min(heightForMaxWidth, maxHeight.toFloat())
 
     return DpSize(
-        width = when {
-            else -> maxWidth.dp
-        },
-        height = when {
-            variantHeight == 0 -> maxWidth.dp
-            else -> minOf(heightByAspectRation, maxHeight).dp
-        },
+        width = maxWidth.dp,
+        height = finalHeight.dp,
     )
 }
