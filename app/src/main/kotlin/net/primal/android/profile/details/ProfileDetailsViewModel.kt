91dffef55baf4f6518c5001f0622877e4aa3728a
Aleksandar Ilic <aleksandar@appollo41.com>
2025-07-31 12:49:31 +0200

    Fix primal nprofile1 deep-link

--- app/src/main/kotlin/net/primal/android/profile/details/ProfileDetailsViewModel.kt
+++ app/src/main/kotlin/net/primal/android/profile/details/ProfileDetailsViewModel.kt
@@ -613,11 +613,7 @@ class ProfileDetailsViewModel @Inject constructor(
     private fun String.parseForProfileId(): String? =
         when {
             this.startsWith("npub") -> runCatching { bech32ToHexOrThrow() }.getOrNull()
-            this.startsWith("nprofile1") -> {
-                val pubkey = Nip19TLV.parseUriAsNprofileOrNull(this)?.pubkey
-                runCatching { pubkey?.bech32ToHexOrThrow() }.getOrNull()
-            }
-
+            this.startsWith("nprofile1") -> Nip19TLV.parseUriAsNprofileOrNull(this)?.pubkey
             else -> this
         }
 }
