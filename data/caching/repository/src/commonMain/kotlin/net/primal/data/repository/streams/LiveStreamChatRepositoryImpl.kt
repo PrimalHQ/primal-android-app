5b7f51863bc271b5acea87cc5a33fb28cda2d6b9
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-08-07 17:05:34 +0200

    Implement support for sending chat messages

--- data/caching/repository/src/commonMain/kotlin/net/primal/data/repository/streams/LiveStreamChatRepositoryImpl.kt
+++ data/caching/repository/src/commonMain/kotlin/net/primal/data/repository/streams/LiveStreamChatRepositoryImpl.kt
@@ -2,8 +2,14 @@ package net.primal.data.repository.streams
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import net.primal.core.utils.serialization.encodeToJsonString
+import net.primal.data.local.dao.streams.StreamChatMessageData
 import net.primal.data.local.db.PrimalDatabase
 import net.primal.data.repository.mappers.local.asChatMessageDO
+import net.primal.domain.nostr.NostrEventKind
+import net.primal.domain.nostr.NostrUnsignedEvent
+import net.primal.domain.nostr.asReplaceableEventTag
+import net.primal.domain.nostr.findFirstClient
 import net.primal.domain.publisher.PrimalPublisher
 import net.primal.domain.streams.chat.ChatMessage
 import net.primal.domain.streams.chat.LiveStreamChatRepository
@@ -25,14 +31,26 @@ class LiveStreamChatRepositoryImpl(
         streamATag: String,
         content: String,
     ) {
-//        val unsignedEvent = NostrUnsignedEvent(
-//            pubKey = userId,
-//            kind = NostrEventKind.LiveChatMessage.value,
-//            tags = listOf(
-//                streamATag.asReplaceableEventTag(marker = "root"),
-//            ),
-//            content = content,
-//        )
-//        primalPublisher.signPublishImportNostrEvent(unsignedEvent)
+        val unsignedEvent = NostrUnsignedEvent(
+            pubKey = userId,
+            kind = NostrEventKind.ChatMessage.value,
+            tags = listOf(
+                streamATag.asReplaceableEventTag(marker = "root"),
+            ),
+            content = content,
+        )
+        val publishedMessage = primalPublisher.signPublishImportNostrEvent(unsignedEvent)
+
+        database.streamChats().upsert(
+            StreamChatMessageData(
+                messageId = publishedMessage.nostrEvent.id,
+                streamATag = streamATag,
+                authorId = userId,
+                createdAt = publishedMessage.nostrEvent.createdAt,
+                content = content,
+                raw = publishedMessage.nostrEvent.encodeToJsonString(),
+                client = publishedMessage.nostrEvent.tags.findFirstClient(),
+            ),
+        )
     }
 }
