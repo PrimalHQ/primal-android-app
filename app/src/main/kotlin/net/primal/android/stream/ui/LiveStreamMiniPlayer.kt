e14d8d67a20521fc4812068ec47a9a07fd94683f
Marko Kocic <marko@appollo41.com>
2025-08-15 12:51:11 +0200

    Adjust MiniPlayer to handle Keyboard changes and `StreamMode.Hidden`
    MiniPlayer now starts at `bottomStart` and icon tint was changed.

--- app/src/main/kotlin/net/primal/android/stream/ui/LiveStreamMiniPlayer.kt
+++ app/src/main/kotlin/net/primal/android/stream/ui/LiveStreamMiniPlayer.kt
@@ -2,8 +2,10 @@ package net.primal.android.stream.ui
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
@@ -11,12 +13,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
@@ -24,7 +25,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -35,12 +38,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
@@ -50,12 +51,14 @@ import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import net.primal.android.R
+import net.primal.android.core.compose.KeyboardState
 import net.primal.android.core.compose.PrimalLoadingSpinner
 import net.primal.android.core.compose.animatableSaver
 import net.primal.android.core.compose.icons.PrimalIcons
 import net.primal.android.core.compose.icons.primaliconpack.VideoCloseMini
 import net.primal.android.core.compose.icons.primaliconpack.VideoPauseMini
 import net.primal.android.core.compose.icons.primaliconpack.VideoPlayMini
+import net.primal.android.core.compose.rememberKeyboardState
 import net.primal.android.core.video.toggle
 import net.primal.android.stream.LiveStreamContract
 import net.primal.android.stream.player.LocalStreamState
@@ -70,6 +73,7 @@ private val springSpec = spring<Float>(
     stiffness = Spring.StiffnessLow,
 )
 
+@OptIn(ExperimentalLayoutApi::class)
 @UnstableApi
 @Composable
 fun LiveStreamMiniPlayer(
@@ -85,10 +89,10 @@ fun LiveStreamMiniPlayer(
     val displayMetrics = LocalContext.current.resources.displayMetrics
     val screenWidthPx = displayMetrics.widthPixels
     val screenHeightPx = displayMetrics.heightPixels
+    val playerWidth = screenWidthPx / 2
+    val playerHeight = playerWidth / (VIDEO_ASPECT_RATIO_WIDTH / VIDEO_ASPECT_RATIO_HEIGHT)
     val statusBarHeight = WindowInsets.statusBars.getTop(localDensity)
     val paddingPx = with(localDensity) { PADDING.toPx() }
-
-    var overlayPosition by remember { mutableStateOf(IntSize.Zero) }
 
     var controlsOverlayVisibility by remember { mutableStateOf(false) }
 
@@ -100,77 +104,85 @@ fun LiveStreamMiniPlayer(
             }
         }
     }
-    fun minSafeY() =
-        if (streamState.topBarHeight == 0) {
-            statusBarHeight + paddingPx
-        } else {
-            streamState.topBarHeight + paddingPx
+    val minSafeY by remember {
+        derivedStateOf {
+            if (streamState.topBarHeight == 0) {
+                statusBarHeight + paddingPx
+            } else {
+                streamState.topBarHeight + paddingPx
+            }
         }
+    }
 
-    fun maxSafeY() = screenHeightPx - streamState.bottomBarHeight - overlayPosition.height - paddingPx
+    val maxSafeY by remember {
+        derivedStateOf { screenHeightPx - streamState.bottomBarHeight - playerHeight - paddingPx }
+    }
 
     val offsetX = rememberSaveable(saver = animatableSaver()) { Animatable(paddingPx) }
-    val offsetY = rememberSaveable(saver = animatableSaver()) { Animatable(minSafeY()) }
+    val offsetY = rememberSaveable(saver = animatableSaver()) { Animatable(maxSafeY) }
 
-    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
-        val playerWidth = this.maxWidth / 2
-        val playerWidthPx = with(localDensity) { playerWidth.toPx() }
+    adjustPositionWithKeyboard(offsetY, screenHeightPx, playerHeight, paddingPx)
 
-        LaunchedEffect(streamState.bottomBarHeight, streamState.topBarHeight) {
-            val targetY = offsetY.value.coerceIn(
-                minimumValue = minSafeY(),
-                maximumValue = maxSafeY(),
-            )
-            launch { offsetY.animateTo(targetValue = targetY, animationSpec = springSpec) }
-        }
-
-        Box(
-            modifier = modifier
-                .offset { IntOffset(offsetX.value.toInt(), offsetY.value.toInt()) }
-                .pointerInput(Unit) {
-                    detectDragGestures(
-                        onDragEnd = {
-                            val maxOffsetX = (screenWidthPx - playerWidthPx - paddingPx).coerceAtLeast(0f)
-
-                            val snapToRight = offsetX.value < screenWidthPx / 4
-
-                            val targetX = if (snapToRight) paddingPx else maxOffsetX
-
-                            val targetY = offsetY.value.coerceIn(
-                                minimumValue = minSafeY(),
-                                maximumValue = maxSafeY(),
-                            )
-                            scope.launch { offsetX.animateTo(targetX, animationSpec = springSpec) }
-                            scope.launch { offsetY.animateTo(targetY, animationSpec = springSpec) }
-                        },
-                        onDrag = { change, dragAmount ->
-                            change.consume()
-                            scope.launch {
-                                offsetX.snapTo(offsetX.value + dragAmount.x)
-                                offsetY.snapTo(offsetY.value + dragAmount.y)
-                            }
-                        },
-                    )
-                }
-                .onGloballyPositioned { coordinates -> overlayPosition = coordinates.size }
-                .size(
-                    width = playerWidth,
-                    height = playerWidth / (VIDEO_ASPECT_RATIO_WIDTH / VIDEO_ASPECT_RATIO_HEIGHT),
+    AnimatedVisibility(
+        visible = !streamState.isHidden(),
+        enter = fadeIn(animationSpec = tween(durationMillis = 400)),
+        exit = fadeOut(animationSpec = tween(durationMillis = 50)),
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            LaunchedEffect(streamState.bottomBarHeight, streamState.topBarHeight) {
+                val targetY = offsetY.value.coerceIn(
+                    minimumValue = minSafeY,
+                    maximumValue = maxSafeY,
                 )
-                .imePadding()
-                .clip(AppTheme.shapes.large)
-                .background(AppTheme.colorScheme.background)
-                .clickable { controlsOverlayVisibility = true },
-        ) {
-            PlayerBox(exoPlayer = exoPlayer, state = state)
+                launch { offsetY.animateTo(targetValue = targetY, animationSpec = springSpec) }
+            }
 
-            PlayerControls(
-                controlsOverlayVisibility = controlsOverlayVisibility,
-                onTogglePlayer = { exoPlayer.toggle() },
-                isPlaying = exoPlayer.isPlaying,
-                onStopStream = onStopStream,
-                onExpandStream = onExpandStream,
-            )
+            Box(
+                modifier = modifier
+                    .offset { IntOffset(offsetX.value.toInt(), offsetY.value.toInt()) }
+                    .pointerInput(Unit) {
+                        detectDragGestures(
+                            onDragEnd = {
+                                val maxOffsetX = (screenWidthPx - playerWidth - paddingPx).coerceAtLeast(0f)
+
+                                val snapToRight = offsetX.value < screenWidthPx / 4
+
+                                val targetX = if (snapToRight) paddingPx else maxOffsetX
+
+                                val targetY = offsetY.value.coerceIn(
+                                    minimumValue = minSafeY,
+                                    maximumValue = maxSafeY,
+                                )
+                                scope.launch { offsetX.animateTo(targetX, animationSpec = springSpec) }
+                                scope.launch { offsetY.animateTo(targetY, animationSpec = springSpec) }
+                            },
+                            onDrag = { change, dragAmount ->
+                                change.consume()
+                                scope.launch {
+                                    offsetX.snapTo(offsetX.value + dragAmount.x)
+                                    offsetY.snapTo(offsetY.value + dragAmount.y)
+                                }
+                            },
+                        )
+                    }
+                    .size(
+                        width = with(localDensity) { playerWidth.toDp() },
+                        height = with(localDensity) { playerHeight.toDp() },
+                    )
+                    .clip(AppTheme.shapes.large)
+                    .background(AppTheme.colorScheme.background)
+                    .clickable { controlsOverlayVisibility = true },
+            ) {
+                PlayerBox(exoPlayer = exoPlayer, state = state)
+
+                PlayerControls(
+                    controlsOverlayVisibility = controlsOverlayVisibility,
+                    onTogglePlayer = { exoPlayer.toggle() },
+                    isPlaying = exoPlayer.isPlaying,
+                    onStopStream = onStopStream,
+                    onExpandStream = onExpandStream,
+                )
+            }
         }
     }
 }
@@ -210,7 +222,7 @@ private fun PlayerControls(
                 IconButton(onClick = onTogglePlayer) {
                     Icon(
                         modifier = Modifier.size(36.dp),
-                        tint = AppTheme.extraColorScheme.onSurfaceVariantAlt2,
+                        tint = Color.White,
                         imageVector = playPauseIcon,
                         contentDescription = stringResource(id = R.string.accessibility_play_pause),
                     )
@@ -218,7 +230,7 @@ private fun PlayerControls(
                 IconButton(onClick = onStopStream) {
                     Icon(
                         modifier = Modifier.size(36.dp),
-                        tint = AppTheme.extraColorScheme.onSurfaceVariantAlt2,
+                        tint = Color.White,
                         imageVector = PrimalIcons.VideoCloseMini,
                         contentDescription = null,
                     )
@@ -226,6 +238,41 @@ private fun PlayerControls(
             }
         }
     }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+private fun adjustPositionWithKeyboard(
+    offsetY: Animatable<Float, AnimationVector1D>,
+    screenHeightPx: Int,
+    playerHeight: Float,
+    paddingPx: Float,
+): KeyboardState {
+    val keyboardState by rememberKeyboardState()
+
+    var beforeKeyboardOffsetY by remember { mutableFloatStateOf(0f) }
+    var returnOffsetYAfterKeyboard by remember { mutableStateOf(false) }
+
+    fun maxSafeKeyboardY() = screenHeightPx - playerHeight - paddingPx - keyboardState.heightPx
+
+    LaunchedEffect(keyboardState) {
+        if (keyboardState.isVisible) {
+            if (offsetY.value > maxSafeKeyboardY()) {
+                if (!returnOffsetYAfterKeyboard) {
+                    beforeKeyboardOffsetY = offsetY.value
+                }
+                returnOffsetYAfterKeyboard = true
+                offsetY.snapTo(maxSafeKeyboardY())
+            }
+        } else {
+            if (returnOffsetYAfterKeyboard) {
+                launch { offsetY.animateTo(targetValue = beforeKeyboardOffsetY, animationSpec = springSpec) }
+                returnOffsetYAfterKeyboard = false
+            }
+        }
+    }
+
+    return keyboardState
 }
 
 @UnstableApi
