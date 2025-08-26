85a6c8b027559f9cc2786e6ce02c19570d7e5607
Marko Kocic <marko@appollo41.com>
2025-08-26 11:42:59 +0200

    Implement `parametrized_replaceable_event(s)` endpoints

--- data/caching/remote/src/commonMain/kotlin/net/primal/data/remote/api/events/EventStatsApi.kt
+++ data/caching/remote/src/commonMain/kotlin/net/primal/data/remote/api/events/EventStatsApi.kt
@@ -1,13 +1,21 @@
 package net.primal.data.remote.api.events
 
+import net.primal.core.utils.Result
 import net.primal.data.remote.api.events.model.EventActionsRequestBody
 import net.primal.data.remote.api.events.model.EventActionsResponse
 import net.primal.data.remote.api.events.model.EventZapsRequestBody
 import net.primal.data.remote.api.events.model.EventZapsResponse
+import net.primal.data.remote.api.events.model.ReplaceableEventRequest
+import net.primal.data.remote.api.events.model.ReplaceableEventResponse
+import net.primal.data.remote.api.events.model.ReplaceableEventsRequest
 
 interface EventStatsApi {
 
     suspend fun getEventZaps(body: EventZapsRequestBody): EventZapsResponse
 
     suspend fun getEventActions(body: EventActionsRequestBody): EventActionsResponse
+
+    suspend fun getReplaceableEvent(body: ReplaceableEventRequest): Result<ReplaceableEventResponse>
+
+    suspend fun getReplaceableEvents(body: ReplaceableEventsRequest): Result<ReplaceableEventResponse>
 }
