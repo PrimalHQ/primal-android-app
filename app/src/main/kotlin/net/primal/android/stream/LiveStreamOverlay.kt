8c3a071dff410d64b085066c05a8483095925a05
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-09-02 03:14:00 +0200

    Pause player when app enters background

--- app/src/main/kotlin/net/primal/android/stream/LiveStreamOverlay.kt
+++ app/src/main/kotlin/net/primal/android/stream/LiveStreamOverlay.kt
@@ -11,6 +11,7 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
@@ -20,6 +21,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
@@ -116,6 +120,20 @@ private fun LiveStreamOverlay(
             viewModel.setEvent(UiEvent.OnVideoUnavailable)
         },
     )
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner, exoPlayer) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_STOP) {
+                exoPlayer.pause()
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
 
     BackHandler(enabled = streamState.mode is StreamMode.Expanded) {
         if (uiState.value.playerState.isVideoFinished) {
