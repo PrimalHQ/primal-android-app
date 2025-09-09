7dab3807a1f4524881fdc3cd1629721cf6e06363
Marko Kocic <marko@appollo41.com>
2025-09-09 13:09:38 +0200

    Implement `PrimalMediaSessionService` to provide background playback

--- app/src/main/kotlin/net/primal/android/core/video/ExoPlayerExt.kt
+++ app/src/main/kotlin/net/primal/android/core/video/ExoPlayerExt.kt
@@ -1,15 +1,9 @@
 package net.primal.android.core.video
 
 import android.content.Context
-import androidx.annotation.OptIn
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.media3.common.AudioAttributes
-import androidx.media3.common.C
-import androidx.media3.common.PlaybackException
-import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DefaultHttpDataSource
 import androidx.media3.datasource.cache.CacheDataSource
@@ -18,7 +12,7 @@ import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
-import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaController
 import net.primal.android.core.video.PlaybackConstants.BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MSEC
 import net.primal.android.core.video.PlaybackConstants.BUFFER_FOR_PLAYBACK_MSEC
 import net.primal.android.core.video.PlaybackConstants.MAX_BITRATE
@@ -26,7 +20,6 @@ import net.primal.android.core.video.PlaybackConstants.MAX_BUFFER_MSEC
 import net.primal.android.core.video.PlaybackConstants.MAX_VIDEO_HEIGHT
 import net.primal.android.core.video.PlaybackConstants.MAX_VIDEO_WIDTH
 import net.primal.android.core.video.PlaybackConstants.MIN_BUFFER_MSEC
-import net.primal.domain.nostr.Naddr
 
 private object PlaybackConstants {
     const val MAX_VIDEO_WIDTH = 1280
@@ -88,66 +81,7 @@ fun initializePlayer(context: Context): ExoPlayer {
         .build()
 }
 
-@Composable
-fun rememberPrimalStreamExoPlayer(
-    streamNaddr: Naddr,
-    onIsPlayingChanged: (ExoPlayer, Boolean) -> Unit,
-    onPlaybackStateChanged: (ExoPlayer, Int) -> Unit,
-    onPlayerError: (errorCode: Int) -> Unit,
-): ExoPlayer {
-    val context = LocalContext.current
-    val exoPlayer = remember(streamNaddr) {
-        val audioAttributes = AudioAttributes.Builder()
-            .setUsage(C.USAGE_MEDIA)
-            .setContentType(C.AUDIO_CONTENT_TYPE_MOVIE)
-            .build()
-        ExoPlayer.Builder(context)
-            .setAudioAttributes(audioAttributes, true)
-            .build()
-    }
-
-    DisposableEffect(exoPlayer, onIsPlayingChanged, onPlaybackStateChanged, onPlayerError) {
-        val listener = object : Player.Listener {
-            override fun onIsPlayingChanged(isPlaying: Boolean) {
-                onIsPlayingChanged(exoPlayer, isPlaying)
-            }
-
-            override fun onPlaybackStateChanged(playbackState: Int) {
-                onPlaybackStateChanged(exoPlayer, playbackState)
-            }
-
-            override fun onPlayerError(error: PlaybackException) {
-                onPlayerError(error.errorCode)
-            }
-        }
-        exoPlayer.addListener(listener)
-
-        onDispose {
-            exoPlayer.removeListener(listener)
-            exoPlayer.release()
-        }
-    }
-
-    return exoPlayer
-}
-
-@OptIn(UnstableApi::class)
-@Composable
-fun rememberMediaSession(exoPlayer: ExoPlayer): MediaSession {
-    val context = LocalContext.current
-
-    val mediaSession = remember(context, exoPlayer) {
-        MediaSession.Builder(context, exoPlayer).build()
-    }
-
-    DisposableEffect(mediaSession) {
-        onDispose { mediaSession.release() }
-    }
-
-    return mediaSession
-}
-
-fun ExoPlayer.toggle() =
+fun MediaController.toggle() =
     if (isPlaying) {
         pause()
     } else {
