7e52ec411c4e38c0d7aec2c14ca1ee8304b9dd62
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-09-22 15:56:56 +0200

    Improve live stream playback stability

--- app/src/aosp/kotlin/net/primal/android/core/player/AospPlayerManager.kt
+++ app/src/aosp/kotlin/net/primal/android/core/player/AospPlayerManager.kt
@@ -1,23 +1,36 @@
 package net.primal.android.core.player
 
 import android.content.Context
+import androidx.annotation.OptIn
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.media3.exoplayer.upstream.DefaultLoadErrorHandlingPolicy
 import javax.inject.Inject
 import net.primal.android.core.service.PlayerManager
+import net.primal.android.stream.player.LIVE_STREAM_MANIFEST_MIN_RETRY_COUNT
 
 class AospPlayerManager @Inject constructor() : PlayerManager {
 
+    @OptIn(UnstableApi::class)
     override fun createPlayer(context: Context): Player {
         val audioAttributes = AudioAttributes.Builder()
             .setUsage(C.USAGE_MEDIA)
             .setContentType(C.AUDIO_CONTENT_TYPE_MOVIE)
             .build()
 
+        val loadErrorHandlingPolicy =
+            DefaultLoadErrorHandlingPolicy(LIVE_STREAM_MANIFEST_MIN_RETRY_COUNT)
+
+        val mediaSourceFactory = DefaultMediaSourceFactory(context)
+            .setLoadErrorHandlingPolicy(loadErrorHandlingPolicy)
+
         return ExoPlayer.Builder(context)
             .setAudioAttributes(audioAttributes, true)
+            .setMediaSourceFactory(mediaSourceFactory)
             .build()
     }
 
