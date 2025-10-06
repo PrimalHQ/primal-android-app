6d10f1636e47bf99879a421e61462dd80970d8ff
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-10-06 16:29:38 +0200

    Fix format issue

--- app/src/main/kotlin/net/primal/android/stream/LiveStreamViewModel.kt
+++ app/src/main/kotlin/net/primal/android/stream/LiveStreamViewModel.kt
@@ -654,7 +654,7 @@ class LiveStreamViewModel @AssistedInject constructor(
                 when (result.error) {
                     is ZapError.InvalidZap, is ZapError.FailedToFetchZapPayRequest,
                     is ZapError.FailedToFetchZapInvoice,
-                        -> setState { copy(error = UiError.InvalidZapRequest()) }
+                    -> setState { copy(error = UiError.InvalidZapRequest()) }
 
                     ZapError.FailedToPublishEvent, ZapError.FailedToSignEvent -> {
                         setState { copy(error = UiError.FailedToPublishZapEvent()) }
