87f577198898c3516e2f9ad27ba29421ca8fa98a
Mehmedalija Karisik <mehmedalija@appollo41.com>
2025-10-16 15:34:28 +0200

    Fix player line visible at the top on light theme

--- app/src/main/kotlin/net/primal/android/stream/ui/LiveStreamPlayerBox.kt
+++ app/src/main/kotlin/net/primal/android/stream/ui/LiveStreamPlayerBox.kt
@@ -20,7 +20,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -71,7 +70,7 @@ fun LiveStreamPlayerBox(
                 StreamPlayerLoadingIndicator(
                     modifier = loadingModifier
                         .matchParentSize()
-                        .background(Color.Black),
+                        .background(AppTheme.colorScheme.background),
                     isFullscreen = localConfiguration.orientation == Configuration.ORIENTATION_LANDSCAPE,
                 )
             }
