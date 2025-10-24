69f9c13d2a9828d7dd93340ecf10510ceb4b933e
Marko Kocic <marko@appollo41.com>
2025-10-24 13:11:10 +0200

    Add annotations for ios repository factory

--- data/account/repository/src/iosMain/kotlin/net/primal/data/account/repository/repository/factory/IosRepositoryFactory.kt
+++ data/account/repository/src/iosMain/kotlin/net/primal/data/account/repository/repository/factory/IosRepositoryFactory.kt
@@ -1,10 +1,13 @@
 package net.primal.data.account.repository.repository.factory
 
+import kotlin.experimental.ExperimentalObjCName
 import net.primal.data.account.local.db.AccountDatabase
 import net.primal.shared.data.local.db.LocalDatabaseFactory
 
 typealias AccountRepositoryFactory = IosRepositoryFactory
 
+@OptIn(ExperimentalObjCName::class)
+@ObjCName("AccountRepositoryFactory")
 object IosRepositoryFactory : RepositoryFactory() {
 
     private val accountDatabase by lazy {
