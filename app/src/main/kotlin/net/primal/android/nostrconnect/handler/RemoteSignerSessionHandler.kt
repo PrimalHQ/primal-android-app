4f7630385a9c10a978f9a060210e0eb2158514e7
Marko Kocic <marko@appollo41.com>
2025-11-04 11:47:34 +0100

    Implement `RemoteSignerSesssionHandler`

---
+++ app/src/main/kotlin/net/primal/android/nostrconnect/handler/RemoteSignerSessionHandler.kt
@@ -0,0 +1,29 @@
+package net.primal.android.nostrconnect.handler
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+import net.primal.android.core.service.PrimalRemoteSignerService
+import net.primal.core.utils.onSuccess
+import net.primal.domain.account.repository.SessionRepository
+
+@Singleton
+class RemoteSignerSessionHandler @Inject constructor(
+    @param:ApplicationContext private val context: Context,
+    private val sessionRepository: SessionRepository,
+) {
+
+    suspend fun startSession(connectionId: String) {
+        sessionRepository.startSession(connectionId = connectionId)
+            .onSuccess {
+                if (!PrimalRemoteSignerService.isServiceRunning.value) {
+                    PrimalRemoteSignerService.start(context = context)
+                }
+            }
+    }
+
+    suspend fun endSession(sessionId: String) {
+        sessionRepository.endSession(sessionId = sessionId)
+    }
+}
