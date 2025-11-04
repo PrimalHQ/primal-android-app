381ec00437ced88566596a39c11dc761a9c5368a
Marko Kocic <marko@appollo41.com>
2025-11-04 11:45:06 +0100

    Add `callback` parameter to room database factory

--- data/shared/local/src/iosMain/kotlin/net/primal/shared/data/local/db/IosLocalDatabaseFactory.kt
+++ data/shared/local/src/iosMain/kotlin/net/primal/shared/data/local/db/IosLocalDatabaseFactory.kt
@@ -13,12 +13,22 @@ typealias LocalDatabaseFactory = IosLocalDatabaseFactory
 
 object IosLocalDatabaseFactory {
 
-    inline fun <reified T : RoomDatabase> createDatabase(databaseName: String): T {
+    inline fun <reified T : RoomDatabase> createDatabase(
+        databaseName: String,
+        callback: RoomDatabase.Callback? = null,
+    ): T {
         val dbFilePath = documentDirectory() + "/$databaseName"
         return buildLocalDatabase {
             Room.databaseBuilder<T>(name = dbFilePath)
                 .setQueryCoroutineContext(IOSDispatcherProvider().io())
                 .setDriver(NativeSQLiteDriver())
+                .run {
+                    if (callback != null) {
+                        this.addCallback(callback)
+                    } else {
+                        this
+                    }
+                }
         }
     }
 
