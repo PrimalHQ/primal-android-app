a880e224d58ee80ce69c95adddd9c87fd98160f2
Aleksandar Ilic <aleksandar@appollo41.com>
2025-11-07 11:12:56 +0100

    Change autoStart from default to required parameter

--- data/account/local/src/commonMain/kotlin/net/primal/data/account/local/dao/AppConnectionData.kt
+++ data/account/local/src/commonMain/kotlin/net/primal/data/account/local/dao/AppConnectionData.kt
@@ -23,5 +23,5 @@ data class AppConnectionData(
     val clientPubKey: Encryptable<String>,
     val signerPubKey: Encryptable<String>,
     val userPubKey: Encryptable<String>,
-    val autoStart: Boolean = true,
+    val autoStart: Boolean,
 )
