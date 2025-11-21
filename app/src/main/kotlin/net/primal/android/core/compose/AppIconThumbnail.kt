67e09cf8fc95d99fa25954f81b6ec045f29d36a6
Marko Kocic <marko@appollo41.com>
2025-11-21 17:49:04 +0100

    Adjust colors and center fallback letter icon vertically

--- app/src/main/kotlin/net/primal/android/core/compose/AppIconThumbnail.kt
+++ app/src/main/kotlin/net/primal/android/core/compose/AppIconThumbnail.kt
@@ -1,22 +1,41 @@
 package net.primal.android.core.compose
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import net.primal.android.core.compose.foundation.isAppInDarkPrimalTheme
+import net.primal.android.core.compose.preview.PrimalPreview
+import net.primal.android.core.images.AvatarCoilImageLoader
 import net.primal.android.theme.AppTheme
+import net.primal.android.theme.domain.PrimalTheme
 import net.primal.domain.links.CdnImage
+
+private val LETTER_ICON_BACKGROUND
+    @Composable
+    get() = if (isAppInDarkPrimalTheme()) {
+        Color(0xFF333333)
+    } else {
+        Color(0xFFDDDDDD)
+    }
 
 @Composable
 fun AppIconThumbnail(
@@ -38,25 +57,91 @@ fun AppIconThumbnail(
         )
     } else {
         val fallbackCharacter = appName?.firstOrNull()?.uppercase() ?: "?"
+        val textMeasurer = rememberTextMeasurer()
+        val fontSize = (avatarSize.value / 2).sp
+        val fontColor = AppTheme.extraColorScheme.onSurfaceVariantAlt2
+
         Box(
             modifier = modifier
                 .size(avatarSize)
                 .clip(CircleShape)
+                .background(LETTER_ICON_BACKGROUND)
                 .border(
-                    width = 1.dp,
-                    color = AppTheme.extraColorScheme.onSurfaceVariantAlt4,
+                    width = if (avatarSize <= 24.dp) 1.dp else 2.dp,
+                    color = AppTheme.extraColorScheme.onSurfaceVariantAlt2,
                     shape = CircleShape,
                 ),
-            contentAlignment = Alignment.Center,
         ) {
-            Text(
-                modifier = Modifier.padding(top = 2.dp),
-                text = fallbackCharacter,
-                style = AppTheme.typography.titleMedium,
-                fontSize = (avatarSize.value / 2).sp,
-                fontWeight = FontWeight.Bold,
-                color = AppTheme.extraColorScheme.onSurfaceVariantAlt2,
-            )
+            Canvas(Modifier.matchParentSize()) {
+                val textLayout = textMeasurer.measure(
+                    text = fallbackCharacter,
+                    style = TextStyle(
+                        fontSize = fontSize,
+                        fontWeight = FontWeight.Bold,
+                        color = fontColor,
+                        platformStyle = PlatformTextStyle(includeFontPadding = false),
+                    ),
+                )
+
+                val textWidth = textLayout.size.width.toFloat()
+                val textHeight = textLayout.size.height.toFloat()
+
+                val x = (size.width - textWidth) / 2f
+                val y = (size.height - textHeight) / 2f
+
+                drawText(
+                    textLayoutResult = textLayout,
+                    topLeft = Offset(x, y),
+                )
+            }
         }
+    }
+}
+
+@Composable
+@Preview
+fun PreviewAppIconThumbnailBig() {
+    PrimalPreview(primalTheme = PrimalTheme.Sunset) {
+        AppIconThumbnail(
+            avatarCdnImage = null,
+            appName = "Test",
+            avatarSize = 48.dp,
+        )
+    }
+}
+
+@Composable
+@Preview
+fun PreviewAppIconThumbnailMedium() {
+    PrimalPreview(primalTheme = PrimalTheme.Sunset) {
+        AppIconThumbnail(
+            avatarCdnImage = null,
+            appName = "Test",
+            avatarSize = 36.dp,
+        )
+    }
+}
+
+@Composable
+@Preview
+fun PreviewAppIconThumbnailSmall() {
+    PrimalPreview(primalTheme = PrimalTheme.Sunset) {
+        AppIconThumbnail(
+            avatarCdnImage = null,
+            appName = "Test",
+            avatarSize = 24.dp,
+        )
+    }
+}
+
+@Composable
+@Preview
+fun PreviewAppIconThumbnailExtraSmall() {
+    PrimalPreview(primalTheme = PrimalTheme.Sunset) {
+        AppIconThumbnail(
+            avatarCdnImage = null,
+            appName = "Test",
+            avatarSize = 18.dp,
+        )
     }
 }
