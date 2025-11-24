9bdc41e9f4ddeac0a44bbe7c6ed074f6ee8ca249
alltheseas <alltheseas@users.noreply.github.com>
2025-11-24 11:35:05 -0600

    fix: remove google services from root build

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ plugins {
     alias(libs.plugins.ktlint.wrapper.plugin)
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
-    alias(libs.plugins.google.services) apply false
     alias(libs.plugins.android.test) apply false
 }
 
