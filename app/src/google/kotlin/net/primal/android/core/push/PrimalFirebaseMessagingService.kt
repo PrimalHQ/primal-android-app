95f753bcb494e74ceb91d9990735a215e5c6dc22
Marko Kocic <marko@appollo41.com>
2025-11-25 15:14:58 +0100

    WIP

--- app/src/google/kotlin/net/primal/android/core/push/PrimalFirebaseMessagingService.kt
+++ app/src/google/kotlin/net/primal/android/core/push/PrimalFirebaseMessagingService.kt
@@ -1,17 +1,24 @@
 package net.primal.android.core.push
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class PrimalFirebaseMessagingService : FirebaseMessagingService() {
 
     @Inject
     lateinit var tokenUpdater: PushNotificationsTokenUpdater
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        Timber.tag("Signer").d("Received message: $message")
+        super.onMessageReceived(message)
+    }
 
     @OptIn(DelicateCoroutinesApi::class)
     override fun onNewToken(token: String) {
