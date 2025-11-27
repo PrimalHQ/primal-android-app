08572dfc79244c06018d9f845ddeb472701087f4
Marko Kocic <marko@appollo41.com>
2025-11-27 18:20:54 +0100

    Provide fallback stream title for stream pills

--- app/src/main/kotlin/net/primal/android/notes/feed/model/StreamPillUi.kt
+++ app/src/main/kotlin/net/primal/android/notes/feed/model/StreamPillUi.kt
@@ -1,11 +1,12 @@
 package net.primal.android.notes.feed.model
 
 import net.primal.domain.links.CdnImage
+import net.primal.domain.nostr.utils.authorNameUiFriendly
 import net.primal.domain.streams.Stream
 
 data class StreamPillUi(
     val naddr: String,
-    val title: String?,
+    val title: String,
     val currentParticipants: Int,
     val hostProfileId: String,
     val hostAvatarCdnImage: CdnImage?,
@@ -14,7 +15,12 @@ data class StreamPillUi(
 fun Stream.asStreamPillUi() =
     StreamPillUi(
         naddr = this.toNaddrString(),
-        title = this.title,
+        title = this.title
+            ?: authorNameUiFriendly(
+                displayName = this.mainHostProfile?.displayName,
+                name = this.mainHostProfile?.handle,
+                pubkey = this.mainHostId,
+            ),
         currentParticipants = this.currentParticipants ?: 0,
         hostProfileId = this.mainHostId,
         hostAvatarCdnImage = this.mainHostProfile?.avatarCdnImage,
