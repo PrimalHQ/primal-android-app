21c0f408f8547f65da56094e4359fd58122de2cf
Marko Kocic <marko@appollo41.com>
2025-11-27 11:04:51 +0100

    Handle signer wake-up notification from backend

---
+++ app/src/google/kotlin/net/primal/android/nostrconnect/handler/RemoteSignerRemoteMessageHandler.kt
@@ -0,0 +1,32 @@
+package net.primal.android.nostrconnect.handler
+
+import com.google.firebase.messaging.RemoteMessage
+import javax.inject.Inject
+import net.primal.android.user.credentials.CredentialsStore
+import net.primal.android.user.domain.asKeyPair
+import net.primal.domain.account.repository.SessionEventRepository
+
+private const val NIP46_EVENT_PUBKEY = "nip46_event_pubkey"
+private const val NIP46_EVENT_ID = "nip46_event_id"
+
+class RemoteSignerRemoteMessageHandler @Inject constructor(
+    private val sessionHandler: RemoteSignerSessionHandler,
+    private val sessionEventRepository: SessionEventRepository,
+    private val credentialsStore: CredentialsStore,
+) {
+    fun isRemoteSignerMessage(message: RemoteMessage) =
+        message.data[NIP46_EVENT_PUBKEY] != null && message.data[NIP46_EVENT_ID] != null
+
+    suspend fun process(message: RemoteMessage) {
+        val clientPubKey = message.data[NIP46_EVENT_PUBKEY]
+        val eventId = message.data[NIP46_EVENT_ID]
+
+        if (clientPubKey != null && eventId != null) {
+            sessionHandler.startSessionForClient(clientPubKey = clientPubKey)
+            sessionEventRepository.processMissedEvents(
+                signerKeyPair = credentialsStore.getOrCreateInternalSignerCredentials().asKeyPair(),
+                eventIds = listOf(eventId),
+            )
+        }
+    }
+}
