071686a1812d9ee1914200f3d4901c53778abaec
Marko Kocic <marko@appollo41.com>
2025-11-27 11:04:23 +0100

    Fix processing duplicate session events

--- data/account/local/src/commonMain/kotlin/net/primal/data/account/local/dao/SessionEventDataDao.kt
+++ data/account/local/src/commonMain/kotlin/net/primal/data/account/local/dao/SessionEventDataDao.kt
@@ -1,15 +1,15 @@
 package net.primal.data.account.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 import net.primal.shared.data.local.encryption.Encryptable
 
 @Dao
 interface SessionEventDataDao {
-    @Upsert
-    suspend fun upsert(data: SessionEventData)
+    @Insert
+    suspend fun insert(data: SessionEventData)
 
     @Query("SELECT * FROM SessionEventData WHERE sessionId = :sessionId ORDER BY requestedAt DESC")
     fun observeEventsBySessionId(sessionId: String): Flow<List<SessionEventData>>
