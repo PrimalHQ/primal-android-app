e1b68ac792d8732ce8d79b09fedabf077b9551e2
Marko Kocic <marko@appollo41.com>
2025-11-28 10:31:40 +0100

    Connect note with self in `FeedResponseProcessor`

--- data/caching/repository/src/commonMain/kotlin/net/primal/data/repository/feed/processors/FeedResponseProcessor.kt
+++ data/caching/repository/src/commonMain/kotlin/net/primal/data/repository/feed/processors/FeedResponseProcessor.kt
@@ -116,6 +116,14 @@ internal suspend fun FeedResponse.persistToDatabaseAsTransaction(userId: String,
         database.articles().upsertAll(list = allArticles)
         database.highlights().upsertAll(data = referencedHighlights)
         database.streams().upsertStreamData(data = streamData)
+        database.threadConversations().connectNoteWithReply(
+            data = allPosts.map {
+                NoteConversationCrossRef(
+                    noteId = it.postId,
+                    replyNoteId = it.postId,
+                )
+            },
+        )
 
         val eventHintsDao = database.eventHints()
         val hintsMap = eventHints.associateBy { it.eventId }
