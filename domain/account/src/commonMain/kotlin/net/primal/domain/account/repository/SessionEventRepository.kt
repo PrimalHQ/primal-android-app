ffac746c163ab96bfe9e88a360f9285930f1dbfb
Aleksandar Ilic <aleksandar@appollo41.com>
2025-12-11 14:56:06 +0100

    Migrate to SessionEventUserChoice from Pair

--- domain/account/src/commonMain/kotlin/net/primal/domain/account/repository/SessionEventRepository.kt
+++ domain/account/src/commonMain/kotlin/net/primal/domain/account/repository/SessionEventRepository.kt
@@ -3,6 +3,7 @@ package net.primal.domain.account.repository
 import kotlinx.coroutines.flow.Flow
 import net.primal.core.utils.Result
 import net.primal.domain.account.model.SessionEvent
+import net.primal.domain.account.model.SessionEventUserChoice
 import net.primal.domain.account.model.UserChoice
 import net.primal.domain.nostr.cryptography.NostrKeyPair
 
@@ -17,5 +18,5 @@ interface SessionEventRepository {
 
     suspend fun respondToEvent(eventId: String, userChoice: UserChoice): Result<Unit>
 
-    suspend fun respondToEvents(eventIdToUserChoice: List<Pair<String, UserChoice>>): Result<Unit>
+    suspend fun respondToEvents(userChoices: List<SessionEventUserChoice>): Result<Unit>
 }
