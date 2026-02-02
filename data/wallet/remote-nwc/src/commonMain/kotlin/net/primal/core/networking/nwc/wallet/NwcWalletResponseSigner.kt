7fc968afebc3fe8c56108aab7b0728a1c90e632d
Marko Kocic <marko@appollo41.com>
2026-02-02 14:58:46 +0100

    Implement sending nwc info event on relay when connection is established

--- data/wallet/remote-nwc/src/commonMain/kotlin/net/primal/core/networking/nwc/wallet/NwcWalletResponseSigner.kt
+++ data/wallet/remote-nwc/src/commonMain/kotlin/net/primal/core/networking/nwc/wallet/NwcWalletResponseSigner.kt
@@ -4,9 +4,12 @@ import fr.acinq.secp256k1.Hex
 import kotlin.io.encoding.ExperimentalEncodingApi
 import net.primal.core.networking.nwc.wallet.model.WalletNwcRequest
 import net.primal.core.networking.nwc.wallet.model.WalletNwcRequestException
+import net.primal.domain.connections.nostr.model.NwcConnection
 import net.primal.domain.nostr.NostrEventKind
 import net.primal.domain.nostr.NostrUnsignedEvent
+import net.primal.domain.nostr.asEncryptionTag
 import net.primal.domain.nostr.asEventIdTag
+import net.primal.domain.nostr.asNotificationsTag
 import net.primal.domain.nostr.asPubkeyTag
 import net.primal.domain.nostr.cryptography.SignResult
 import net.primal.domain.nostr.cryptography.SignatureException
@@ -65,6 +68,37 @@ fun signNwcErrorResponseNostrEvent(error: WalletNwcRequestException, responseJso
         kind = NostrEventKind.NwcResponse.value,
         content = encrypted,
         tags = tags,
+    )
+
+    return runCatching {
+        SignResult.Signed(
+            unsigned.signOrThrow(hexPrivateKey = Hex.decode(connection.serviceKeyPair.privateKey)),
+        )
+    }.getOrElse {
+        SignResult.Rejected(SignatureException(cause = it))
+    }
+}
+
+@OptIn(ExperimentalEncodingApi::class)
+fun signNwcInfoNostrEvent(
+    connection: NwcConnection,
+    supportedEncryption: List<String>,
+    supportedNotifications: List<String>,
+    supportedMethods: List<String>,
+): SignResult {
+    val content = supportedMethods.joinToString(" ")
+
+    val encryptionTag = supportedEncryption.takeIf { it.isNotEmpty() }
+        ?.joinToString(" ")?.asEncryptionTag()
+
+    val notificationsTag = supportedNotifications.takeIf { it.isNotEmpty() }
+        ?.joinToString(" ")?.asNotificationsTag()
+
+    val unsigned = NostrUnsignedEvent(
+        pubKey = connection.serviceKeyPair.pubKey,
+        kind = NostrEventKind.WalletInfo.value,
+        content = content,
+        tags = listOfNotNull(encryptionTag, notificationsTag),
     )
 
     return runCatching {
