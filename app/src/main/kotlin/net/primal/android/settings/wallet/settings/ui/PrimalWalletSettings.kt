9b3d2cbef64dce4ddd508ee0b59e510004c6166f
Mehmedalija Karisik <mehmedalija@appollo41.com>
2026-02-02 13:32:35 +0100

    Hide backup wallet options for wallets that don't support backup

--- app/src/main/kotlin/net/primal/android/settings/wallet/settings/ui/PrimalWalletSettings.kt
+++ app/src/main/kotlin/net/primal/android/settings/wallet/settings/ui/PrimalWalletSettings.kt
@@ -95,7 +95,7 @@ fun PrimalWalletSettings(
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        if (!state.showBackupWidget) {
+        if (state.showBackupListItem) {
             SettingsItem(
                 headlineText = stringResource(id = R.string.settings_wallet_backup_wallet_title),
                 supportText = stringResource(id = R.string.settings_wallet_backup_wallet_subtitle),
