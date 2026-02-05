fca712f78b90b8ddd929e9a8e82ab2a0bc73ac05
Aleksandar Ilic <aleksandar@appollo41.com>
2026-02-05 19:08:53 +0100

    Clear tx migration state on reverting back to custodial

--- domain/wallet/src/commonMain/kotlin/net/primal/domain/account/SparkWalletAccountRepository.kt
+++ domain/wallet/src/commonMain/kotlin/net/primal/domain/account/SparkWalletAccountRepository.kt
@@ -29,4 +29,6 @@ interface SparkWalletAccountRepository {
     suspend fun deleteSparkWalletByUserId(userId: String): Result<String>
 
     suspend fun isPrimalTxsMigrationCompleted(walletId: String): Boolean
+
+    suspend fun clearPrimalTxsMigrationState(walletId: String)
 }
