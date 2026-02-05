7740294dec6004e09515aee70fcca1e342dca41b
Aleksandar Ilic <aleksandar@appollo41.com>
2026-02-05 22:26:02 +0100

    Primal Shared 0.1.5 release

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -33,7 +33,7 @@ val unzipBreezFramework by tasks.registering(Copy::class) {
 }
 
 private val xcfName = "PrimalShared"
-private val xcfVersionName = "0.1.4"
+private val xcfVersionName = "0.1.5"
 
 // Shared dependencies exported to iOS
 private val exportedDependencies = listOf(
