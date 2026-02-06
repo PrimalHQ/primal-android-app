ffb76b610876c1d10687ddf2aaeedde9fb13ae06
Marko Kocic <marko@appollo41.com>
2026-02-06 18:16:31 +0100

    Implement Wallet Upgrade FAQ screen

--- app/src/main/kotlin/net/primal/android/premium/info/ui/FAQTabContent.kt
+++ app/src/main/kotlin/net/primal/android/premium/info/ui/FAQTabContent.kt
@@ -1,15 +1,11 @@
 package net.primal.android.premium.info.ui
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import net.primal.android.R
+import net.primal.android.core.compose.QAList
 
 @Composable
 fun FAQTabContent(modifier: Modifier = Modifier, contentPadding: PaddingValues) {
@@ -67,20 +63,9 @@ fun FAQTabContent(modifier: Modifier = Modifier, contentPadding: PaddingValues) 
             stringResource(id = R.string.premium_more_info_faq_thirteenth_answer),
         ),
     )
-    LazyColumn(
-        contentPadding = contentPadding,
+    QAList(
+        questionAndAnswerPairs = questionAndAnswerPairs,
         modifier = modifier,
-    ) {
-        item { Spacer(modifier = Modifier.height(20.dp)) }
-        items(
-            items = questionAndAnswerPairs,
-            key = { it.first },
-        ) { qaPair ->
-            QAColumn(
-                question = qaPair.first,
-                answer = qaPair.second,
-            )
-            Spacer(modifier = Modifier.height(42.dp))
-        }
-    }
+        contentPadding = contentPadding,
+    )
 }
