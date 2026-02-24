ca9a669d596d090e04e2a336950b3a90f86f62bd
Marko Kocic <marko@appollo41.com>
2026-02-24 14:16:32 +0100

    Fix wallet text parser failing when there is no `lightning:` prefix

--- data/wallet/repository/src/commonMain/kotlin/net/primal/wallet/data/parser/factory/ParserFactory.kt
+++ data/wallet/repository/src/commonMain/kotlin/net/primal/wallet/data/parser/factory/ParserFactory.kt
@@ -1,8 +1,8 @@
 package net.primal.wallet.data.parser.factory
 
-import WalletTextParserImpl
 import net.primal.domain.parser.WalletTextParser
 import net.primal.domain.wallet.WalletRepository
+import net.primal.wallet.data.parser.WalletTextParserImpl
 
 object ParserFactory {
     fun createWalletTextParser(walletRepository: WalletRepository): WalletTextParser =
