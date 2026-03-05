6696c006aca3426c69c94f1c60857c4ce4ca55c7
Aleksandar Ilic <aleksandar@appollo41.com>
2026-03-05 18:24:53 +0100

    Implement wallet creation from bottom sheet notices

--- app/src/main/kotlin/net/primal/android/wallet/notice/sheet/WalletNoticeBottomSheet.kt
+++ app/src/main/kotlin/net/primal/android/wallet/notice/sheet/WalletNoticeBottomSheet.kt
@@ -1,22 +1,31 @@
 package net.primal.android.wallet.notice.sheet
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -31,10 +40,11 @@ import net.primal.android.core.compose.PrimalClickableText
 import net.primal.android.core.compose.UniversalAvatarThumbnail
 import net.primal.android.core.compose.button.PrimalFilledButton
 import net.primal.android.theme.AppTheme
+import net.primal.android.wallet.notice.sheet.WalletNoticeSheetContract.UiEvent
+import net.primal.android.wallet.notice.sheet.WalletNoticeSheetContract.UiState
 
 private const val FAQ_ANNOTATION_TAG = "FaqAnnotationTag"
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WalletNoticeBottomSheet(
     viewModel: WalletNoticeSheetViewModel,
@@ -44,12 +54,44 @@ fun WalletNoticeBottomSheet(
     content: @Composable () -> Unit,
 ) {
     val state by viewModel.state.collectAsState()
+
+    val context = LocalContext.current
+    LaunchedEffect(state.error) {
+        if (state.error != null) {
+            Toast.makeText(context, R.string.wallet_dashboard_create_wallet_error, Toast.LENGTH_SHORT).show()
+            viewModel.setEvent(UiEvent.DismissError)
+        }
+    }
+
+    WalletNoticeBottomSheet(
+        state = state,
+        eventPublisher = viewModel::setEvent,
+        onUpgradeClick = onUpgradeClick,
+        onFaqClick = onFaqClick,
+        onRestoreWalletClick = onRestoreWalletClick,
+        content = content,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun WalletNoticeBottomSheet(
+    state: UiState,
+    eventPublisher: (UiEvent) -> Unit,
+    onUpgradeClick: () -> Unit,
+    onFaqClick: () -> Unit,
+    onRestoreWalletClick: () -> Unit,
+    content: @Composable () -> Unit,
+) {
+    val creatingWallet by rememberUpdatedState(state.creatingWallet)
     val sheetState = rememberModalBottomSheetState(
         skipPartiallyExpanded = true,
+        confirmValueChange = { it != SheetValue.Hidden || !creatingWallet },
     )
 
     if (state.noticeType != null && state.shouldShowNotice) {
-        val onDismiss: () -> Unit = { viewModel.setEvent(WalletNoticeSheetContract.UiEvent.DismissSheet) }
+        val onDismiss: () -> Unit = { if (!state.creatingWallet) eventPublisher(UiEvent.DismissSheet) }
+        val onCreateWallet: () -> Unit = { eventPublisher(UiEvent.CreateWallet) }
 
         when (state.noticeType) {
             WalletNoticeType.UpgradeWallet -> UpgradeWalletSheetContent(
@@ -70,13 +112,22 @@ fun WalletNoticeBottomSheet(
                 state = state,
                 sheetState = sheetState,
                 onDismissRequest = onDismiss,
+                onRestoreWalletClick = {
+                    onDismiss()
+                    onRestoreWalletClick()
+                },
+                onCreateWalletClick = onCreateWallet,
             )
 
             WalletNoticeType.WalletDetected -> WalletDetectedSheetContent(
                 state = state,
                 sheetState = sheetState,
                 onDismissRequest = onDismiss,
-                onRestoreWalletClick = onRestoreWalletClick,
+                onRestoreWalletClick = {
+                    onDismiss()
+                    onRestoreWalletClick()
+                },
+                onCreateWalletClick = onCreateWallet,
             )
 
             null -> Unit
@@ -89,7 +140,7 @@ fun WalletNoticeBottomSheet(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun UpgradeWalletSheetContent(
-    state: WalletNoticeSheetContract.UiState,
+    state: UiState,
     sheetState: SheetState,
     onDismissRequest: () -> Unit,
     onUpgradeClick: () -> Unit,
@@ -211,9 +262,11 @@ private fun UpgradeWalletNowButton(modifier: Modifier = Modifier, onClick: () ->
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun WalletDiscontinuedSheetContent(
-    state: WalletNoticeSheetContract.UiState,
+    state: UiState,
     sheetState: SheetState,
     onDismissRequest: () -> Unit,
+    onRestoreWalletClick: () -> Unit,
+    onCreateWalletClick: () -> Unit,
 ) {
     ModalBottomSheet(
         onDismissRequest = onDismissRequest,
@@ -255,18 +308,11 @@ private fun WalletDiscontinuedSheetContent(
                 textAlign = TextAlign.Center,
             )
 
-            PrimalFilledButton(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .fillMaxWidth(),
-                onClick = onDismissRequest,
-            ) {
-                Text(
-                    text = stringResource(id = R.string.wallet_notice_discontinued_close_button),
-                    fontWeight = FontWeight.Bold,
-                    style = AppTheme.typography.bodyLarge,
-                )
-            }
+            RestoreOrCreateSection(
+                creatingWallet = state.creatingWallet,
+                onRestoreWalletClick = onRestoreWalletClick,
+                onCreateWalletClick = onCreateWalletClick,
+            )
         }
     }
 }
@@ -274,10 +320,11 @@ private fun WalletDiscontinuedSheetContent(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun WalletDetectedSheetContent(
-    state: WalletNoticeSheetContract.UiState,
+    state: UiState,
     sheetState: SheetState,
     onDismissRequest: () -> Unit,
     onRestoreWalletClick: () -> Unit,
+    onCreateWalletClick: () -> Unit,
 ) {
     ModalBottomSheet(
         onDismissRequest = onDismissRequest,
@@ -313,43 +360,83 @@ private fun WalletDetectedSheetContent(
             }
 
             Text(
+                modifier = Modifier.padding(horizontal = 16.dp),
                 text = stringResource(id = R.string.wallet_notice_detected_description),
                 color = AppTheme.extraColorScheme.onSurfaceVariantAlt2,
                 style = AppTheme.typography.bodyMedium.copy(lineHeight = 24.sp),
                 textAlign = TextAlign.Center,
             )
 
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-            ) {
-                PrimalFilledButton(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .fillMaxWidth(),
-                    onClick = {
-                        onDismissRequest()
-                        onRestoreWalletClick()
-                    },
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.wallet_notice_detected_restore_button),
-                        fontWeight = FontWeight.Bold,
-                        style = AppTheme.typography.bodyLarge,
-                    )
-                }
+            RestoreOrCreateSection(
+                creatingWallet = state.creatingWallet,
+                onRestoreWalletClick = onRestoreWalletClick,
+                onCreateWalletClick = onCreateWalletClick,
+            )
+        }
+    }
+}
 
-                TextButton(
-                    onClick = onDismissRequest,
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.wallet_notice_detected_create_button),
-                        color = AppTheme.colorScheme.secondary,
-                        fontWeight = FontWeight.SemiBold,
-                        style = AppTheme.typography.bodyLarge,
-                    )
-                }
-            }
+@Composable
+private fun RestoreOrCreateSection(
+    modifier: Modifier = Modifier,
+    creatingWallet: Boolean,
+    onRestoreWalletClick: () -> Unit,
+    onCreateWalletClick: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(128.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        if (creatingWallet) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(40.dp),
+                color = AppTheme.colorScheme.secondary,
+            )
+        } else {
+            RestoreOrCreateButtons(
+                modifier = Modifier.padding(bottom = 16.dp),
+                onRestoreWalletClick = onRestoreWalletClick,
+                onCreateWalletClick = onCreateWalletClick,
+            )
+        }
+    }
+}
+
+@Composable
+private fun RestoreOrCreateButtons(
+    modifier: Modifier = Modifier,
+    onRestoreWalletClick: () -> Unit,
+    onCreateWalletClick: () -> Unit,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(24.dp),
+    ) {
+        PrimalFilledButton(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth(),
+            onClick = onRestoreWalletClick,
+        ) {
+            Text(
+                text = stringResource(id = R.string.wallet_notice_detected_restore_button),
+                fontWeight = FontWeight.Bold,
+                style = AppTheme.typography.bodyLarge,
+            )
+        }
+
+        TextButton(
+            onClick = onCreateWalletClick,
+        ) {
+            Text(
+                text = stringResource(id = R.string.wallet_notice_detected_create_button),
+                color = AppTheme.colorScheme.secondary,
+                fontWeight = FontWeight.SemiBold,
+                style = AppTheme.typography.bodyLarge,
+            )
         }
     }
 }
