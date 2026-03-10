5b19285ddf0c8f17d84fffb63f0140f46515d234
Mehmedalija <rg.mehmedalija@gmail.com>
2026-03-10 13:36:51 +0100

    Fix PollMapper and use right PollOptionInfo

--- data/caching/repository/src/commonMain/kotlin/net/primal/data/repository/mappers/local/PollMapper.kt
+++ data/caching/repository/src/commonMain/kotlin/net/primal/data/repository/mappers/local/PollMapper.kt
@@ -2,6 +2,7 @@ package net.primal.data.repository.mappers.local
 
 import net.primal.data.local.dao.polls.PollData
 import net.primal.data.local.dao.polls.PollType
+import net.primal.domain.polls.PollOptionInfo
 import net.primal.domain.posts.FeedPostPollInfo
 
 fun PollData.asFeedPostPollInfo(): FeedPostPollInfo {
@@ -11,7 +12,7 @@ fun PollData.asFeedPostPollInfo(): FeedPostPollInfo {
             PollType.Zap -> FeedPostPollInfo.PollType.Zap
         },
         options = this.options.map { option ->
-            FeedPostPollInfo.PollOptionInfo(
+            PollOptionInfo(
                 id = option.id,
                 label = option.label,
                 voteCount = option.voteCount,
