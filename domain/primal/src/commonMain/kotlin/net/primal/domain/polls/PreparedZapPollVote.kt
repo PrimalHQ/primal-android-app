40865492f9ed6fa24c1869a5fd84c3d81f3b1e22
Mehmedalija <rg.mehmedalija@gmail.com>
2026-03-10 17:39:27 +0100

    Remove PreparedZapPollVote

--- domain/primal/src/commonMain/kotlin/net/primal/domain/polls/PreparedZapPollVote.kt
+++
@@ -1,9 +0,0 @@
-package net.primal.domain.polls
-
-import kotlinx.serialization.json.JsonArray
-
-data class PreparedZapPollVote(
-    val recipientUserId: String,
-    val recipientLnUrl: String,
-    val pollOptionTag: JsonArray,
-)
