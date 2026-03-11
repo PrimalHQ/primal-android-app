94dd53364c2cc485ab1e84da4d89f871c355599a
Mehmedalija Karišik <mehmedalija@appollo41.com>
2026-03-11 14:10:36 +0100

    Update onboarding text colors for light background

--- app/src/main/kotlin/net/primal/android/auth/compose/OnboardingTextTypography.kt
+++ app/src/main/kotlin/net/primal/android/auth/compose/OnboardingTextTypography.kt
@@ -1,8 +1,6 @@
 package net.primal.android.auth.compose
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import net.primal.android.theme.AppTheme
 
@@ -11,6 +9,5 @@ fun onboardingTextHintTypography() =
     AppTheme.typography.bodyMedium.copy(
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        fontWeight = FontWeight.SemiBold,
-        color = Color.White,
+        color = OnboardingTextColor.copy(alpha = 0.8f),
     )
