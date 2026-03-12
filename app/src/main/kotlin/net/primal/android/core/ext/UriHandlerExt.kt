7fa3e8156a84be88bd58b4a7187cdb6c5ef60c23
Mehmedalija Karišik <mehmedalija@appollo41.com>
2026-03-12 15:06:10 +0100

    Fix links on welcome screen

--- app/src/main/kotlin/net/primal/android/core/ext/UriHandlerExt.kt
+++ app/src/main/kotlin/net/primal/android/core/ext/UriHandlerExt.kt
@@ -1,7 +1,10 @@
 package net.primal.android.core.ext
 
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
 import androidx.compose.ui.platform.UriHandler
+import androidx.core.net.toUri
 
 fun UriHandler.openUriSafely(uri: String) {
     try {
@@ -15,5 +18,17 @@ fun UriHandler.openUriSafely(uri: String) {
         runCatching {
             openUri("https://$uri")
         }
+    }
+}
+
+fun Context.openUriInExternalBrowser(uri: String) {
+    runCatching {
+        val intent = Intent(Intent.ACTION_VIEW, uri.toUri()).apply {
+            addCategory(Intent.CATEGORY_BROWSABLE)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        val browserIntent = Intent.makeMainSelectorActivity(Intent.ACTION_MAIN, Intent.CATEGORY_APP_BROWSER)
+        intent.selector = browserIntent
+        startActivity(intent)
     }
 }
