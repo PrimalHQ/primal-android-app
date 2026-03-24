266840235ebc63207cfd30856a9e6c9d196b8316
Aleksandar Ilic <aleksandar@appollo41.com>
2026-03-24 22:04:01 +0100

    Primal 3.0.18 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ fun extractSigningConfigProperties(storeName: String): SigningConfigProperties? 
     )
 }
 
-val appVersionCode = 2692
-val appVersionName = "3.0.17"
+val appVersionCode = 2693
+val appVersionName = "3.0.18"
 
 tasks.register("generateReleaseProperties") {
     doLast {
