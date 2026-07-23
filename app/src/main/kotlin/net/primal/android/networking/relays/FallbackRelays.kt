9abd344bad661facea544885b10f9e755d01052c
Aleksandar Ilic <aleksandar@appollo41.com>
2026-07-23 16:31:02 +0200

    Update fallback relay urls

--- app/src/main/kotlin/net/primal/android/networking/relays/FallbackRelays.kt
+++ app/src/main/kotlin/net/primal/android/networking/relays/FallbackRelays.kt
@@ -4,13 +4,11 @@ import net.primal.android.user.domain.toRelay
 
 val FALLBACK_RELAY_URLS = listOf(
     "wss://relay.primal.net",
-    "wss://relay.damus.io",
-    "wss://relay.nostr.band",
-    "wss://relay.current.fyi",
-    "wss://purplepag.es",
     "wss://nos.lol",
-    "wss://offchain.pub",
-    "wss://nostr.bitcoiner.social",
+    "wss://relay.nostr.net",
+    "wss://relay.snort.social",
+    "wss://purplepag.es",
+    "wss://nostr.land",
 )
 
 val FALLBACK_RELAYS = FALLBACK_RELAY_URLS.map { it.toRelay() }
